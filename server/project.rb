23c505fe894993ca993ceadbe3170fa5fb36c837
Marcus Riemer <Marcus@GurXite.de>
2016-01-29 17:23:37 +0100

    Early stages of JSON schema description

--- server/project.rb
+++ server/project.rb
@@ -1,2 +1,77 @@
-require 'sinatra/base'
+require "sqlite3"
+require "json"
 
+
+# Describes a single column of a SQLite Table
+class SchemaColumn
+  attr_reader :index, :name, :type, :not_null, :dflt_value, :primary
+  
+  def initialize(index, name, type, not_null, dflt_value, pk)
+    @index = index
+    @name = name
+    @type = type
+    @not_null = not_null == 1
+    @dflt_value = dflt_value
+    @primary = pk == 1
+  end
+
+  def to_json(options)
+    {
+      :index => @index, :name => @name, :type => @type,
+      :not_null => @not_null, :dflt_value => @dflt_value,
+      :primary => @primary
+    }.to_json(options)
+  end
+end
+
+# Describes a SQLite table with its columns
+class SchemaTable
+  attr_reader :name, :columns
+  
+  def initialize(name)
+    @name = name
+    @columns = []
+  end
+
+  def add_column(schema_column)
+    @columns[schema_column.index] = schema_column
+  end
+
+  def [](idx)
+    return @columns[idx]
+  end
+
+  def to_json(options)
+    { :name => @name, :columns => @columns.map {|col| col.to_json(options) } }.to_json(options)
+  end
+end
+
+# Describes the schema of a whole database as a handy dictionary
+# of tables with their columns.
+def database_describe_schema(sqlite_file_path)
+  db = SQLite3::Database.new(sqlite_file_path)
+
+  # Find out names of tables
+  table_names = db.execute("SELECT name
+                       FROM sqlite_master
+                       WHERE type='table'
+                       ORDER BY name;")
+
+  tables = Hash.new
+
+  # Fill in the column for each table
+  table_names.each do |name|
+    name = name[0]
+    
+    table_schema = SchemaTable.new name
+    db.execute("PRAGMA table_info(#{name})") do |ci|
+
+      column_schema = SchemaColumn.new(ci[0],ci[1],ci[2],ci[3],ci[4],ci[5])
+      table_schema.add_column(column_schema)
+    end
+
+    tables[name] = table_schema
+  end
+
+  return tables
+end
