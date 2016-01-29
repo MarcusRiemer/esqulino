1b74f274cc38f8b295d5703472a82630ef615836
Marcus Riemer <Marcus@GurXite.de>
2016-01-29 17:23:37 +0100

    Early stages of JSON schema description

---
+++ server/project.test.rb
@@ -0,0 +1,47 @@
+require "./project.rb"
+
+require "test/unit"
+require "json"
+
+# Not strictly a unit test, as it requires the use of external files.
+# But for the moment this is what we will roll with, as this does
+# show how the SQLite API behaves.
+class TestProject < Test::Unit::TestCase
+ 
+  def test_describe_events_schema
+    schema = database_describe_schema "../data/dev/events/db.sqlite"
+
+    assert_equal(2, schema.count)
+
+    table_person = schema['person']
+    column_person_id = table_person[0]
+    assert_equal("person_id", column_person_id.name)
+    assert_equal("INTEGER", column_person_id.type)
+    assert_equal(true, column_person_id.not_null)
+    assert_nil(column_person_id.dflt_value)
+    assert_equal(true, column_person_id.primary)
+
+    column_person_name = table_person[1]
+    assert_equal("name", column_person_name.name)
+    assert_equal("TEXT", column_person_name.type)
+    assert_equal(true, column_person_name.not_null)
+    assert_nil(column_person_name.dflt_value)
+    assert_equal(false, column_person_name.primary)
+
+    column_person_geb_dat = table_person[2]
+    assert_equal("geb_dat", column_person_geb_dat.name)
+    assert_equal("INTEGER", column_person_geb_dat.type)
+    assert_equal(true, column_person_geb_dat.not_null)
+    assert_nil(column_person_geb_dat.dflt_value)
+    assert_equal(false, column_person_geb_dat.primary)
+  end
+
+  def test_json_events_schema
+    schema = database_describe_schema "../data/dev/events/db.sqlite"
+
+    puts schema.to_s
+
+    puts JSON.dump(schema['person'])
+  end
+ 
+end
