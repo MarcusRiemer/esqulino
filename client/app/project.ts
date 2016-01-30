b86543eaa934e3b6c71f68e4496ce6b251576241
Marcus Riemer <Marcus@GurXite.de>
2016-01-30 16:30:27 +0100

    Introduced interface for project descriptions as a lightweight alternative to full projects

--- client/app/project.ts
+++ client/app/project.ts
@@ -1,3 +1,17 @@
+/**
+ * The properties of a project that can be queried from the
+ * server. Only contains publically visible data, not the
+ * password or other private information.
+ */
+export interface ProjectDescription {
+    name : string;
+    description : string;
+}
+
+/**
+ * A single project in greater detail, including means of
+ * manipulating it.
+ */
 export class Project {
     constructor(public name:string) {
 
