0dbdffb44d90428c9189d043e4b132e28b5393e4
Marcus Riemer <Marcus@GurXite.de>
2016-03-09 15:25:39 +0100

    Switched build system and updated Angular
    * Now using grunt
    * Angular 2 Beta 7

---
+++ client/Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-ts');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  var destPath =  '../dist/client/';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    ts: {
+      default: {
+        tsconfig: 'tsconfig.json'
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['**/*.ts', '!**/node_modules/**'],
+        tasks: ['ts'],
+        options: {
+          spawn: false,
+        },
+      },
+      css: {
+        files: ['**/*.css'],
+        tasks: ['copy'],
+        options: {
+          spawn: false,
+        },
+      },
+      css: {
+        files: ['**/*.html'],
+        tasks: ['copy'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+    
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            src: ['index.html', 'app/**/*.html', 'app/**/*.css', 'vendor/**/*.css'],
+            dest: destPath,
+            filter: 'isFile'
+          },
+          {
+            expand: true,
+            src: ['node_modules/es6-shim/**/*.js',
+                  'node_modules/systemjs/dist/**/*.js',
+                  'node_modules/rxjs/bundles/**/*.js',
+                  'node_modules/angular2/bundles/**/*.js'],
+            dest: destPath,
+            filter: 'isFile'
+          }
+        ],
+      },
+    }
+  });
+
+  grunt.registerTask('default', ['ts', 'copy']);
+}
