9ce2edf2d6c3821bb0ce1644b0bb72d0dcb33360
Marcus Riemer <Marcus@GurXite.de>
2016-03-29 18:04:17 +0200

    Attempted to introduce renaming of queries but added the settings component by accident

--- client/app/editor/settings.component.ts
+++ client/app/editor/settings.component.ts
@@ -1,9 +1,31 @@
-
 import {Component}                      from 'angular2/core';
+
+import {Project}        from './project'
+import {ProjectService} from './project.service'
+
 
 @Component({
     templateUrl: 'app/editor/templates/settings.html'
 })
 export class SettingsComponent {
+    /**
+     * The currently edited project
+     */
+    public project : Project;
 
+    /**
+     * Used for dependency injection.
+     */
+    constructor(
+        private _projectService: ProjectService
+    ) {
+    }
+
+    /**
+     * Load the project to access the schema
+     */
+    ngOnInit() {        
+        this._projectService.ActiveProject
+            .subscribe(res => this.project = res);
+    }
 }
