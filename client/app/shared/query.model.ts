0e87e34bfeae506866bbf23f66245bded34ffff9
Marcus Riemer <Marcus@GurXite.de>
2016-04-03 15:12:21 +0200

    Splitted up model and syntaxtree in different files

---
+++ client/app/shared/query.model.ts
@@ -0,0 +1,110 @@
+import {Table}                          from './table'
+
+/**
+ * Maps the JSON structure that is used to represent the data
+ * over the wire or on disk.
+ */
+export module Model {
+    /**
+     * Valid string values for data types
+     */
+    export type DataTypeStrings = "INTEGER" | "REAL" | "TEXT";
+    
+    /**
+     * One "typical" logical leaf of an expression tree, postpones
+     * the actual value lookup to execution time and ends recursion.
+     */
+    export interface SingleColumnExpression {
+        column : string
+        table? : string
+        alias? : string
+    }
+
+    /**
+     * The other "typical" leaf, a compile time expression that also
+     * ends recursion.
+     */
+    export interface ConstantExpression {
+        type : DataTypeStrings
+        value : string
+    }
+
+    /**
+     * Combines two expressions with a binary operator.
+     */
+    export interface BinaryExpression {
+        lhs : Expression
+        operator : string
+        rhs : Expression
+        simple : boolean
+    }
+
+    /**
+     * Denotes an expression that is intentionally missing.
+     */
+    export interface MissingExpression {
+
+    }
+
+    /**
+     * We use a single base type for all kinds of expression, as
+     * this vastly simplifies the storage process. Each kind of
+     * concrete expression is stored under a key. Only one of these
+     * keys may be set at runtime.
+     */
+    export interface Expression {
+        singleColumn? : SingleColumnExpression
+        binary? : BinaryExpression
+        constant? : ConstantExpression
+        missing? : MissingExpression
+    }
+
+    export interface Select {
+        columns : SelectColumn[],
+        allData? : boolean
+    }
+
+    export interface SelectColumn {
+        expr : Expression,
+        as? : string
+    }
+
+    /**
+     * Named tables as described in the FROM
+     */
+    export interface TableNameDefinition {
+        name : string,
+        alias? : string
+    }
+
+    export interface From {
+        first : TableNameDefinition,
+        joins? : Join[]
+    }
+
+    export interface Join {
+        table : TableNameDefinition,
+        cross? : string,
+        inner? : {
+            using? : string,
+            on? : Expression
+        }
+    }
+
+    export interface Where {
+        first : Expression
+    }
+
+    /**
+     * Outermost description of a query. This contains
+     * the whole structure and some identifying properties.
+     */
+    export interface Query {
+        select : Select,
+        from : From,
+        where? : Where,
+        name : string,
+        id : string
+    }
+}
+
