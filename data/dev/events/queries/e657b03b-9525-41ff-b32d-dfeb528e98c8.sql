2218f0385c94039906e5177d885ef20b5dd9cc0b
Marcus Riemer <Marcus@GurXite.de>
2016-04-27 20:59:29 +0200

    Latest pretty printed version of events project

--- data/dev/events/queries/e657b03b-9525-41ff-b32d-dfeb528e98c8.sql
+++ data/dev/events/queries/e657b03b-9525-41ff-b32d-dfeb528e98c8.sql
@@ -1,4 +1,4 @@
-SELECT person.nachname, ereignis.bezeichnung, ereignis.beginn_jahr
+SELECT person.nachname, ereignis.bezeichnung, ereignis.beginn_jahr, ereignis.ende_jahr
 FROM person
 	JOIN ereignis
 WHERE person.geb_jahr <= ereignis.beginn_jahr
