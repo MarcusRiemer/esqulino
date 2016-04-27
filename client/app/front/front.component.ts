f4cd8fd2d22fd9613f95318b49fc7a2f6c25d929
Marcus Riemer <Marcus@GurXite.de>
2016-04-27 19:18:41 +0200

    Bumped Angular 2 to Beta 16

--- client/app/front/front.component.ts
+++ client/app/front/front.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit}              from 'angular2/core';
 import {HTTP_PROVIDERS}                 from 'angular2/http';
 import {CORE_DIRECTIVES}                from 'angular2/common';
-import {RouteConfig, ROUTER_DIRECTIVES, Location} from 'angular2/router';
+import {
+    RouteConfig, ROUTER_DIRECTIVES
+} from 'angular2/router';
 
 import {ProjectDescriptionService} from '../shared/project.description.service';
 import {ProjectListComponent}      from './project.list.component';
@@ -17,19 +19,4 @@ import {AboutComponent}            from './about.component';
     { path: '/project', name: "ProjectList", component: ProjectListComponent},
 ])
 export class FrontComponent {
-
-    constructor(private _location: Location) { }
-
-    /**
-     * Tests whether a certain path is currently visited.
-     */
-    isCurrentlyVisited(path : string) {
-        if(path === this._location.path()){
-            return true;
-        }
-        else if(path.length > 0){
-            return this._location.path().indexOf(path) > -1;
-        }
-    }
-    
 }
