6b73dc9e0fb972ce44d982288f2f8d627a59c67f
Marcus Riemer <Marcus@GurXite.de>
2016-05-01 19:13:39 +0200

    Added validation to UI

---
+++ client/app/editor/query/validator.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input}               from 'angular2/core'
+
+import {Query}                          from '../../shared/query'
+import {ValidationResult}               from '../../shared/query.validation'
+
+@Component({
+    templateUrl: 'app/editor/query/templates/validator.html',
+    selector : 'sql-validator'
+})
+export class ValidatorComponent {
+    /**
+     * The query to validate
+     */
+    @Input() query : Query;
+
+    get result() : ValidationResult {
+        return (this.query.validate());
+    }
+}
+           
