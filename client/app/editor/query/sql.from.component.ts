944dbf69b5fd8394cc2be316f7737f59a1ccc001
Marcus Riemer <Marcus@GurXite.de>
2016-05-10 17:42:57 +0200

    Restored code for FROM component, was accidentally deleted

--- client/app/editor/query/sql.from.component.ts
+++ client/app/editor/query/sql.from.component.ts
@@ -1,17 +1,52 @@
 import {Component, Input}               from '@angular/core'
 
+import {DragService, SqlDragEvent}      from './drag.service'
 import {ExpressionComponent}            from './sql.expr.component'
 
-import {QueryInsert}                    from '../../shared/query'
+import {QueryFrom}                      from '../../shared/query'
+import {Join}                           from '../../shared/syntaxtree/from'
 
 @Component({
     selector : 'sql-from',
     templateUrl : 'app/editor/query/templates/query-from.html',
 })
 export class FromComponent {
-    @Input() query : QueryInsert;
+    @Input() query : QueryFrom;
 
-    constructor() {
+    constructor(public dragService : DragService) {
 
+    }
+
+    onJoinDragStart(join : Join, evt : DragEvent) {
+        this.dragService.startTableDrag(join.toModel(), "from", evt, join);
+    }
+
+    /**
+     * Fired when something is being dragged over this target.
+     */
+    onBlueprintDrag(evt : DragEvent) {
+        // Indicates we can drop here
+        evt.preventDefault();
+    }
+
+    /**
+     * Fired when something is dropped onto this target.
+     */
+    onBlueprintJoinDrop(evt : DragEvent) {
+        // Make sure that no redirection to the data associated with
+        // the drop target occurs
+        evt.preventDefault();
+
+        // Grab the actual sql drag event
+        const sqlEvt = <SqlDragEvent> JSON.parse(evt.dataTransfer.getData('text/plain'));
+        this.query.from.addJoin(sqlEvt.join);
+    }
+
+    /**
+     * Read Only View Accessor
+     * @return True, if a drop target for a new join should be shown.
+     */
+    get showBlueprintJoinDropTarget() {
+        return (this.dragService.activeTable);
     }
 }
