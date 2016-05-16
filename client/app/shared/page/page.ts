08fbec8466095034c29e5e289f2e384324d3ccbd
Marcus Riemer <Marcus@GurXite.de>
2016-05-16 18:31:59 +0200

    Skeleton for page editor

--- client/app/shared/page/page.ts
+++ client/app/shared/page/page.ts
@@ -1,1 +1,19 @@
 import {PageDescription}           from './page.description'
+
+export class Page {
+    private _id : string;
+    private _name : string;
+    
+    constructor(desc : PageDescription) {
+        this._id = desc.id;
+        this._name = desc.name;
+    }
+
+    get name() {
+        return (this._name);
+    }
+
+    get id() {
+        return (this._id);
+    }
+}
