f2a5c86b6b9ec9381dbb376e56b925e4505414b4
Marcus Riemer <Marcus@GurXite.de>
2016-05-22 23:23:48 +0200

    Basic bundling process via systemjs builder

---
+++ dist/bundle.app.js
@@ -0,0 +1,12 @@
+var SystemBuilder = require('systemjs-builder');
+var builder = new SystemBuilder();
+
+builder.loadConfig('./systemjs.config.js')
+  .then(function(){
+	  return builder.bundle(
+          'app - libs-bundle.js', // build the app only, exclude everything already included in dependencies
+          'app-bundle.js');
+  })
+  .then(function(){
+	  console.log('Application bundles built successfully!');
+  });
