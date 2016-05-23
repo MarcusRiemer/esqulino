7704787523172018615957ccb4305ab08997f6f2
Marcus Riemer <Marcus@GurXite.de>
2016-05-23 10:06:15 +0200

    The dist Makefile now correctly depends on all relevant files

--- dist/bundle.app.js
+++ dist/bundle.app.js
@@ -4,7 +4,7 @@ var builder = new SystemBuilder();
 builder.loadConfig('./systemjs.config.js')
   .then(function(){
 	  return builder.bundle(
-          'app - libs-bundle.js', // build the app only, exclude everything already included in dependencies
+          'app - lib-bundle.js', // build the app only, exclude everything already included in dependencies
           'app-bundle.js');
   })
   .then(function(){
