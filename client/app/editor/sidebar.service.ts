1af8d52ce1f56f47e1633e63f446078aa19e7a49
Marcus Riemer <Marcus@GurXite.de>
2016-06-01 16:56:46 +0200

    Sidebar now loaded without template

--- client/app/editor/sidebar.service.ts
+++ client/app/editor/sidebar.service.ts
@@ -1,47 +1,77 @@
-import {Injectable, ApplicationRef} from '@angular/core'
-import {Router}                     from '@angular/router'
+import {Injectable, Type}            from '@angular/core'
 
-import {Observable}                 from 'rxjs/Observable'
-import {BehaviorSubject}            from 'rxjs/BehaviorSubject'
+import {BehaviorSubject}             from 'rxjs/BehaviorSubject'
+import {Observable}                  from 'rxjs/Observable'
 
+
+import * as Query from './query/sidebar.component'
+import * as Page  from './page/sidebar.component'
+/**
+ * Manages the global state of the sidebar.
+ */
 @Injectable()
 export class SidebarService {
-    private _sidebar : string;
+    private _sidebar : BehaviorSubject<string>;
 
-    constructor(
-        private _router : Router,
-        private _appRef : ApplicationRef
-    ) {
-        // Usual case: There is no sidebar
-        this._sidebar = undefined; //new BehaviorSubject<string>(undefined);
+    /**
+     * Valid types for sidebars.
+     * TODO: Allow different sidebars to somehow register themself.
+     */
+    private _knownTypes : { [typeName:string] : Type} = {
+        "page" : Page.SidebarComponent,
+        "query" : Query.SidebarComponent
+    };
 
-        // Hide the sidebar on every URL change
-        // this._router.changes.subscribe(() => this.hideSidebar());
-    }
-
-    hideSidebar() {
-        this._sidebar = undefined;
-        //this._appRef.tick();
-    }
-
-    showSidebar(newType : string) {
-        if (["page", "query"].indexOf(newType) < 0) {
-            throw new Error(`Unknown sidebar type: ${newType}`);
-        }
-        
-        this._sidebar = newType;
-
-        // this._appRef.tick();
+    constructor() {
+        this._sidebar = new BehaviorSubject<string>(undefined);
     }
 
     /**
-     * @return True, if the sidebar should be visible
+     * Hides the currently shown sidebar.
      */
-    get isSidebarVisible() : boolean {
-        return (!!this._sidebar);
+    hideSidebar() {
+        this._sidebar.next(undefined);
     }
 
-    get sidebarType() : string {
+    /**
+     * @return True, if the given type could be shown by the sidebar.
+     */
+    isValidType(newType : string) : boolean {
+        return (!!this._knownTypes[newType]);
+    }
+
+    /**
+     * @return The component type for the given identifier.
+     */
+    getComponentType(newType : string) : Type {
+        return (this._knownTypes[newType]);
+    }
+
+    /**
+     * Triggers showing a different sidebar.
+     * 
+     * @newType The new type of sidebar to show.
+     */
+    showSidebar(newType : string) {
+        if (!this.isValidType(newType)) {
+            throw new Error(`Unknown sidebar type: ${newType}`);
+        }
+
+        this._sidebar.next(newType);
+    }
+
+    /**
+     * @return An observable that raises events when
+     *         the visibilit changes.
+     */
+    get isSidebarVisible() : Observable<boolean> {
+        return (this._sidebar.map(s => !!s));
+    }
+
+    /**
+     * @return An observable for the current type of the sidebar
+     */
+    get sidebarType() : Observable<string> {
         return (this._sidebar);
     }
 }
