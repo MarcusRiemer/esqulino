70d82a21aef3674234118fc02cd4b1cfd569b637
Marcus Riemer <Marcus@GurXite.de>
2016-06-29 15:48:44 +0200

    Fixed: CreateComponent would not navigate to created query after server response

--- client/app/editor/query/create.component.ts
+++ client/app/editor/query/create.component.ts
@@ -59,7 +59,7 @@ export class QueryCreateComponent implements OnInit, OnDestroy {
 
                 res.subscribe( query => {
                     console.log(`New query ${query.id}`);
-                    this._router.navigate(["editor", this._project.id ,"query", query.id]);
+                    this._router.navigate(["/editor", this._project.id ,"query", query.id]);
                 });
             }
         });
