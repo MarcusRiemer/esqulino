baad3e82727ea57321ac108df47667e00ade3b77
Marcus Riemer <Marcus@GurXite.de>
2016-07-19 22:20:52 +0200

    Now using subdomains to route enduser page views

--- client/app/front/project-list-item.component.ts
+++ client/app/front/project-list-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input}  from '@angular/core'
+import {Component, Input}          from '@angular/core'
 import {ROUTER_DIRECTIVES}         from '@angular/router'
 
 import {ProjectDescription}        from '../shared/project.description'
@@ -12,23 +12,41 @@ import {ProjectDescriptionService} from '../shared/project.description.service'
     templateUrl: 'app/front/templates/project-list-item.html',
     directives: [ROUTER_DIRECTIVES]
 })
-export class ProjectListItemComponent implements OnInit {
+export class ProjectListItemComponent {
     @Input() project : ProjectDescription;
 
-    public hasImage = false;
+    useSobdomain = true;
 
     /**
      * @return The image URL of this project
      */
-    public get imageUrl() : string {
+    get imageUrl() : string {
         return (`/api/project/${this.project.id}/preview`);
     }
 
     /**
-     * Check whether there should be an image.
+     * @return True, if this item has an image
      */
-    ngOnInit() {
-        this.hasImage = !!this.project.preview;
+    get hasImage() : boolean {
+        return (this.project && !!this.project.preview);
+    }
+
+    /**
+     * @return The currently visited hostname
+     */
+    get hostname() : string {
+        return (window.location.host);
+    }
+
+    /**
+     * @return A URL that allows to view this page as a "normal" user.
+     */
+    get viewUrl() : string {
+        if (this.useSobdomain) {
+            return (`//${this.project.id}.${this.hostname}`);
+        } else {
+            return (`/view/${this.project.id}/`)
+        }
     }
 }
 
