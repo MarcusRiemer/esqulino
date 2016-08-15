650be93b042c7b5b45cd21350d627602fd721c42
Marcus Riemer <Marcus@GurXite.de>
2016-08-15 15:30:40 +0200

    Page Tree Editor: Reworked drop targets and allowed columns
    * Opening and closing node are now treated distinct
    * Columns may now be dragged on rows
    * Fixed stupid parenting bug when creating columns (parent was always the page, never the row)

--- client/app/editor/page/tree/widget-node.component.ts
+++ client/app/editor/page/tree/widget-node.component.ts
@@ -9,6 +9,10 @@ import {SidebarService}                   from '../../sidebar.service'
 import {DragService, PageDragEvent}       from '../drag.service'
 import {WidgetComponent}                  from '../widget.component'
 
+// Specifies whether an operation has happened on the opening or
+// closing part of the node
+type NodeLocation = "open" | "close";
+
 /**
  * Represents a widget as a node in a tree.
  */
@@ -26,19 +30,29 @@ export class WidgetNode extends WidgetComponent<Widget> {
     }
 
     /**
-     * @return True, if this drag event should be accepted.
+     * @return A widget host that would accept this widget, or `undefined` if there 
+     *         is no accepting host available.
      */
     private acceptsDrag(widget : Widget | WidgetHost, pageEvt : PageDragEvent) : WidgetHost {
+        // Don't accept anything that isn't a widget
         if (!pageEvt.widget) {
             return (undefined);
-        } if (isWidgetHost(widget) &&
-              widget.acceptsWidget(pageEvt.widget)) {
+        }
+        // Is this an accepting widget host?
+        else if (isWidgetHost(widget) &&
+                 widget.acceptsWidget(pageEvt.widget)) {
+            // Then it can handle the drag itself
             return (widget);
-        } else if (isWidget(widget) &&
-                   isWidgetHost(widget.parent) &&
-                   widget.parent.acceptsWidget(pageEvt.widget)) {
+        }
+        // Is the parent of this thing an accepting widget host?
+        else if (isWidget(widget) &&
+                 isWidgetHost(widget.parent) &&
+                 widget.parent.acceptsWidget(pageEvt.widget)) {
+            // The parent can handle the drag
             return (widget.parent);
-        } else {
+        }
+        // No evidence found that we should accept this
+        else {
             return (undefined);
         }
     }
@@ -46,12 +60,11 @@ export class WidgetNode extends WidgetComponent<Widget> {
     /**
      * Giving feedback about ongoing drag operations.
      */
-    onDragOver(evt : DragEvent, index : number) {
+    onDragOverNode(evt : DragEvent, place : NodeLocation) {
         const pageEvt = <PageDragEvent> JSON.parse(evt.dataTransfer.getData('text/plain'));
 
         // Is this a meaningful child for this node or its parent?
-        if (this.acceptsDrag(this.model, pageEvt) ||
-            this.acceptsDrag(this.model.parent, pageEvt)) {
+        if (this.acceptsDrag(this.model, pageEvt)) {
             evt.preventDefault();
         }
     }
@@ -59,22 +72,40 @@ export class WidgetNode extends WidgetComponent<Widget> {
     /**
      * Something has been dropped.
      */
-    onDrop(evt : DragEvent, index : number) {
+    onDropNode(evt : DragEvent, place : NodeLocation) {
         evt.preventDefault();
-        
+
         const pageEvt = <PageDragEvent> JSON.parse(evt.dataTransfer.getData('text/plain'));
-        const host = this.acceptsDrag(this.model, pageEvt);
+        let host = this.acceptsDrag(this.model, pageEvt);
+        
         if (host) {
-            // If we are adding at the parent, change the index to our
-            // own index.
-            if (host == this.model.parent) {
+            // True, if this node will be hosting the new node itself
+            const parentHost = host == this.model.parent;
+            const selfHost = !parentHost;
+            
+            let index : number = undefined;
+
+            if (parentHost && place === "open") {
+                // No placement, the opening node is meant for children
+                index = undefined;
+            } else if (parentHost && place === "close") {
+                // Place after this element
                 index = this.model.parent.children.indexOf(this.model) + 1;
+            } else if (selfHost && place === "open") {
+                // Insert as new first element
+                index = 0;
+            } else if (selfHost && place === "close" && this.model.parent.acceptsWidget(pageEvt.widget)) {
+                // Insert after self, going up to the parent
+                host = this.model.parent;
+                index =  this.model.parent.children.indexOf(this.model) + 1;
             }
             
-            host.addWidget(pageEvt.widget, index);
+            if (index != undefined) {
+                host.addWidget(pageEvt.widget, index);
 
-            if (this._dragService.currentDrag.callbacks.onWidget) {
-                this._dragService.currentDrag.callbacks.onWidget(host);
+                if (this._dragService.currentDrag.callbacks.onWidget) {
+                    this._dragService.currentDrag.callbacks.onWidget(host);
+                }
             }
         } else {
             throw new Error("No valid drop target");
@@ -115,7 +146,7 @@ export class WidgetNode extends WidgetComponent<Widget> {
      *         be rendered.
      */
     get needsClosingNode() : boolean {
-        return (this.children.length > 0 || (!!(this.model as any).text));
+        return (true);
     }
 
     /**
