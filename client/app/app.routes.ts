3bfdb861a79638ca23032abf7400c8ccf3cf3808
Marcus Riemer <Marcus@GurXite.de>
2016-08-19 20:55:23 +0200

    Page: Frontpage navigation is now absolute

--- client/app/app.routes.ts
+++ client/app/app.routes.ts
@@ -6,24 +6,19 @@ import {frontRoutes}                    from './front/front.routes'
 const routes : Routes = [
     {
         path: '',
-        children: [
-            {
-                path: '',
-                redirectTo: '/about',
-                pathMatch: 'full',
-            },
-            {
-                path: 'editor/:projectId',
-                children: [...editorRoutes]
-                //loadChildren: '/app/editor/editor.module'
-            },
-            {
-                path: 'about',
-                children: [...frontRoutes]
-                //loadChildren: '/app/front/front.module'
-            }
-        ]
+        redirectTo: '/about',
+        pathMatch: 'full',
     },
+    {
+        path: 'editor/:projectId',
+        children: [...editorRoutes]
+        //loadChildren: '/app/editor/editor.module'
+    },
+    {
+        path: 'about',
+        children: [...frontRoutes]
+        //loadChildren: '/app/front/front.module'
+    }
 ]
 
 export const routing = RouterModule.forRoot(routes);
