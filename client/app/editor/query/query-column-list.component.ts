7977b6daa77dc8c5fe5ae4bb09761f310bd646c1
Marcus Riemer <Marcus@GurXite.de>
2016-08-26 13:27:16 +0200

    Page Tree Editor: Various bugfixes for empty pages
    * Query Table can now be displayed without any existing query

--- client/app/editor/query/query-column-list.component.ts
+++ client/app/editor/query/query-column-list.component.ts
@@ -1,5 +1,6 @@
 import {
-    Component, Input, Output, OnInit, EventEmitter
+    Component, Input, Output, OnInit, EventEmitter,
+    ChangeDetectionStrategy, ChangeDetectorRef
 } from '@angular/core'
 
 import {DragService}                      from './drag.service'
@@ -44,7 +45,8 @@ export class QueryColumnListComponent implements OnInit {
     private _currentHoverIndex : number = undefined;
 
     constructor(
-        private _dragService : DragService
+        private _dragService : DragService,
+        private _cdRef : ChangeDetectorRef
     ) {
     }
 
@@ -72,12 +74,14 @@ export class QueryColumnListComponent implements OnInit {
             index : index
         };
         this._currentHoverIndex = index;
+        this._cdRef.detach();
 
         // And make sure it is forgotten once the drag has ended.
         evt.target.addEventListener("dragend", () => {
             console.log(`Stop: Reordering column: ${dragData}`);
             this._currentDrag = undefined;
             this._currentHoverIndex = undefined;
+            this._cdRef.reattach();
         });
 
         console.log(`Start: Reordering column: ${dragData}`);
@@ -90,24 +94,92 @@ export class QueryColumnListComponent implements OnInit {
         return (col.fullName);
     }
 
+    allowDrag(evt : DragEvent) {
+        evt.preventDefault();
+    }
+
     /**
      * Used to update the hovering index.
      */
     onColumnDragOver(evt : DragEvent, index : number) {
-        evt.preventDefault();
-        this._currentHoverIndex = index;
+        /*this._currentHoverIndex = index;
+        evt.preventDefault();*/
+
+        // TODO: The code below shows how it should be done, but
+        //       it seems that angular keeps recreating the DOM
+        //       which happens faster then the code below can keep
+        //       up with setting data and firing preventDefault().
+        //
+        //       If that is the case, it's a hell of a
+        //       race condition ...
+        
+        const dragData = JSON.parse(evt.dataTransfer.getData('text/plain'));
+        if (dragData.reorder || dragData.columnRef) {
+            evt.preventDefault();
+
+            const oldIndex = this._currentHoverIndex;
+            
+            if (oldIndex != index) {
+                
+                //this._cdRef.detectChanges();
+                this._currentHoverIndex = index;
+
+                console.log(`${oldIndex} -> ${index}`);
+                
+            }
+        } else {
+            console.log(`Invalid Drag: Reordering column: ${dragData}`);
+        }
     }
 
-    onColumnDrop(evt : DragEvent, new_index : number) {
-        // Prevent that the browser triggers any kind of navigation
-        evt.preventDefault();
-        
-        // Move the item in the names array
-        // http://stackoverflow.com/questions/5306680/#5306832
-        const old_index = this._currentDrag.index;
-        this.columnNames.splice(new_index + 1, 0, this.columnNames.splice(old_index, 1)[0]);
+    /**
+     * Something was dropped on a column.
+     */
+    onColumnDrop(evt : DragEvent, newIndex : number) {
+        const dragData = JSON.parse(evt.dataTransfer.getData('text/plain'));
 
-        this.columnNamesChange.emit(this.columnNames);
+        console.log(`Drop: Reordering column: ${dragData}`);
+
+        if (dragData.reorder || dragData.columnRef) {
+            
+            // Prevent that the browser triggers any kind of navigation
+            evt.preventDefault();
+
+            if (dragData.reorder) {
+                // Move the item in the names array
+                // http://stackoverflow.com/questions/5306680/#5306832
+                const oldIndex = this._currentDrag.index;
+                console.log(`Reorder Drop: ${oldIndex} -> ${newIndex}`);
+
+                if (oldIndex < newIndex) {
+                    newIndex -= 1;
+                }
+
+                this.columnNames.splice(newIndex + 1, 0, this.columnNames.splice(oldIndex, 1)[0]);
+            }
+
+            this.columnNamesChange.emit(this.columnNames);
+        } else {
+            throw new Error(`Invalid Drop: Reordering column: ${dragData}`);
+        }
+    }
+
+    getColumnIndex(index : number, col : ResultColumn) : number {
+        if (this._currentDrag === undefined || this._currentHoverIndex === undefined) {
+            // NOP if no dragging happens
+            return (index);
+        } else if (col.fullName === this._currentDrag.col.fullName) {
+            // The current hover index if this ist the element that is curently
+            // being dragged.
+            return (this._currentHoverIndex);
+        } else if (index >= this._currentHoverIndex) {
+            // Something was pulled before this element
+            return (index + 1);
+        } else {
+            // Something is being dragged after this element, but that
+            // doesn't affect this element at all.
+            return (index);
+        }
     }
 
     /**
