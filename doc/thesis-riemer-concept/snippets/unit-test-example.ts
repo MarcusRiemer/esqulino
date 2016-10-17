b8646f8693f8fd0fd65a39a16d2df643f456c8f5
Marcus Riemer <Marcus@GurXite.de>
2016-10-17 22:39:28 +0200

    Thesis: Description of e2e-Tests and ideas for the UI editor

--- doc/thesis-riemer-concept/snippets/unit-test-example.ts
+++ doc/thesis-riemer-concept/snippets/unit-test-example.ts
@@ -7,33 +7,14 @@ const model : Model.QueryDescription = {
     name : 'where-simple',
     id : 'where-1',
     apiVersion : CURRENT_API_VERSION,
-    select : {
-        columns : [{ expr : { star : { } } }]
-    },
-    from : {
-        first : {
-            name : "person"
-        }
-    },
-    where : {
-        first : {
-            constant : { value : "1" }
-        }
-    }
+    select : { columns : [{ expr : { star : { } } }] },
+    from : { first : { name : "person" } },
+    where : { first : { constant : { value : "1" } } }
 };
-
 let q = new QuerySelect(schema, model);
 
-// Double checking inherited properties
-expect(q.name).toEqual('where-simple');
-expect(q.id).toEqual('where-1');
-
 // SELECT, star needs to be expanded according to the model
-const columns = q.select.actualColums;
-expect(columns.length).toEqual(3);
-expect(columns[0].fullName).toEqual('person.personId');
-expect(columns[1].fullName).toEqual('person.name');
-expect(columns[2].fullName).toEqual('person.gebDat');
+expect(q.select.actualColums).toEqual(3);
 
 // FROM, has no joins at all
 expect(q.from.numberOfJoins).toEqual(0);
