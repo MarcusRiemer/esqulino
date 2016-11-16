1da3e6ecafc9297a79a2fd3d3cacd5b31878fd83
Marcus Riemer <Marcus@GurXite.de>
2016-11-16 15:37:28 +0100

    Client: Now tells the user about errors happening during project enumeration

--- client/app/shared/project.description.service.ts
+++ client/app/shared/project.description.service.ts
@@ -8,6 +8,7 @@ import {Http, Response}     from '@angular/http';
 
 import {ServerApiService}   from './serverapi.service'
 import {ProjectDescription} from './project.description'
+import {FlashService}       from './flash.service'
 
 /**
  * Wraps access to project descriptions.
@@ -25,9 +26,9 @@ export class ProjectDescriptionService {
     /**
      * @param _http Dependently injected
      */
-    constructor(
-        private _http : Http,
-        private _serverApi : ServerApiService) {
+    constructor(private _http : Http,
+                private _serverApi : ServerApiService,
+                private _flashService : FlashService) {
         this._cache = new BehaviorSubject<ProjectDescription[]>([]);
     }
 
@@ -43,9 +44,19 @@ export class ProjectDescriptionService {
      * Fetch a new set of projects and also place them in the cache.
      */
     fetchProjects() : Observable<ProjectDescription[]> {
+        // Ask the server for available projects
         const uri = this._serverApi.getProjectListUrl();
         this._httpRequest = this._http.get(uri)
-            .map(res => <ProjectDescription[]> res.json());
+            .map(res => <ProjectDescription[]> res.json())
+            .catch(err => {
+                this._flashService.addMessage({
+                    caption: "Fehler beim Laden der Projekte: ",
+                    text: err.toString(),
+                    type: "danger"
+                });
+
+                return ([]);
+            });
 
         this._httpRequest.subscribe(projects => {
             this._cache.next(projects)
