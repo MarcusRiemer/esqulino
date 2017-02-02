8a66bee9e4f51bb6d6e9449947fbee97d8c3f7c1
Marcus Riemer <Marcus@GurXite.de>
2017-02-02 16:39:50 +0100

    Data: Included type information in Pokémon-Database

--- data/dev/projects/pokemongo/queries/2fae109d-a121-4a54-bbc2-38c295317e35.sql
+++ data/dev/projects/pokemongo/queries/2fae109d-a121-4a54-bbc2-38c295317e35.sql
@@ -1,4 +1,6 @@
-SELECT gefangen.gefangen_id, pokedex.nummer, pokedex.name, pokedex.typ, gefangen.spitzname, gefangen.staerke
+SELECT gefangen.gefangen_id, pokedex.nummer, pokedex.name, gefangen.spitzname, gefangen.staerke, typ.typ_name
 FROM gefangen
 	JOIN pokedex
+	JOIN typ
 WHERE gefangen.pokedex_nummer = pokedex.nummer
+	AND pokedex.typ1 = typ.typ_id
