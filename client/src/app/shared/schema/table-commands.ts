3e34f8de10ecf4e2da1c72eef449cd562ab4a899
Marco Pawlowski <mpa@fh-wedel.de>
2017-02-14 22:15:12 +0100

    Client: Table commands toModel functions added

--- client/src/app/shared/schema/table-commands.ts
+++ client/src/app/shared/schema/table-commands.ts
@@ -9,6 +9,55 @@ import { Column, ColumnStatus }                 from './column'
  * on the server. 
  */
 
+export interface CommandDescription {
+  type : string;
+  columnIndex : number;
+}
+
+export interface AddColumnDescription extends CommandDescription {
+  type : "addColumn";
+}
+
+export interface DeleteColumnDescription extends CommandDescription {
+  type : "deleteColumn";
+}
+
+export interface SwitchColumnDescription extends CommandDescription {
+  type : "switchColumn";
+  to : number;
+}
+
+export interface RenameColumnDescription extends CommandDescription {
+  type : "renameColumn";
+  oldName : string;
+  newName : string;
+}
+
+export interface ChangeColumnTypeDescription extends CommandDescription {
+  type : "changeColumnType";
+  oldType : string;
+  newType : string;
+}
+
+export interface ChangeColumnPKDescription extends CommandDescription {
+  type : "changeColumnPK";
+}
+
+export interface ChangeColumnNNDescription extends CommandDescription {
+  type : "changeColumnNN";
+}
+
+export interface ChangeColumnStandartValueDescription extends CommandDescription {
+  type : "changeColumnStandartValue";
+  oldValue : string;
+  newValue : string;
+}
+
+export interface ChangeTableNameDescription extends CommandDescription {
+  type : "renameTable";
+  oldName : string;
+  newName : string;
+}
 
 /**
  * abstract class for all table commands
@@ -39,6 +88,8 @@ export abstract class TableCommand {
   undo(table: Table): void {
     table.columns[this._columnIndex].state = this._lastStatus;
   };
+
+  abstract toModel() : CommandDescription;
 }
 
 /**
@@ -53,6 +104,13 @@ export class AddNewColumn extends TableCommand {
   undo(table: Table): void {
     table.columns.splice(-1, 1);
   }
+
+  toModel() : AddColumnDescription {
+    return {
+      type : "addColumn",
+      columnIndex : this._columnIndex
+    };
+  }
 }
 
 /**
@@ -62,7 +120,7 @@ export class DeleteColumn extends TableCommand {
 
   constructor(table : Table, columnIndex: number) {
     super(table.columns[columnIndex].state, columnIndex);
-    this._columnIndex = columnIndex;
+    
   }
 
   do(table: Table): void {
@@ -72,33 +130,46 @@ export class DeleteColumn extends TableCommand {
   undo(table: Table): void {
     super.undo(table);
   }
+
+  toModel() : DeleteColumnDescription {
+    return {
+      type : "deleteColumn",
+      columnIndex : this._columnIndex
+    };
+  }
 }
 
 /**
  * Class for switching the place of a column
  */
 export class SwitchColumnOrder extends TableCommand {
-  private _from: number;
   private _to: number;
 
   constructor(table : Table, from: number, to: number) {
     super(table.columns[from].state, from);
-    this._from = from;
     this._to = to;
   }
 
   do(table: Table): void {
     super.do(table);
-    this.moveColumn(table.columns, this._from, this._to);
+    this.moveColumn(table.columns, this._columnIndex, this._to);
   }
 
   undo(table: Table): void {
-    this.moveColumn(table.columns, this._to, this._from);
+    this.moveColumn(table.columns, this._to, this._columnIndex);
     super.undo(table);
   }
 
   private moveColumn(columns: Column[], from: number, to: number) {
     columns.splice(to, 0, columns.splice(from, 1)[0]);
+  }
+
+  toModel() : SwitchColumnDescription {
+    return {
+      type : "switchColumn",
+      columnIndex : this._columnIndex,
+      to : this._to
+    };
   }
 }
 
@@ -111,7 +182,6 @@ export class RenameColumn extends TableCommand {
 
   constructor(table : Table, columnIndex: number, oldName: string, newName: string) {
     super(table.columns[columnIndex].state, columnIndex);
-    this._columnIndex = columnIndex;
     this._newName = newName;
     this._oldName = oldName
   }
@@ -125,6 +195,15 @@ export class RenameColumn extends TableCommand {
     table.columns[this._columnIndex].name = this._oldName;
     super.undo(table);
   }
+
+  toModel() : RenameColumnDescription {
+    return{
+      type : "renameColumn",
+      columnIndex : this._columnIndex,
+      newName : this._newName,
+      oldName : this._oldName
+    };
+  }
 }
 
 /**
@@ -136,7 +215,6 @@ export class ChangeColumnType extends TableCommand {
 
   constructor(table : Table, columnIndex: number, oldType: string, newType: string) {
     super(table.columns[columnIndex].state, columnIndex);
-    this._columnIndex = columnIndex;
     this._newType = newType;
     this._oldType = oldType;
   }
@@ -150,6 +228,15 @@ export class ChangeColumnType extends TableCommand {
     table.columns[this._columnIndex].type = this._oldType;
     super.undo(table);
   }
+
+  toModel() : ChangeColumnTypeDescription {
+    return {
+      type : "changeColumnType",
+      columnIndex : this._columnIndex,
+      newType : this._newType,
+      oldType : this._oldType
+    };
+  }
 }
 
 /**
@@ -159,7 +246,6 @@ export class ChangeColumnPK extends TableCommand {
 
   constructor(table : Table, columnIndex: number) {
     super(table.columns[columnIndex].state, columnIndex);
-    this._columnIndex = columnIndex;
   }
 
   do(table: Table): void {
@@ -170,6 +256,14 @@ export class ChangeColumnPK extends TableCommand {
   undo(table: Table): void {
     table.columns[this._columnIndex].primary = !table.columns[this._columnIndex].primary;
     super.undo(table);
+  }
+
+  toModel() : ChangeColumnPKDescription {
+
+    return{
+      type : "changeColumnPK",
+      columnIndex : this._columnIndex
+    };
   }
 }
 
@@ -190,6 +284,13 @@ export class ChangeColumnNN extends TableCommand {
   undo(table: Table): void {
     table.columns[this._columnIndex].not_null = !table.columns[this._columnIndex].not_null;
     super.undo(table);
+  }
+
+  toModel() : ChangeColumnNNDescription {
+    return {
+      type : "changeColumnNN",
+      columnIndex : this._columnIndex
+    }
   }
 }
 
@@ -215,6 +316,16 @@ export class ChangeColumnStandartValue extends TableCommand {
     table.columns[this._columnIndex].dflt_value = this._oldValue;
     super.undo(table);
   }
+
+  toModel() : ChangeColumnStandartValueDescription {
+
+    return{
+      type : "changeColumnStandartValue",
+      columnIndex : this._columnIndex,
+      newValue : this._newValue,
+      oldValue : this._oldValue
+    };
+  }
 }
 
 /**
@@ -236,6 +347,15 @@ export class ChangeTableName extends TableCommand {
 
   undo(table: Table): void {
     table.name = this._oldName;
+  }
+
+  toModel() : ChangeTableNameDescription {
+    return {
+      type : "renameTable",
+      columnIndex : this._columnIndex,
+      newName : this._newName,
+      oldName : this._oldName
+    };
   }
 }
 
