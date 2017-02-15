2d415d2a8cbd3a10407323dab7b133ba6f15e3e5
Marcus Riemer <Marcus@GurXite.de>
2017-02-15 13:24:02 +0100

    Server: Example database testcase

--- server/schema.test.rb
+++ server/schema.test.rb
@@ -1,34 +1,39 @@
-require "./schema.rb"
+require './schema.rb'
 
-require "test/unit"
+require 'tempfile'
+require 'test/unit'
 
 
 # Not strictly a unit test, as it requires the use of external files.
 # But for the moment this is what we will roll with, as this does
 # show how the SQLite API behaves.
 class Database < Test::Unit::TestCase
-  @@sqlite_test_path = "../data/dev/projects/test/databases/default.sqlite"
+  def test_created_schema
+    temp_db_file = Tempfile.new('test.sqlite')
+    db = SQLite3::Database.new(temp_db_file.path)
+    puts "Test #{temp_db_file.path}"
+    
+    db.execute <<-delim
+      create table students (
+        name TEXT,
+        email TEXT,  
+        grade INTEGER,
+        blog URL
+      );
+    delim
+    
+    db.close
 
-  # Ensures the database description is correct
-  def test_database_describe_schema
-    schema = database_describe_schema @@sqlite_test_path
-
-    assert_equal(1, schema.count)
-
-    table_key_value = schema[schema.index { |v| v.name == "key_value" }]
-
-    column_key = table_key_value[0]
-    assert_equal("key", column_key.name)
-    assert_equal("INTEGER", column_key.type)
-    assert_equal(true, column_key.not_null)
-    assert_nil(column_key.dflt_value)
-    assert_equal(true, column_key.primary)
-
-    column_value = table_key_value[1]
-    assert_equal("value", column_value.name)
-    assert_equal("TEXT", column_value.type)
-    assert_equal(true, column_value.not_null)
-    assert_nil(column_value.dflt_value)
-    assert_equal(false, column_value.primary)
+    schema = database_describe_schema(temp_db_file.path)
+    
+    assert_true schema[0].name == "students"
+    assert_true schema[0].columns[0].name == "name"
+    assert_true schema[0].columns[0].type == "TEXT"
+    assert_true schema[0].columns[1].name == "email"
+    assert_true schema[0].columns[1].type == "TEXT"
+    assert_true schema[0].columns[2].name == "grade"
+    assert_true schema[0].columns[2].type == "INTEGER"
+    assert_true schema[0].columns[3].name == "blog"
+    assert_true schema[0].columns[3].type == "URL"
   end
 end
