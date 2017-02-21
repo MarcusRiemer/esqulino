df54dd04b0723d4cfd9f73f800cd02bfd436e89d
Marco Pawlowski <mpa@fh-wedel.de>
2017-02-21 06:01:12 +0100

    Server: Included function to change databases and first tests for it

--- server/schema.test.rb
+++ server/schema.test.rb
@@ -1,4 +1,5 @@
 require './schema.rb'
+require './schema-alter.rb'
 
 require 'tempfile'
 require 'test/unit'
@@ -17,7 +18,7 @@ class Database < Test::Unit::TestCase
   def test_created_schema
     temp_db_file = Tempfile.new('test.sqlite')
     db = SQLite3::Database.new(temp_db_file.path)
-    puts "Test #{temp_db_file.path}"
+    #puts "Test #{temp_db_file.path}"
     
     db.execute <<-delim
       create table students (
@@ -38,4 +39,211 @@ class Database < Test::Unit::TestCase
     assert_column schema, 0, 2, 'grade', 'INTEGER'
     assert_column schema, 0, 3, 'blog', 'URL'
   end
+
+
+  def test_datatype_constraint
+    temp_db_file = Tempfile.new('test.sqlite')
+    db = SQLite3::Database.new(temp_db_file.path)
+    #puts "Test #{temp_db_file.path}"
+    
+    db.create_function('regexp', 2) do |func, pattern, expression|
+      unless expression.nil? #expression.to_s.empty?
+        func.result = expression.to_s.match(
+          Regexp.new(pattern.to_s, Regexp::IGNORECASE)) ? 1 : 0
+      else
+        # Return true if the value is null, let the DB handle this
+        func.result = 1
+      end   
+    end
+
+    db.execute <<-delim
+      create table constraint_check (
+        text TEXT,
+        boolean BOOLEAN CONSTRAINT "ERR: Value is not of type boolean" CHECK (boolean == 1 or boolean == 0),  
+        integer INTEGER CONSTRAINT "ERR: Value is not of type integer" CHECK (integer regexp '^[+-]?[0-9]+$'),
+        float FLOAT CONSTRAINT "ERR: Value is not of type float" CHECK (float regexp '^[+-]?([0-9]*\.[0-9]+$|[0-9]+$)')
+      );
+    delim
+
+    assert_nothing_raised do
+      db.execute("insert into constraint_check values (null, null, null, null)")
+    end
+
+    assert_raise SQLite3::ConstraintException  do
+      db.execute("insert into constraint_check values (null, null, '', null)")
+    end
+
+    assert_nothing_raised do
+      db.execute("insert into constraint_check values (null, 1, null, null)")
+    end
+
+    assert_nothing_raised do
+      db.execute("insert into constraint_check values (null, 0, null, null)")
+    end
+
+    assert_nothing_raised do
+      db.execute("insert into constraint_check values (null, null, -2, null)")
+    end
+
+    assert_nothing_raised do
+      db.execute("insert into constraint_check values (null, null, +3213, null)")
+    end
+
+    assert_nothing_raised do
+      db.execute("insert into constraint_check values (null, null, null, '-12.8')")
+    end
+
+    assert_nothing_raised do
+      db.execute("insert into constraint_check values (null, null, null, '18')")
+    end
+
+    assert_nothing_raised do
+      db.execute("insert into constraint_check values (null, null, null, '+12.7')")
+    end
+
+    assert_raise SQLite3::ConstraintException  do
+      db.execute("insert into constraint_check values (null, 2, null, null)")
+    end
+
+    assert_equal(db.errmsg(), "CHECK constraint failed: ERR: Value is not of type boolean")
+
+    assert_raise SQLite3::ConstraintException  do
+      db.execute("insert into constraint_check values (null, 'asd', null, null)")
+    end
+
+    assert_equal(db.errmsg(), "CHECK constraint failed: ERR: Value is not of type boolean")
+
+    assert_raise SQLite3::ConstraintException  do
+      db.execute("insert into constraint_check values (null, null, 'asd', null)")
+    end
+
+    assert_equal(db.errmsg(), "CHECK constraint failed: ERR: Value is not of type integer")
+
+    assert_raise SQLite3::ConstraintException  do
+      db.execute("insert into constraint_check values (null, null, '123a', null)")
+    end
+
+    assert_equal(db.errmsg(), "CHECK constraint failed: ERR: Value is not of type integer")
+
+    assert_raise SQLite3::ConstraintException  do
+      db.execute("insert into constraint_check values (null, null, 'a123', null)")
+    end
+
+    assert_equal(db.errmsg(), "CHECK constraint failed: ERR: Value is not of type integer")
+
+    assert_raise SQLite3::ConstraintException  do
+      db.execute("insert into constraint_check values (null, null, null, '-12.2g')")
+    end
+
+    assert_equal(db.errmsg(), "CHECK constraint failed: ERR: Value is not of type float")
+
+    db.close
+
+    schema = database_describe_schema(temp_db_file.path)
+    
+    assert_equal schema[0].name, "constraint_check"
+    assert_column schema, 0, 0, 'text', 'TEXT'
+    assert_column schema, 0, 1, 'boolean', 'BOOLEAN'
+    assert_column schema, 0, 2, 'integer', 'INTEGER'
+    assert_column schema, 0, 3, 'float', 'FLOAT'
+  end
+
+
+  def test_create_createStatement
+    temp_db_file = Tempfile.new('test.sqlite')
+    temp_db_file2 = Tempfile.new('test2.sqlite')
+    db = SQLite3::Database.new(temp_db_file.path)
+    db2 = SQLite3::Database.new(temp_db_file2.path)
+    #puts "Test #{temp_db_file.path}"
+
+    db2.create_function('regexp', 2) do |func, pattern, expression|
+      unless expression.nil? #expression.to_s.empty?
+        func.result = expression.to_s.match(
+          Regexp.new(pattern.to_s, Regexp::IGNORECASE)) ? 1 : 0
+      else
+        # Return true if the value is null, let the DB handle this
+        func.result = 1
+      end   
+    end
+    
+    create1 = String.new("create table students (name TEXT NOT NULL, email TEXT DEFAULT blah, grade INTEGER, blog URL, PRIMARY KEY(name, email));")
+    create2 = String.new("create table person (id TEXT NOT NULL, id2 TEXT ,FOREIGN KEY (id, id2) REFERENCES students(name, email));")
+
+    db.execute(create1)
+
+    db.execute(create2)
+    
+    schema = database_describe_schema(temp_db_file.path)
+    
+    db2.execute(table_to_create_statement(schema[0]))
+    db2.execute(table_to_create_statement(schema[1]))
+
+    # Write function to compare two table objects
+    #puts (schema[0].columns.map{|col| col.name}).join(',')
+    
+    db.close
+    db2.close
+  end
+
+  def test_change_database
+    temp_db_file = Tempfile.new('test.sqlite')
+    db = SQLite3::Database.new(temp_db_file.path)
+
+    db.create_function('regexp', 2) do |func, pattern, expression|
+      unless expression.nil? #expression.to_s.empty?
+        func.result = expression.to_s.match(
+          Regexp.new(pattern.to_s, Regexp::IGNORECASE)) ? 1 : 0
+      else
+        # Return true if the value is null, let the DB handle this
+        func.result = 1
+      end   
+    end
+    
+    create1 = String.new("create table students (name TEXT NOT NULL, email TEXT DEFAULT blah, grade INTEGER CONSTRAINT 'ERR: Value is not of type integer' CHECK (grade regexp '^[+-]?[0-9]+$'), blog URL, PRIMARY KEY(name, email));")
+    db.execute(create1)
+    db.execute("INSERT INTO students values('dataname', 'datamail', 5, 'blogurl');")
+    count = db.execute("Select count(*) from students;")
+    schema = database_describe_schema(temp_db_file.path)
+    assert_column schema, 0, 2, 'grade', 'INTEGER'
+    assert_equal([[1]], count)
+
+    table = schema[0]
+
+    # changing type of grade
+    table.columns[2].type = 'TEXT'
+
+    database_alter_schema(temp_db_file.path, table, false)
+
+    schema = database_describe_schema(temp_db_file.path)
+    assert_column schema, 0, 2, 'grade', 'TEXT'
+    assert_equal([[1]], count)
+    
+
+    # changing column order
+    table.columns.insert(3, table.columns.delete_at(2))
+
+    database_alter_schema(temp_db_file.path, table, true)
+
+    schema = database_describe_schema(temp_db_file.path)
+    assert_column schema, 0, 3, 'grade', 'TEXT'
+    assert_equal([[1]], count)
+
+
+    # changing type of blog to Integer so the values don't fit in
+    table.columns[2].type = 'INTEGER'
+
+    #there should be an error
+    assert_raise SQLite3::ConstraintException  do
+      database_alter_schema(temp_db_file.path, table, false)
+    end
+
+    schema = database_describe_schema(temp_db_file.path)
+    # the database didn't changed
+    assert_column schema, 0, 2, 'blog', 'URL'
+    assert_equal([[1]], count)
+
+
+    db.close
+  end
+
 end
