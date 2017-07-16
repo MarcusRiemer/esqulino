1351656abf76535fd3591095a1b13d22fb42e017
Marcus Riemer <Marcus@GurXite.de>
2017-07-16 21:56:13 +0200

    Server: Fixed wrong constants for mailings

--- server/app/mailers/project_mailer.rb
+++ server/app/mailers/project_mailer.rb
@@ -6,6 +6,6 @@ class ProjectMailer < ApplicationMailer
     @project_visitor_url = "http://#{project_id}.#{project_site_url}"
     @project_editor_url = "http://#{project_editor_url}/editor/#{project_id}"
     
-    mail(to: admin_mail, subject: "New Project: #{project_name} (#{project_id}))")
+    mail(to: admin_mail, subject: "New Project: #{project_name} (#{project_id})")
   end
 end
