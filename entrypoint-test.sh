f3f35af0362a72155ee5e26ae2f8a44a6edb712e
Marcus Riemer <Marcus@GurXite.de>
2017-07-17 17:44:34 +0200

    Docker: Introduced instructions for travis-ci

--- entrypoint-test.sh
+++ entrypoint-test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-git clone https://bitbucket.org/marcusriemer/esqulino.git --depth 1 .
-make install-deps dist
+export TEST_SINGLE_RUN=true
+
+make install-deps
 make -C server test
-TEST_SINGLE_RUN=true make -C client test
+make -C client test
