a1bdd3af7fd8469e469b9db554ecef2a75087b08
Marcus Riemer <Marcus@GurXite.de>
2017-08-16 20:08:45 +0200

    Client: Splitted HTML nodes into only two types

--- client/src/app/shared/syntaxtree/syntaxtree.ts
+++ client/src/app/shared/syntaxtree/syntaxtree.ts
@@ -18,12 +18,10 @@ type NodeChildren = { [childrenCategory: string]: Node[] }
  * enough information to build arbitrarily structured trees. There
  * are no compile-time checks to do any kind of validation, these
  * checks can only be made at runtime.
- *
- * All members of this description are prefixed with "node" to
- * minimise the chance of name clashes.
  */
-export abstract class Node {
+export class Node {
   private _nodeName: string
+  private _nodeFamily: string
   private _nodeProperties: NodeProperties
   private _nodeChildren: NodeChildren
 
@@ -31,23 +29,48 @@ export abstract class Node {
 
   /**
    * The constructor is responsible to transfer relevant description
-   * properties and does nothing else. Most importantly, it does not
-   * do any kind of validation
+   * properties and to construct any children.
    */
   constructor(desc: NodeDescription, parent: Node) {
     this._nodeName = desc.nodeName;
+    this._nodeFamily = desc.nodeFamily;
     this._nodeParent = parent;
 
-    // Make a deep copy of those properties, just in case ...
-    this._nodeProperties = JSON.parse(JSON.stringify(desc.nodeProperties));
+    // We don't want any undefined fields during runtime
+    this._nodeProperties = {}
+    this._nodeChildren = {}
+
+    // Load properties (if there are any)
+    if (desc.nodeProperties) {
+      // Make a deep copy of those properties, just in case ...
+      this._nodeProperties = JSON.parse(JSON.stringify(desc.nodeProperties));
+    }
+
+    // Load children (if there are any)
+    if (desc.nodeChildren) {
+      // Load all children in all categories
+      for (let categoryName in desc.nodeChildren) {
+        const category = desc.nodeChildren[categoryName];
+        this._nodeChildren[categoryName] = category.map(childDesc => new Node(childDesc, this))
+      }
+    }
   }
 
   get nodeName(): string {
     return (this._nodeName);
   }
 
-  get nodeChildren(): NodeChildren {
-    return (this._nodeChildren);
+  get nodeFamily(): string {
+    return (this._nodeFamily);
+  }
+
+  getChildren(categoryName: string): Node[] {
+    const result = this._nodeChildren[categoryName];
+    if (result) {
+      return (result);
+    } else {
+      return ([]);
+    }
   }
 
   get nodeProperties(): NodeProperties {
@@ -61,3 +84,4 @@ export abstract class Node {
     return (this._nodeParent);
   }
 }
+
