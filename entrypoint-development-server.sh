725f90abc6d3266b175252320b3c8de998c867e7
Ole Just <minf9724@fh-wedel.de>
2017-08-30 11:05:05 +0200

    DOCKER: don't run processes as root to enable editing of project files and deleting of node modules without sudo

--- entrypoint-development-server.sh
+++ entrypoint-development-server.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-cat /etc/hosts
+groupadd -g "$USER_GID" user
+
+useradd -m -u "$USER_UID" -g "$USER_GID" user
 
 export GEM_PATH=/srv/esqulino/server/vendor/bundle/ruby/2.4.0
 make -C server install-deps
-make -C server run-dev
+su user -c "make -C server run-dev"
