8a6026528254ec54493dd2045b5cfde4f91d5e82
Marcus Riemer <Marcus@GurXite.de>
2017-09-01 10:56:00 +0200

    Client: Started to implement Validator

--- client/src/app/shared/syntaxtree/validator.spec.ts
+++ client/src/app/shared/syntaxtree/validator.spec.ts
@@ -1,33 +1,97 @@
 import * as Schema from './validator.description'
+import { Validator } from './validator'
+
+/**
+ * Basically describes a language where each document needs to be
+ * <html>
+ *   <head></head>
+ *   <body></body>
+ * </html>
+ */
+const langMiniHtml: Schema.LanguageDescription = {
+  languageName: "mini-html",
+  types: [
+    {
+      nodeName: "html",
+      type: "complex",
+      chidlrenCategories: [
+        {
+          categoryName: "children",
+          children: {
+            type: "sequence",
+            nodeTypes: ["head", "body"]
+          }
+        }
+      ]
+    } as Schema.NodeComplexTypeDescription,
+    {
+      nodeName: "head",
+      type: "complex",
+    } as Schema.NodeComplexTypeDescription,
+    {
+      nodeName: "body",
+      type: "complex",
+    } as Schema.NodeComplexTypeDescription
+  ],
+  root: ["html"]
+};
+
+const langMiniSql: Schema.LanguageDescription = {
+  languageName: "mini-sql",
+  types: [
+    {
+      nodeName: "select",
+      type: "complex"
+    },
+    {
+      nodeName: "delete",
+      type: "complex"
+    },
+    {
+      nodeName: "from",
+      type: "complex"
+    },
+    {
+      nodeName: "where",
+      type: "complex"
+    },
+    {
+      nodeName: "query-select",
+      type: "complex",
+      chidlrenCategories: [
+        {
+          categoryName: "children",
+          children: {
+            type: "sequence",
+            nodeTypes: ["select", "from", "where"]
+          }
+        }
+      ]
+    } as Schema.NodeComplexTypeDescription,
+    {
+      nodeName: "query-delete",
+      type: "complex",
+      chidlrenCategories: [
+        {
+          categoryName: "children",
+          children: {
+            type: "sequence",
+            nodeTypes: ["delete", "from", "where"]
+          }
+        }
+      ]
+    } as Schema.NodeComplexTypeDescription,
+  ],
+  root: ["query-select", "query-delete"]
+}
 
 describe('Schema Validator', () => {
-  it('describes a very basic HTML variant', () => {
-    const schemaDesc: Schema.SchemaDescription = {
-      languageName: "mini-html",
-      types: [
-        {
-          nodeName: "html",
-          type: "complex",
-          chidlrenCategories: [
-            {
-              categoryName: "children",
-              children: {
-                type: "sequence",
-                nodeTypes: ["head", "body"]
-              }
-            }
-          ]
-        } as Schema.NodeComplexTypeDescription,
-        {
-          nodeName: "head",
-          type: "complex",
-        } as Schema.NodeComplexTypeDescription,
-        {
-          nodeName: "body",
-          type: "complex",
-        } as Schema.NodeComplexTypeDescription
-      ],
-      root: "html"
-    }
+  it('registers types in a single language', () => {
+    const v = new Validator([langMiniHtml]);
+
+    expect(v.isKnownLanguage(langMiniHtml.languageName)).toBeTruthy();
+    expect(v.isKnownType(langMiniHtml.languageName, langMiniHtml.types[0].nodeName)).toBeTruthy();
+    expect(v.isKnownType(langMiniHtml.languageName, langMiniHtml.types[1].nodeName)).toBeTruthy();
+    expect(v.isKnownType(langMiniHtml.languageName, langMiniHtml.types[2].nodeName)).toBeTruthy();
   });
 });
--- client/src/app/shared/syntaxtree/validator.ts
+++ client/src/app/shared/syntaxtree/validator.ts
@@ -1,4 +1,7 @@
 import * as Desc from './validator.description'
+import * as AST from './syntaxtree'
+
+export type ErrorCodes = "UNKNOWN_ROOT"
 
 export interface ValidationItem {
   type: "error" | "warning"
@@ -12,11 +15,20 @@ export class ValidationResult {
 
 }
 
+interface ValidationError {
+  code: string;
+  node: AST.Node;
+}
+
 /**
  * Used during validation to accumulate validation results.
  */
 class ValidationContext {
+  private _errors: ValidationError[] = [];
 
+  addError(code: ErrorCodes | string, node: AST.Node) {
+    this._errors.push({ code: code, node: node });
+  }
 }
 
 /**
@@ -25,34 +37,165 @@ class ValidationContext {
  */
 abstract class NodeType {
   private _familyName: string;
-  private _nodeName: string;
+  private _typeName: string;
 
-  constructor(typeDesc: Desc.NodeTypeDescription, familyName: string) {
-    this._nodeName = typeDesc.nodeName;
-    this._familyName = familyName;
+  constructor(typeDesc: Desc.NodeTypeDescription, language: string) {
+    this._typeName = typeDesc.nodeName;
+    this._familyName = language;
   }
-}
 
-class NodeComplexType extends NodeType {
-  constructor(typeDesc: Desc.NodeTypeDescription, familyName: string) {
-    super(typeDesc, familyName)
+  /**
+   * @return The name of the language this type belongs to.
+   */
+  get languageName() {
+    return (this._familyName);
   }
-}
 
-class NodeComplexTypeChildren {
+  /**
+   * @return The type of node this instance will validate.
+   */
+  get typeName() {
+    return (this._typeName);
+  }
 
-}
+  validate(ast: AST.Node, context: ValidationContext) {
+    if (this._familyName == ast.nodeFamily && this._typeName == ast.nodeName) {
+      context.addError("UNEXPECTED_NODE", ast);
+    } else {
+      this.validateImpl(ast, context);
+    }
+  }
 
-class NodeComplexTypeChildrenSequence {
-
+  abstract validateImpl(ast: AST.Node, context: ValidationContext);
 }
 
 /**
- * A schema consists of type definitions and
+ * Describes a complex node that may have any kind of children.
  */
-class Schema {
-  constructor(desc: Desc.SchemaDescription) {
+class NodeComplexType extends NodeType {
+  private _allowedChildren: NodeComplexTypeChildren[] = [];
 
+  constructor(typeDesc: Desc.NodeTypeDescription, familyName: string) {
+    super(typeDesc, familyName)
+  }
+
+  validateImpl(ast: AST.Node, context: ValidationContext) {
+
+  }
+}
+
+/**
+ * Validates children of a complex type.
+ */
+class NodeComplexTypeChildren {
+  private _categoryName: string;
+
+  private _childValidator: NodeComplexTypeChildrenSequence;
+
+  constructor(desc: Desc.NodeComplexTypeChildrenGroupDescription) {
+    this._categoryName = desc.categoryName;
+    this._childValidator = new NodeComplexTypeChildrenSequence(desc.children as Desc.NodeTypesSequenceDescription);
+  }
+}
+
+class NodeComplexTypeChildrenSequence {
+  private _nodeTypes: string[];
+
+  constructor(desc: Desc.NodeTypesSequenceDescription) {
+    this._nodeTypes = desc.nodeTypes;
+  }
+}
+
+class TypeReference {
+  private _languageName: string;
+  private _typeName: string;
+  private _validator: Validator;
+
+  constructor(_validator: Validator, desc: Desc.TypeReference, currentLang: string) {
+    this._validator = _validator;
+
+    if (Desc.isQualifiedTypeReference(desc)) {
+      this._languageName = desc.languageName;
+      this._typeName = desc.typeName;
+    } else {
+      this._languageName = currentLang;
+      this._typeName = desc;
+    }
+  }
+
+  /**
+   * @return True if this reference can be resolved to a "proper" type.
+   */
+  get isResolveable() {
+    return (this._validator.isKnownType(this._languageName, this._typeName));
+  }
+
+  get type() {
+    if (!this.isResolveable) {
+      throw new Error(`Could not resolve type "${this._languageName}.${this._typeName}"`);
+    } else {
+      return (this._validator.getType(this._languageName, this._typeName));
+    }
+  }
+}
+
+/**
+ * A language consists of type definitions and a set of types that may occur at the root.
+ */
+class Language {
+  private _validator: Validator;
+  private _languageName: string;
+  private _registeredTypes: { [name: string]: NodeComplexType } = {};
+  private _rootTypes: TypeReference[] = [];
+
+  constructor(validator: Validator, desc: Desc.LanguageDescription) {
+    this._validator = validator;
+    this._languageName = desc.languageName;
+
+    desc.root.forEach(rootRef => desc.types.forEach(typeDesc => this.registerTypeValidator(typeDesc)));
+  }
+
+  validateFromRoot(ast: AST.Node, context: ValidationContext) {
+    const rootType = this._rootTypes.find(rootRef => rootRef.type.typeName == ast.nodeName);
+
+    if (!rootType) {
+      context.addError("UNKNOWN_ROOT", ast);
+    }
+
+    rootType.type.validate(ast, context);
+  }
+
+  /**
+   * @return True if the given typename is known in this language.
+   */
+  isKnownType(typename: string) {
+    return (!!this._registeredTypes[typename]);
+  }
+
+  getType(typename: string) {
+    if (!this.isKnownType(typename)) {
+      throw new Error(`Language "${this._languageName}" does not have type "${typename}"`);
+    } else {
+      return (this._registeredTypes[typename]);
+    }
+  }
+
+  /**
+   * Registers a new type validator with this language.
+   */
+  private registerTypeValidator(desc: Desc.NodeTypeDescription) {
+    if (this.isKnownType(desc.nodeName)) {
+      throw new Error(`Attempted to register node "${desc.nodeName}" twice for "${this._languageName}"`);
+    }
+
+    this._registeredTypes[desc.nodeName] = new NodeComplexType(desc, this._languageName);
+  }
+
+  /**
+   * Registers a new possible root with this language.
+   */
+  private registerRootReference(desc: Desc.TypeReference) {
+    this._rootTypes.push(new TypeReference(this._validator, desc, this._languageName));
   }
 }
 
@@ -60,14 +203,60 @@ class Schema {
  * A validator receives instances of one or multiple schemas and will
  * check any AST against those languages.
  */
-class Validator {
-  private _registeredLanguages: { [langName: string]: Schema };
+export class Validator {
+  private _registeredLanguages: { [langName: string]: Language } = {};
 
-  registerLanguage(desc: Desc.SchemaDescription) {
-    this._registeredLanguages[desc.languageName] = new Schema(desc);
+  constructor(langs: Desc.LanguageDescription[]) {
+    langs.forEach(langDesc => this.registerLanguage(langDesc));
   }
 
-  validate() {
+  registerLanguage(desc: Desc.LanguageDescription) {
+    if (this.isKnownLanguage(desc.languageName)) {
+      throw new Error(`Attempted to register language "${desc.languageName}" twice`);
+    }
 
+    this._registeredLanguages[desc.languageName] = new Language(this, desc);
+  }
+
+  validateFromRoot(ast: AST.Node) {
+    const lang = this.getLanguage(ast.nodeFamily);
+    const result = new ValidationContext();
+
+    lang.validateFromRoot(ast, result);
+
+    return (result);
+  }
+
+  getLanguage(language: string) {
+    if (!this.isKnownLanguage(language)) {
+      throw new Error(`Validator does not know language "${language}"`);
+    } else {
+      return (this._registeredLanguages[language]);
+    }
+  }
+
+  getType(language: string, typename: string) {
+    if (!this.isKnownType(language, typename)) {
+      throw new Error(`Validator does not know type "${language}.${typename}"`);
+    } else {
+      return (this._registeredLanguages[language].getType(typename));
+    }
+  }
+
+  /**
+   * @return True if the given language is known to this validator.
+   */
+  isKnownLanguage(language: string) {
+    return (!!this._registeredLanguages[language]);
+  }
+
+  /**
+   * @return True if the given typename is known in the given language.
+   */
+  isKnownType(language: string, typename: string) {
+    return (
+      this.isKnownLanguage(language) &&
+      this._registeredLanguages[language].isKnownType(typename)
+    );
   }
 }
