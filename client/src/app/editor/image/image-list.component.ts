281df7984d63ed17c57c1e64140ea94c1b313f4f
Ole Just <minf9724@fh-wedel.de>
2017-10-06 20:07:42 +0200

    CLIENT: styled image library

--- client/src/app/editor/image/image-list.component.ts
+++ client/src/app/editor/image/image-list.component.ts
@@ -9,39 +9,53 @@ import { ToolbarService } from '../toolbar.service'
 import { ImageService } from './image.service'
 
 @Component({
-  templateUrl: 'templates/image-list.html'
+    selector: 'radio-ng-model-example',
+    templateUrl: 'templates/image-list.html'
 })
 export class ImageListComponent {
-  constructor(
-    private _serverApi: ServerApiService,
-    private _http: Http,
-    private _projectService: ProjectService,
-    private _imageService: ImageService,
-    private _sidebarService: SidebarService,
-    private _toolbarService: ToolbarService
-  ) {
-  }
+    availableDisplayTypes = [
+        'list',
+        'card'
+    ];
 
-  ngOnInit() {
-    this._sidebarService.hideSidebar();
-    this._toolbarService.resetItems();
-    this._toolbarService.savingEnabled = false;
-
-    this._imageService.loadImageList();
-  }
-
-  deleteImage(image_id: string, image_name: string) {
-    if (confirm('"' + image_name + '"' + " löschen?")) {
-      this._http.delete(this._serverApi.getImageDeleteUrl(this._projectService.cachedProject.id, image_id))
-          .subscribe(res => {
-            console.log(res);
-            //TODO handle failure
-            this._imageService.loadImageList();
-          });
+    displayNames = {
+        'card': "Gallerie",
+        'list': "Liste"
     }
-  }
 
-  get images() {
-    return (this._imageService.images);
-  }
+    currentDisplayType = this.availableDisplayTypes[0];
+
+    constructor(
+        private _serverApi: ServerApiService,
+        private _http: Http,
+        private _projectService: ProjectService,
+        private _imageService: ImageService,
+        private _sidebarService: SidebarService,
+        private _toolbarService: ToolbarService
+    ) {
+    }
+
+    ngOnInit() {
+        this._sidebarService.hideSidebar();
+        this._toolbarService.resetItems();
+        this._toolbarService.savingEnabled = false;
+
+        this._imageService.loadImageList();
+    }
+
+    deleteImage(image_id: string, image_name: string) {
+        if (confirm('"' + image_name + '"' + " löschen?")) {
+            this._http.delete(this._serverApi.getImageDeleteUrl(this._projectService.cachedProject.id, image_id))
+                .subscribe(res => {
+                    console.log(res);
+                    //TODO handle failure
+                    this._imageService.loadImageList();
+                });
+        }
+    }
+
+    get images() {
+        return (this._imageService.images);
+    }
+
 }
