4818378774c771ea5ba2453ea18a5d580ea2f64e
Marcus Riemer <Marcus@GurXite.de>
2017-10-19 14:25:58 +0200

    Client: Spec and Codegen for basic regular expressions

---
+++ client/src/app/shared/syntaxtree/regex/regex.codegenerator.ts
@@ -0,0 +1,70 @@
+import { NodeConverterRegistration, CodeGeneratorProcess, OutputSeparator } from '../codegenerator'
+import { Node } from '../syntaxtree'
+
+/**
+ * Converts a RegEx-AST to a properly indented stringified representation.
+ */
+export const NODE_CONVERTER: NodeConverterRegistration[] = [
+  {
+    type: {
+      languageName: "regex",
+      typeName: "constant"
+    },
+    converter: {
+      init: function(node: Node, process: CodeGeneratorProcess) {
+        // TODO: Escaping
+        process.addConvertedFragment(node.properties["value"], node);
+      }
+    }
+  },
+  {
+    type: {
+      languageName: "regex",
+      typeName: "alternative"
+    },
+    converter: {
+      init: function(node: Node, process: CodeGeneratorProcess) {
+        process.addConvertedFragment("(", node);
+
+        const alternatives = node.children["expressions"];
+        alternatives.forEach((expr, i) => {
+          if (i > 0) {
+            process.addConvertedFragment("|", node);
+          }
+          process.generateNode(expr);
+        });
+
+        process.addConvertedFragment(")", node);
+
+        return ([]);
+      }
+    }
+  },
+  {
+    type: {
+      languageName: "regex",
+      typeName: "expr"
+    },
+    converter: {
+      init: function(node: Node, process: CodeGeneratorProcess) {
+        process.generateNode(node.children["singleExpression"][0]);
+
+        return ([]);
+      }
+    }
+  },
+  {
+    type: {
+      languageName: "regex",
+      typeName: "root"
+    },
+    converter: {
+      init: function(node: Node, process: CodeGeneratorProcess) {
+        const expressions = node.children["expressions"];
+        expressions.forEach(expr => process.generateNode(expr));
+
+        return ([]);
+      }
+    }
+  }
+]
