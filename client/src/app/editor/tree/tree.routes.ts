fa7ec6ec05b22d30cae7e582dfedbef858228466
Marcus Riemer <Marcus@GurXite.de>
2017-11-16 10:31:02 +0100

    Client: Editor type is now a suffix of a resource editor URL

--- client/src/app/editor/tree/tree.routes.ts
+++ client/src/app/editor/tree/tree.routes.ts
@@ -5,7 +5,7 @@ import { BlockEditorComponent } from './block/block-editor.component'
 
 export const syntaxTreeEditorRoutes: Routes = [
   { path: '', component: RawTreeEditorComponent },
-  { path: 'raw/:resourceId', component: RawTreeEditorComponent },
-
-  { path: 'block/:resourceId', component: BlockEditorComponent },
+  { path: ':resourceId', redirectTo: ':resourceId/block', pathMatch: 'full' },
+  { path: ':resourceId/raw', component: RawTreeEditorComponent },
+  { path: ':resourceId/block', component: BlockEditorComponent },
 ]
