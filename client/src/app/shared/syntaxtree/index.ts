780c685be562c87b978ff2a10ca45143ce884830
Marcus Riemer <Marcus@GurXite.de>
2017-11-26 21:21:33 +0100

    Client: Removed inital draft of XML in favor of DXML

--- client/src/app/shared/syntaxtree/index.ts
+++ client/src/app/shared/syntaxtree/index.ts
@@ -7,7 +7,6 @@ export * from './validator.description'
 
 import { Language } from './language'
 import * as Sql from './sql'
-import * as Xml from './xml'
 import * as DynamicXml from './dxml'
 import * as RegEx from './regex'
 
@@ -16,20 +15,17 @@ import * as RegEx from './regex'
  */
 export const AvailableLanguages = {
   Sql: new Language(Sql.LANGUAGE_DESCRIPTION),
-  Xml: new Language(Xml.LANGUAGE_DESCRIPTION),
   DXml: new Language(DynamicXml.LANGUAGE_DESCRIPTION),
   RegEx: new Language(RegEx.LANGUAGE_DESCRIPTION),
   All: new Language({
     id: "all",
     name: "All Languages",
     generators: [
-      ...Xml.LANGUAGE_DESCRIPTION.generators,
       ...DynamicXml.LANGUAGE_DESCRIPTION.generators,
       ...Sql.LANGUAGE_DESCRIPTION.generators,
       ...RegEx.LANGUAGE_DESCRIPTION.generators,
     ],
     validators: [
-      ...Xml.LANGUAGE_DESCRIPTION.validators,
       ...DynamicXml.LANGUAGE_DESCRIPTION.validators,
       ...Sql.LANGUAGE_DESCRIPTION.validators,
       ...RegEx.LANGUAGE_DESCRIPTION.validators,
