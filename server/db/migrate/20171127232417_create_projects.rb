e75ee9266ec898ac181ccfe1eb64d508cb04f09c
sukanta-ghosh <sukanta.ghosh@xing.com>
2017-11-28 00:34:05 +0100

    WIP: add project model and table with relevant fields

---
+++ server/db/migrate/20171127232417_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration[5.1]
+  def change
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    create_table :projects id: :uuid, default: 'gen_random_uuid()' do |t|
+      t.string :name
+      t.text :description
+      t.boolean :public
+      t.uuid :preview
+      t.uuid :index_image_id
+      t.string :active_database
+      t.timestamps
+    end
+  end
+end
