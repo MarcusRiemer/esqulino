6f4b6c53761ff61368c7576fc38d13fc3673f072
Marcus Riemer <Marcus@GurXite.de>
2017-11-28 09:35:58 +0100

    Server: Merged migrations

--- server/db/migrate/20171103124205_create_language_models.rb
+++ server/db/migrate/20171103124205_create_language_models.rb
@@ -1,8 +1,11 @@
 class CreateLanguageModels < ActiveRecord::Migration[5.1]
   def change
-    create_table :language_models, id: false do |t|
-      t.uuid :id
+    enable_extension 'pgcrypto' unless extension_enabled? 'pgcrypto'
+    
+    create_table :language_models, id: :uuid do |t|
       t.string :name
+
+      t.json :model
 
       t.timestamps
     end
