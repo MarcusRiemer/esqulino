acee9da359aac40dbfa5943485013b9b51d32f09
sukanta-ghosh <sukanta.ghosh@xing.com>
2017-11-29 23:25:03 +0100

    fix: project model for migration and operation
    Project class already in lib file so need to use a different name in
    model

--- server/db/migrate/20171127232417_create_projects.rb
+++ server/db/migrate/20171127232417_create_projects.rb
@@ -1,7 +1,7 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
-    create_table :projects id: :uuid, default: 'gen_random_uuid()' do |t|
+    create_table :projects, id: :uuid, default: 'gen_random_uuid()' do |t|
       t.string :name
       t.text :description
       t.boolean :public
