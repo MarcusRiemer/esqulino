6af52c0fa29e29b46c74f0d75c1575982d9d443f
Marcus Riemer <Marcus@GurXite.de>
2017-12-14 11:26:17 +0100

    Client: Pretty printing of properties

--- client/src/app/shared/syntaxtree/prettyprint.spec.ts
+++ client/src/app/shared/syntaxtree/prettyprint.spec.ts
@@ -1,59 +1,55 @@
 import * as d from './validator.description'
 import * as p from './prettyprint'
 
+/**
+ * Ensures that the given in and output files do match correctly.
+ */
+function verifyFiles(fileName: string) {
+  const input = require(`json-loader!./spec/${fileName}.json`);
+  let expected = require(`raw-loader!./spec/${fileName}.txt`) as string;
+
+  if (expected.endsWith("\n")) {
+    expected = expected.substr(0, expected.length - 1);
+  }
+
+  expect(p.prettyPrintGrammar(input)).toEqual(expected);
+}
+
 describe('Grammar PrettyPrinter', () => {
   it('Helper "recursiveJoin": No nesting', () => {
-    const r = p.recursiveJoin("a", ["1", "2", "3"]);
+    const r = p.recursiveJoin("a", "x", ["1", "2", "3"]);
     expect(r).toEqual("1a2a3");
   });
 
   it('Helper "recursiveJoin": First level', () => {
-    expect(p.recursiveJoin("a", [["1", "2", "3"]])).toEqual("  1a  2a  3");
-    expect(p.recursiveJoin("b", [["1"], "2", ["3"]])).toEqual("  1b2b  3");
-    expect(p.recursiveJoin("c", [["1"], ["2"], ["3"]])).toEqual("  1c  2c  3");
+    expect(p.recursiveJoin("a", "x", [["1", "2", "3"]])).toEqual("x1ax2ax3");
+    expect(p.recursiveJoin("b", "x", [["1"], "2", ["3"]])).toEqual("x1b2bx3");
+    expect(p.recursiveJoin("c", "x", [["1"], ["2"], ["3"]])).toEqual("x1cx2cx3");
   });
 
   it('Helper "recursiveJoin": Second level', () => {
-    expect(p.recursiveJoin("a", [[["1"], "2", "3"]])).toEqual("    1a  2a  3");
+    expect(p.recursiveJoin("a", "x", [[["1"], "2", "3"]])).toEqual("xx1ax2ax3");
   });
 
   it('Helper "recursiveJoin": Practical examples', () => {
-    expect(p.recursiveJoin("\n", ["grammar g {", ["node"], "}"])).toEqual("grammar g {\n  node\n}");
-  });
-
-  it('Empty Grammar', () => {
-    const r = p.prettyPrintGrammar({
-      languageName: "addressBook",
-      root: "addressBook",
-      types: {}
-    });
-    expect(r).toEqual(`grammar "addressBook" {\n}`);
-  });
-
-  it('Grammar with empty nodes', () => {
-    const r = p.prettyPrintGrammar({
-      languageName: "addressBook",
-      root: "addressBook",
-      types: {
-        "addressBook": {
-
-        },
-        "card": {
-
-        }
-      }
-    });
-    expect(r).toEqual(`grammar "addressBook" {\n    node "addressBook" {\n    }\n    node "card" {\n    }\n}`);
+    expect(p.recursiveJoin("\n", "  ", ["grammar g {", ["node"], "}"])).toEqual("grammar g {\n  node\n}");
+    expect(p.recursiveJoin("\n", "  ", ['grammar "g1" {', ['node "n1" {', '}'], ['node "n2" {', '}'], '}'])).toEqual(`grammar "g1" {\n  node "n1" {\n  }\n  node "n2" {\n  }\n}`);
   });
 
   it('prop "s1" { string }', () => {
     const r = p.prettyPrintProperty("s1", { base: "string" });
 
-    expect(r.join(' ')).toEqual(`prop "s1" { string }`);
+    expect(r).toEqual([`prop "s1" { string }`]);
   });
 
-  it('prop "s2" { string { length=4 } }', () => {
-    const r = p.prettyPrintProperty("s2", {
+  it('prop? "s2" { string }', () => {
+    const r = p.prettyPrintProperty("s1", { base: "string", isOptional: true });
+
+    expect(r).toEqual([`prop? "s1" { string }`]);
+  });
+
+  it('prop "s3" { string { length=4 } }', () => {
+    const r = p.prettyPrintProperty("s3", {
       base: "string",
       restrictions: [
         {
@@ -63,7 +59,34 @@ describe('Grammar PrettyPrinter', () => {
       ]
     } as d.NodePropertyStringDescription);
 
-    expect(r.join(' ')).toEqual(`prop "s2" { string { length=4 } }`);
+    expect(r).toEqual([`prop "s3" { string { length=4 } }`]);
+  });
+
+  it('prop "s4" { string { minLength=2 maxLength=4 } }', () => {
+    const r = p.prettyPrintProperty("s4", {
+      base: "string",
+      restrictions: [
+        {
+          type: "minLength",
+          value: 2
+        },
+        {
+          type: "maxLength",
+          value: 4
+        }
+      ]
+    } as d.NodePropertyStringDescription);
+
+    expect(r).toEqual(
+      [
+        `prop "s4" {`,
+        [
+          `string {`,
+          [`minLength=2`, `maxLength=4`],
+          `}`
+        ],
+        `}`
+      ]);
   });
 
   it('prop "i1" { integer }', () => {
@@ -71,6 +94,32 @@ describe('Grammar PrettyPrinter', () => {
       base: "integer",
     } as d.NodePropertyIntegerDescription);
 
-    expect(r.join(' ')).toEqual(`prop "i1" { integer }`);
+    expect(r).toEqual([`prop "i1" { integer }`]);
+  });
+
+
+  it('Empty Grammar', () => {
+    const r = p.prettyPrintGrammar({
+      languageName: "empty",
+      root: "empty",
+      types: {}
+    });
+    expect(r).toEqual(`grammar "empty" {\n}`);
+  });
+
+  it('Grammar with empty nodes', () => {
+    verifyFiles('g1-empty-nodes');
+  });
+
+  it('Grammar g2: string node', () => {
+    verifyFiles('g2-string-node');
+  });
+
+  it('Grammar g3: string node with length = 4', () => {
+    verifyFiles('g3-string-node-length');
+  });
+
+  it('Grammar g4: string node with minimum and maximum length', () => {
+    verifyFiles('g4-string-node-length');
   });
 });
--- client/src/app/shared/syntaxtree/prettyprint.ts
+++ client/src/app/shared/syntaxtree/prettyprint.ts
@@ -11,23 +11,31 @@ export function prettyPrintGrammar(g: Desc.GrammarDescription): string {
   const nodes = Object.entries(g.types)
     .map(([name, t]) => prettyPrintType(name, t))
 
-  return (recursiveJoin('\n', [head, nodes, tail] as NestedString));
+  const toReturn = [head, ...nodes, tail] as NestedString
+
+  return (recursiveJoin('\n', '  ', toReturn));
 }
 
-interface NestedString {
+/**
+ * A possibly infinitely nested list of strings.
+ */
+interface NestedString extends Array<NestedString | string> {
   [key: number]: Array<NestedString | string> | string;
 }
 
 /**
- * "Flattens" the given arbitrarily nested strings to a single string.
+ * "Flattens" the given arbitrarily nested strings to a single string. The resulting
+ * string is indented according to the given indentation character which is repeated
+ * according to the current nesting level.
  *
- * @param sep The separation character to use
+ * @param sep The separation character to use after each list
+ * @param indent The indentation character to use in front of each item
  * @param n The nested string
  */
-export function recursiveJoin(sep: string, n: NestedString): string {
+export function recursiveJoin(sep: string, indent: string, n: NestedString): string {
   const impl = (sep: string, n: NestedString, depth: number): string => {
     if (typeof n === 'string') {
-      return ('  '.repeat(depth) + n);
+      return (indent.repeat(depth) + n);
     } else {
       return (
         Object.values(n)
@@ -37,7 +45,11 @@ export function recursiveJoin(sep: string, n: NestedString): string {
     }
   };
 
-  return (impl(sep, n, -1));
+  // Run the recursive implementation
+  const resultString = impl(sep, n, -1);
+
+  // Get rid of whitespace only lines
+  return (resultString);
 }
 
 /**
@@ -57,16 +69,23 @@ export function prettyPrintType(name: string, t: Desc.NodeTypeDescription): Nest
  * Prints the grammar for a concrete node.
  */
 export function prettyPrintConcreteNodeType(name: string, t: Desc.NodeConcreteTypeDescription): NestedString {
-  const head = `node "${name}" {`
-  const tail = `}`;
+  const toReturn: NestedString = [`node "${name}" {`];
 
-  const attributes = Object.entries(t.properties || {})
-    .map(([propName, prop]) => prettyPrintProperty(propName, prop));
+  // Add all properties (if there are any)
+  if (t.properties) {
+    const attributes = Object.entries(t.properties)
+      .map(([propName, prop]) => prettyPrintProperty(propName, prop));
+    toReturn.push(...attributes);
+  }
 
-  const children = Object.entries(t.children || {})
-    .map(([groupName, group]) => prettyPrintChildren(groupName, group));
+  // Add all children (if there are any)
+  if (t.children) {
+    const children = Object.entries(t.children)
+      .map(([groupName, group]) => prettyPrintChildren(groupName, group));
+    toReturn.push(...children);
+  }
 
-  return ([head, ...attributes, ...children, tail]);
+  return ([...toReturn, `}`]);
 }
 
 
@@ -80,23 +99,27 @@ export function prettyPrintOneOfType(name: string, t: Desc.NodeOneOfTypeDescript
 /**
  * Prints the grammar for a property
  */
-export function prettyPrintProperty(name: string, p: Desc.NodePropertyTypeDescription): string[] {
-  const head = `prop "${name}" { ${p.base}`;
+export function prettyPrintProperty(name: string, p: Desc.NodePropertyTypeDescription): NestedString {
+  const optional = p.isOptional ? '?' : '';
+  const head = `prop${optional} "${name}"`;
 
-  let restrictions = [];
+  let restrictions: string[] = [];
   if (Desc.isNodePropertyStringDesciption(p) && p.restrictions) {
     restrictions = p.restrictions.map(r => `${r.type}=${r.value}`);
   }
 
-  const tail = `}`;
-
-  if (restrictions.length > 0) {
-    return ([head, "{", ...restrictions, "}", tail]);
+  if (restrictions.length === 0) {
+    return ([`${head} { ${p.base} }`]);
+  } else if (restrictions.length === 1) {
+    return ([`${head} { ${p.base} { ${restrictions[0]} } }`]);
   } else {
-    return ([head, tail]);
+    return ([head + " {", [p.base + " {", restrictions, "}"], "}"]);
   }
 }
 
+/**
+ * Prints the grammar a single child.
+ */
 export function prettyPrintChildren(name: string, p: Desc.NodeChildrenGroupDescription): string[] {
   const head = `children "${name}" {`;
   const tail = `}`;
