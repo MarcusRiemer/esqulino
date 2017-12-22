bb1bdb667e363e7f6f33e71ce9c641cd7b3be0bd
Marcus Riemer <Marcus@GurXite.de>
2017-12-22 09:54:01 +0100

    Client: Tracking of Drag & Drop events

---
+++ client/src/app/shared/analytics.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+import { Angulartics2 } from 'angulartics2';
+
+export enum TrackCategory {
+  BlockEditor = "blockEditor"
+}
+
+export interface TrackEvent {
+  category: TrackCategory;
+  action: string;
+  name?: string;
+  value?: any;
+}
+
+/**
+ * Encapsulates tracking actions.
+ */
+@Injectable()
+export class AnalyticsService {
+
+  constructor(private _tracking: Angulartics2) {
+
+  }
+
+  /**
+   * Tracks a specific event that has just happened.
+   */
+  trackEvent(evt: TrackEvent) {
+    this._tracking.eventTrack.next({
+      action: evt.action,
+      properties: {
+        category: evt.category,
+        name: evt.name,
+        value: evt.value
+      }
+    });
+  }
+}
