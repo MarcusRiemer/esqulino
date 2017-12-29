6d78cc137ba5f96f7c88c19ad64d999f724fb962
Marcus Riemer <Marcus@GurXite.de>
2017-12-29 11:58:39 +0100

    Client: CLI may now emit names of all available grammars

--- client/src/main.cli.ts
+++ client/src/main.cli.ts
@@ -6,14 +6,45 @@ import { GrammarDescription } from './app/shared/syntaxtree/validator.descriptio
 
 import * as dxml from './app/shared/syntaxtree/dxml/dxml.validator'
 import * as regex from './app/shared/syntaxtree/regex/regex.validator'
+import * as sql from './app/shared/syntaxtree/sql/sql.validator'
 
-interface Command {
-  type: string;
+
+interface PrintGrammarCommand {
+  type: "printGrammar",
+  id: string;
 }
 
-interface PrintGrammarCommand extends Command {
-  type: "printGrammar",
-  name: string;
+interface AvailableGrammarsCommand {
+  type: "available"
+}
+
+type Command = PrintGrammarCommand | AvailableGrammarsCommand;
+
+/**
+ * Retrieves all grammars that are known to this instance.
+ */
+function availableGrammars(): GrammarDescription[] {
+  return ([dxml.GRAMMAR_DESCRIPTION, regex.GRAMMAR_DESCRIPTION, sql.GRAMMAR_DESCRIPTION]);
+}
+
+/**
+ * Retrieves a single grammar by name
+ */
+function findGrammar(name: string) {
+  return (availableGrammars().find(d => d.languageName === name));
+}
+
+/**
+ * Executes the given command
+ */
+function executeCommand(command: Command) {
+  switch (command.type) {
+    case "printGrammar":
+      const g = findGrammar(command.id);
+      return (prettyPrintGrammar(g));
+    case "available":
+      return (availableGrammars().map(g => g.languageName));
+  }
 }
 
 const rl = readline.createInterface({
@@ -22,17 +53,16 @@ const rl = readline.createInterface({
   terminal: false
 });
 
+// Each given line is expected to be a self contained JSON object.
 rl.on('line', function(line) {
   try {
-    const command = JSON.parse(line) as PrintGrammarCommand;
-    switch (command.type) {
-      case "printGrammar":
-        console.log(prettyPrintGrammar(regex.GRAMMAR_DESCRIPTION));
-        break;
+    const command = JSON.parse(line) as Command;
+    let result = executeCommand(command);
+
+    if (result !== undefined) {
+      console.log(JSON.stringify(result));
     }
   } catch (e) {
     console.error(e);
   }
 });
-
-console.log("BlattWerkzeug CLI started - Waiting for JSON requests");
