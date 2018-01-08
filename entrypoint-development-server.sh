5ee41af64245515c5109aab13f4b048ef1a761ce
Marcus Riemer <Marcus@GurXite.de>
2018-01-08 20:52:27 +0100

    Docker: Nailed used version of imagemagick to 6, rmagick isn't ready for 7

--- entrypoint-development-server.sh
+++ entrypoint-development-server.sh
@@ -3,6 +3,8 @@
 groupadd -g "$USER_GID" user
 useradd -m -u "$USER_UID" -g "$USER_GID" user
 
-export GEM_PATH=/srv/esqulino/server/vendor/bundle/ruby/2.4.0
-make -C server install-deps
+export GEM_PATH=/srv/esqulino/server/vendor/bundle/ruby/2.5.0
+
+# TODO: Remove PKG_CONFIG_PATH once rmagick works with imagemagick 7
+PKG_CONFIG_PATH=/usr/lib/imagemagick6/pkgconfig make -C server install-deps
 su user -c "make --no-print-directory -C server run-dev"
