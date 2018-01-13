52ee68085be52b39b816813727019641c9359c86
sukanta-ghosh <sukanta.ghosh@xing.com>
2018-01-13 13:11:08 +0100

    Server:fix# remove conflicts

--- server/db/migrate/20171127232417_create_projects.rb
+++ server/db/migrate/20171127232417_create_projects.rb
@@ -1,11 +1,7 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
-<<<<<<< HEAD
     create_table :projects, id: :uuid, default: 'gen_random_uuid()' do |t|
-=======
-    create_table :projects, id: :uuid do |t|
->>>>>>> master
       t.string :name
       t.text :description
       t.boolean :public
