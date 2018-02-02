4f9092ad5e4bcec36548b19bacd180211bc37f45
sukanta-ghosh <sukanta.ghosh@xing.com>
2018-02-02 13:50:12 +0100

    Server: handle associations:

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 20180201093553) do
     t.index ["project_id"], name: "index_code_resources_on_project_id"
   end
 
-  create_table "language_models", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "language_models", id: false, force: :cascade do |t|
+    t.uuid "id"
     t.string "name"
-    t.json "model"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
