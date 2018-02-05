48fb27796666b2cf193d443aa7c5b304d333bca4
Marcus Riemer <Marcus@GurXite.de>
2018-02-05 12:17:25 +0100

    Server: RSpec now generates coverage and xunit reports

--- server/spec/spec_helper.rb
+++ server/spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'database_cleaner'
+require 'simplecov'
 
 RSpec.configure do |config|
+  SimpleCov.start 'rails'
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
