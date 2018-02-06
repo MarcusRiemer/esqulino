464373dda6db1809241f9df8a5d761b05a2f3aef
Marcus Riemer <Marcus@GurXite.de>
2018-02-06 12:06:05 +0100

    Server: First actual connection from CodeResource to the IdeService

--- server/spec/lib/ide_service_spec.rb
+++ server/spec/lib/ide_service_spec.rb
@@ -3,9 +3,17 @@ require 'rails_helper'
 require_dependency 'ide_service'
 
 RSpec.describe "IDE Service" do
+
+  # Retrieves the "actual" test configuration for the "exec" mode.
+  # This will normally be ignored during testing but is relevant if
+  # we want to test the *actual* communication.
+  def exec_configuration
+    Rails.configuration.sqlino['ide_service']['exec']
+  end
+  
   context "OneShot" do
     it "emits SQL source code" do
-      service = OneShotExecIdeService.new
+      service = OneShotExecIdeService.new(config: exec_configuration)
 
       tree_desc = {
         "language" => "sql",
@@ -20,6 +28,15 @@ RSpec.describe "IDE Service" do
     it "Creates a OneShot instance" do
       service = IdeService.instantiate(service_config: { "exec" => { "mode" => "one-shot" } })
       expect(service.class).to be OneShotExecIdeService
+    end
+
+    it "Mocking has precedence" do
+      service = IdeService.instantiate(service_config: { "mock" => true, "exec" => { "mode" => "one-shot" } })
+      expect(service.class).to be MockIdeService
+    end
+
+    it "missing modes are an error" do
+      expect { IdeService.instantiate(service_config: { }) }.to raise_exception IdeServiceError
     end
   end
 end
