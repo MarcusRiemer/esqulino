d89d640a34e8ffa8b0269d3e910917b200c041d6
Marcus Riemer <Marcus@GurXite.de>
2018-02-06 17:22:17 +0100

    Server: Added possibility to validate against existing JSON-schema
    * This commit throws all serialization logic into the
      ApplicationRecord, that may or may not be a good idea ...
    * Additionaly updated some resources on the client to include the
      "createdAt" and "updatedAt" attributes, these may come in handy.

--- server/spec/factories/project.rb
+++ server/spec/factories/project.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :project, class: Project do
-    name "Spec Project"
+    sequence(:name) { |n| "Spec Project ##{n}"}
+    slug { name.parameterize }
+    description "Generated on the fly, used for tests"
+    index_page_id nil
+    public true
   end
 end
