4f2b529ef6007e9d8a8b49e3022bf0f60cfea8ac
sukanta-ghosh <sukanta.ghosh@xing.com>
2018-02-10 17:06:59 +0100

    Server: Create project finished

---
+++ server/app/models/builders/project_utility.rb
@@ -0,0 +1,38 @@
+module Builders
+
+  # This Utility class is responsible to create project deirectory and sqlite databse for the project
+  # 
+  class ProjectUtility
+    
+    def initialize(id:, db_type:)
+      @id = id
+      @db = db_type
+    end
+
+    def generate
+      create_project_directory
+      create_database
+    end
+
+    def project_path
+      @base_path = Rails.application.config.sqlino[:projects_dir]
+      full_path = File.join @base_path, @id
+    end
+
+    def create_project_directory
+      Dir.mkdir project_path
+    end
+
+    def create_database
+      if @db == 'sqlite3'
+        # Creating folders and files
+        project_database_folder = File.join project_path, 'databases'
+        project_database_default_path = File.join project_database_folder, 'default.sqlite'
+        Dir.mkdir project_database_folder
+        SQLite3::Database.new project_database_default_path
+      else
+        raise EsqulinoError.new "Unknown database type: \"#{db}\" "
+      end
+    end
+  end
+end
