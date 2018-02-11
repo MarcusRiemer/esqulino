d297cf8ccdfab9a546784477dc87a5d65b6d2386
sukanta-ghosh <sukanta.ghosh@xing.com>
2018-02-11 20:42:18 +0100

    Server: use proper pram handler and fix payload for spec

--- server/spec/controllers/proejcts_controller_spec.rb
+++ server/spec/controllers/proejcts_controller_spec.rb
@@ -5,29 +5,40 @@ RSpec.describe ProjectsController do
   describe '#create' do
   
     describe "valid request" do
-      let(:valid_params) { {name: 'Test Projct', slug: 'test-project'} }
+      let(:project_creation_json) { 
+        {
+          "apiVersion": "4",
+          "slug": "test",
+          "name": "Test project",
+          "admin": {
+            "name": "user",
+            "password": "user"
+          },
+          "dbType": "sqlite3"
+        }
+      }
 
       it "adds new project" do
         expect{
-          post :create, params: valid_params
+          post :create, params: project_creation_json
         }.to change(Project, :count).by(1)
       end
 
       it 'returns ok' do
-        post :create, params: valid_params, format: :json
+        post :create, params: project_creation_json, format: :json
 
         expect(response.status).to eq(200)
         expect(response.content_type).to eq "application/json"
-        expect(Project.all.last.slug).to eq("test-project")
+        expect(Project.all.last.slug).to eq("test")
       end
 
     end
     
     describe "invalid request" do
-      let(:invalid_params) { {name: "Some project", slug: "" }}
+      let(:invalid_params) { {"name" => "Some project", "slug" => nil }}
 
       it 'returns bad request' do
-        post :create, params: invalid_params
+        post :create, params: invalid_params, format: :json
         expect(response.status).to eq(400)
       end
     end
@@ -39,7 +50,7 @@ RSpec.describe ProjectsController do
       let(:update_params) { {id: project.id ,name: 'Hello Test', slug: 'test', description: 'This is a test project'} }
 
       xit 'updates an exisiting project' do
-        post :edit, params: update_params
+        post "api/project/#{project.slug}", params: update_params
         expect(Project.find_by(project.id).name).to eq('Hello Test')
       end
 
