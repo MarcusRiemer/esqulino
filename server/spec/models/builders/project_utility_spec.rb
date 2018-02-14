b9f413d9594c55efc8f803b3ba0b40eaea10afcd
Marcus Riemer <Marcus@GurXite.de>
2018-02-14 20:48:48 +0100

    Server: Fixed failing FakeFS-specs with a brute-force solution
    We should probably provide a helper to set up a default filesystem
    layout instead of using `FakeFS::FileSystem.clone` repeatedly.

--- server/spec/models/builders/project_utility_spec.rb
+++ server/spec/models/builders/project_utility_spec.rb
@@ -8,21 +8,28 @@ RSpec.describe Builders::ProjectUtility do
   describe "Create" do
 
     let(:db_type) { "sqlite3" }
-    subject { described_class.new(id: uuid, db_type: db_type) }
+    let(:default_uuid) { "c8832d3d-3809-472b-8dc7-2344e358cab5" }
+    subject { described_class.new(id: default_uuid, db_type: db_type) }
 
     context "project directory" do
-      it 'with project id' do
-        # FakeFS.with_fresh do
+      it 'ends with the uuid' do
+        expect(subject.project_path).to end_with(default_uuid)
+      end
+      
+      it 'can be created' do
+        FakeFS.with_fresh do
+          FakeFS::FileSystem.clone(Rails.application.config.sqlino[:projects_dir])
           subject.create_project_directory
           expect(Dir.exist? subject.project_path).to be true
-        # end
+        end
       end
 
       it 'fails on duplicates' do
-        # FakeFS.with_fresh do
+        FakeFS.with_fresh do
+          FakeFS::FileSystem.clone(Rails.application.config.sqlino[:projects_dir])
           subject.create_project_directory
           expect { subject.create_project_directory }.to raise_error EsqulinoError
-        # end
+        end
       end
     end
 
