814fe794e26325fdf81fd74fca0a38d5e0302b5a
Marcus Riemer <Marcus@GurXite.de>
2018-02-14 20:48:00 +0100

    Server: Updating and creating projects now works

--- server/app/controllers/projects_controller.rb
+++ server/app/controllers/projects_controller.rb
@@ -4,48 +4,46 @@ class ProjectsController < ApplicationController
   include ProjectsHelper
   include JsonSchemaHelper
 
-  before_action :update_params, only: [:edit]
-  before_action :project_params, only: [:create]
-
   def index
     @projects = Project.all
 
     render json: @projects
   end
 
-  # Creating a new project via clients post call
-  # Setup project directory and sqlite database with project utility class
+  # Apart from creating the database object this action also needs to
+  # set up the directory layout for project assets that are stored
+  # on the disk.
   def create
-    @project = Project.new
-    @project.assign_attributes(project_params['project'])
-    if @project.save
-      Builders::ProjectUtility.new(id: @project.id, db_type: project_params['db_type']).generate
-      render json: { 'id' => @project.slug }, :status => 200
+    # Creating the database object
+    project = Project.new
+    project.assign_attributes(project_creation_params)
+
+    # If that worked ...
+    if project.save
+      # ... create the directoy layout
+      Builders::ProjectUtility.new(id: project.id, db_type: 'sqlite3').generate!
+      render json: { 'id' => project.slug }, :status => 200
     else
-      render nothing: true, status: 400
+      # ... otherwise tell the user where he went wrong
+      render :json => { 'errors' => project.errors }, :status => 400
     end
   end
 
+  # Retrieves all information about a single project. This is the only
+  # request the client will make to retrieve the *whole* project with
+  # *everything* that is required to render it properly.
   def show
     project = Project.full.find_by(slug: params[:project_id])
     render json: project.to_full_api_response
   end
 
-  # Update an existing project
+  # Update an existing project.
   def update
-
-    # ensure_write_access do
-    #   updated_project = ensure_request("ProjectUpdateDescription", request.body.read)
-
-    #   current_project.update_description! updated_project
-    #   current_project.save_description
-
-    #   render :status => 200
-    # end
-    # @project = Project.find_by_slug(params[:project_id])
-    project = Project.find_by_slug(params[:project_id])
-    project.update(project_params)
-    head :no_content
+    ensure_write_access do
+      project = Project.find_by_slug(params[:project_id])
+      project.update(project_update_params)
+      head :no_content
+    end
   end
 
   def destroy
@@ -65,12 +63,15 @@ class ProjectsController < ApplicationController
 
   private
 
-  def project_params
-    params.permit(:name, :slug, :apiVersion, :description, :activeDatabase, :dbType, admin: {}, project: {}).transform_keys! { |k| k.underscore }
+  # These attributes are mandatory when a project is created
+  def project_creation_params
+    params.permit(:name, :slug)
+      .transform_keys { |k| k.underscore }
   end
 
-  def update_params
-    @payloads ||= JSON.parse(request.body.read)
-    @payloads.transform_keys! {|k| k.underscore}
+  # These attributes may be changed once a project has been created
+  def project_update_params
+    params.permit(:name, :description, :indexPageId, :preview)
+      .transform_keys { |k| k.underscore }
   end
 end
--- server/app/models/builders/project_utility.rb
+++ server/app/models/builders/project_utility.rb
@@ -1,5 +1,6 @@
-module Builders
+require_dependency 'error'
 
+module Builders
   # This Utility class is responsible to create project deirectory and sqlite databse for the project
   class ProjectUtility
     
@@ -8,26 +9,25 @@ module Builders
       @db = db_type
     end
 
-    def generate
+    # Creates the on-disk directory layout to store this project
+    def generate!
       create_project_directory
       create_database
     end
 
+    # Calculates the path the current project should be created at
     def project_path
-      begin
-        base_path = Rails.application.config.sqlino[:projects_dir]
-        full_path = File.join(base_path, @id)
-      rescue => e
-        raise EsqulinoError.new("Project directory already exist: #{e}") if File.exist? full_path
-      end
-      
-      
+      base_path = Rails.application.config.sqlino[:projects_dir]
+      File.join(base_path, @id)
     end
 
+    # Creates the directory for the current project
     def create_project_directory
+      raise EsqulinoError.new("Project directory already exist: #{project_path}") if File.exist? project_path
       Dir.mkdir project_path
     end
 
+    # Creates and initializes the database for the current project
     def create_database
       if @db == 'sqlite3'
         # Creating folders and files
