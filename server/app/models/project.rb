6e3fa7567f04f2c29bbbe92e85fa5a761804acf9
sukanta-ghosh <sukanta.ghosh@xing.com>
2018-02-14 17:01:28 +0100

    Server: use put call for update action and remove attr_accessor

--- server/app/models/project.rb
+++ server/app/models/project.rb
@@ -2,7 +2,7 @@
 # Currently every project is assumed to be somewhat web-centric
 # (using databases and HTML), but this is not set in stone.
 class Project < ApplicationRecord
-  attr_accessor :project_id
+
   has_many :project_sources, dependent: :destroy
   has_many :code_resources
   has_many :block_languages, through: :code_resources
