6397fa1b1231259f798a1f79edb0ba7bb00464bf
Marcus Riemer <Marcus@GurXite.de>
2018-02-14 21:48:52 +0100

    Server: Ported over missing project request specs

--- server/spec/factories/project.rb
+++ server/spec/factories/project.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :project, class: Project do
-    sequence(:name) { |n| "Spec Project ##{n}"}
+    sequence(:name) { |n| "Spec Project #{n}"}
     slug { name.parameterize }
     description "Generated on the fly, used for tests"
     index_page_id nil
-    public true
+    public false
   end
 end
