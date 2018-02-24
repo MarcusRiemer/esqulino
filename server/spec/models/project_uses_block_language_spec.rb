db00d9e50a49e2078aedb05417494ed67570fba8
Marcus Riemer <Marcus@GurXite.de>
2018-02-24 18:00:16 +0100

    Client and Server: Used BlockLanguages may now be removed by the client

--- server/spec/models/project_uses_block_language_spec.rb
+++ server/spec/models/project_uses_block_language_spec.rb
@@ -1,18 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ProjectUsesBlockLanguage, type: :model do
-  it '#key_search_attributes' do
-    used = ProjectUsesBlockLanguage.new(block_language_id: SecureRandom.uuid,
-                                        project_id: SecureRandom.uuid)
-
-    expect(used.key_search_attributes).to eq({
-                                               "block_language_id" => used.block_language_id,
-                                               "project_id" => used.project_id
-                                             })
-  end
-
   it '#to_api_response' do
-    used = ProjectUsesBlockLanguage.new(block_language_id: SecureRandom.uuid,
+    used = ProjectUsesBlockLanguage.new(id: SecureRandom.uuid,
+                                        block_language_id: SecureRandom.uuid,
                                         project_id: SecureRandom.uuid)
 
     expect(used.to_api_response).to validate_against "ProjectUsesBlockLanguageDescription"
