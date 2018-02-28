0875844a4a701d6b652e070cddf8e6c6603e4164
Marcus Riemer <Marcus@GurXite.de>
2018-02-28 18:24:36 +0100

    Client: Property editors now accept keyboard commands

--- client/src/app/editor/tree/draggable.directive.ts
+++ client/src/app/editor/tree/draggable.directive.ts
@@ -19,16 +19,18 @@ export class DraggableDirective {
     ref: ElementRef,
     dragService: DragService
   ) {
+    // Grab the actual element and make it draggable
     const el = ref.nativeElement as HTMLElement;
     el.draggable = true;
 
+    // Listen to to the dragstart event
     el.addEventListener("dragstart", (evt) => {
-      dragService.dragStart(evt, this.node.toModel(), undefined,
-        {
-          node: this.node,
-          codeResource: this.codeResource,
-        }
-      );
+      // And tell the drag service that somethig is beeing dragged once
+      // the event is fired.
+      dragService.dragStart(evt, this.node.toModel(), undefined, {
+        node: this.node,
+        codeResource: this.codeResource,
+      });
     });
   }
 }
