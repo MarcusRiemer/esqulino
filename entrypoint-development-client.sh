225d45523a36d085f4da50bd593ce656609f1749
Marcus Riemer <Marcus@GurXite.de>
2018-04-13 19:02:44 +0200

    Client: Unified Makefile targets
    `dist` is now `client-compile` and `dist-dev` is `client-compile-dev`.

--- entrypoint-development-client.sh
+++ entrypoint-development-client.sh
@@ -6,4 +6,4 @@ useradd -m -u "$USER_UID" -g "$USER_GID" user
 
 su user -c "make -C client clean-deps"
 su user -c "make -C client install-deps"
-NG_OPTS="--watch" su user -c "make --no-print-directory  -C client dist-dev"
+NG_OPTS="--watch" su user -c "make --no-print-directory -C client client-compile-dev"
