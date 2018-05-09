afb27ea604b8815d1d5ea66fd39cc31952e210c0
KLessing <lessing@live.de>
2018-05-09 11:24:10 +0200

    Client: Finished convertArraysToJSON Function with first Testcase

--- client/src/app/shared/csv-parser.spec.ts
+++ client/src/app/shared/csv-parser.spec.ts
@@ -2,27 +2,27 @@ import * as c from './csv-parser'
 
 describe('Util: CSV Parser', () => {
 
-  it('Split Row To Col Easy', () => {
+  it('Split Row To Columns Easy', () => {
     const row = c.splitRowToCols('a', ',', '"');
     expect(row).toEqual(['a']);
   });
 
-  it('Split Row To Col Medium', () => {
+  it('Split Row To Columns Medium', () => {
     const row = c.splitRowToCols('Stunde,Montag,Dienstag,Mittwoch,Donnerstag,Freitag', ',', '"');
     expect(row).toEqual(['Stunde', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag']);
   });
 
-  it('Split Row To Col Hard', () => {
+  it('Split Row To Columns Hard', () => {
     const row = c.splitRowToCols('3,Sport,"Religion (ev, kath)",Kunst,,Kunst', ',', '"');
     expect(row).toEqual(['3', 'Sport', 'Religion (ev, kath)', 'Kunst', '', 'Kunst']);
   });
 
-  it('Split String To Row Easy', () => {
+  it('Split String To Rows Easy', () => {
     const row = c.splitStringToRows('Stunde,Montag,Dienstag,Mittwoch,Donnerstag,Freitag');
     expect(row).toEqual(['Stunde,Montag,Dienstag,Mittwoch,Donnerstag,Freitag']);
   });
 
-  it('Split Row Medium', () => {
+  it('Split String To Rows Medium', () => {
     const row = c.splitStringToRows('Stunde,Montag,Dienstag,Mittwoch,Donnerstag,Freitag\n'
                                     + '1,Mathematik,Deutsch,Englisch,Mathematik,Kunst\n'
                                     + '2,Sport,Französisch,Geschichte,Sport,Geschichte\n'
@@ -33,7 +33,7 @@ describe('Util: CSV Parser', () => {
                          '3,Sport,"Religion (ev, kath)",Kunst,,Kunst']);
   });
 
-  it('Split Row Hard', () => {
+  it('Split String To Rows Hard', () => {
     const row = c.splitStringToRows('Stunde,Montag,Dienstag,Mittwoch,Donnerstag,Freitag\r\n'
                                     + '1,Mathematik,Deutsch,Englisch,Mathematik,Kunst\r\n'
                                     + '2,Sport,Französisch,Geschichte,Sport,Geschichte\r\n'
@@ -49,20 +49,22 @@ describe('Util: CSV Parser', () => {
                   ['2'],
                   ['3']];
     const header = ['Stunde'];
+    const result = { 
+                      'rows':
+                        [
+                          {
+                            'Stunde': '1'
+                          },
+                          {
+                            'Stunde': '2'
+                          },
+                          {
+                            'Stunde': '3'
+                          }
+                        ]
+    }
     const JSONData = c.convertArraysToJSON(data, header, true);
-    expect(JSONData).toEqual();
-  });
-
-  /*
-  it('Convert Arrays To JSON Easy', () => {
-    const data = [['1'], ['Mathematik'], ['Deutsch'],Englisch,Mathematik,Kunst'],
-                  ['2,Sport,Französisch,Geschichte,Sport,Geschichte'],
-                  ['3,Sport,"Religion (ev, kath)",Kunst,,Kunst']];
-    const header = ['Stunde,Montag,Dienstag,Mittwoch,Donnerstag,Freitag'];
-    const JSONData = c.convertArraysToJSON(data, header, true);
-    expect(JSONData).toEqual();
-  });
-  */
-    
+    expect(JSONData).toEqual(result);
+  });    
 
 });
