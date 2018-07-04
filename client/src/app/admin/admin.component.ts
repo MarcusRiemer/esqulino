74703bc1fe50fb2d6c3132854b0f99ee3c915904
Marcus Riemer <Marcus@GurXite.de>
2018-07-04 13:05:15 +0200

    Client: Admin area now uses a master component

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -2,18 +2,12 @@ import { Component, OnInit } from '@angular/core'
 
 import { ServerDataService } from '../shared/server-data.service';
 
+/**
+ * Hosts general menus and layout.
+ */
 @Component({
   templateUrl: 'templates/admin.html'
 })
 export class AdminComponent {
-  constructor(private _serverData: ServerDataService) {
-  }
 
-  public get availableBlockLanguages() {
-    return (this._serverData.availableBlockLanguages);
-  }
-
-  public get availableGrammars() {
-    return (this._serverData.availableGrammars);
-  }
 }
