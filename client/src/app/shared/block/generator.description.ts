5d47c94efa9d263cb07f29d7df25d66c073093a7
Marcus Riemer <Marcus@GurXite.de>
2018-07-05 21:40:45 +0200

    Client: Skeleton for generation of block languages

---
+++ client/src/app/shared/block/generator.description.ts
@@ -0,0 +1,20 @@
+import { GrammarDescription } from '../syntaxtree/grammar.description'
+
+import { EditorComponentDescription } from './block-language.description'
+
+/**
+ * The nested parts of the generator description that must be stored
+ * as a JSON "blob".
+ */
+export interface BlockLanguageGeneratorDocument {
+  editorComponents: EditorComponentDescription[];
+}
+
+/**
+ * Describes how a grammar might be converted to a block language
+ */
+export interface BlockLanguageGeneratorDescription extends BlockLanguageGeneratorDocument {
+  targetName: string;
+  targetId: string;
+  targetProgrammingLanguage: string;
+}
---
+++ client/src/app/shared/block/generator.spec.ts
@@ -0,0 +1,66 @@
+import { GrammarDescription } from '../syntaxtree/grammar.description'
+
+import { BlockLanguageGeneratorDescription } from './generator.description'
+import { convert } from './generator'
+
+describe("BlockLanguage Generator", () => {
+  it("Almost empty grammar with almost no generation instructions", () => {
+    const grammar: GrammarDescription = {
+      id: "008f7fc3-f9a9-4ba3-932d-e7563ef7b31a",
+      name: "g1",
+      root: "t1",
+      types: {
+        "t1": {
+          type: "concrete",
+          attributes: []
+        }
+      }
+    };
+
+    const generator: BlockLanguageGeneratorDescription = {
+      targetId: "74e4fbc4-123e-485f-932a-4a92efece516",
+      editorComponents: [],
+      targetName: "b1",
+      targetProgrammingLanguage: "sql",
+    };
+
+    const r = convert(generator, grammar);
+
+    expect(r.name).toEqual(generator.targetName);
+    expect(r.editorBlocks.length).toEqual(1);
+  });
+
+  it("No blocks for 'oneOf'-types", () => {
+    const grammar: GrammarDescription = {
+      id: "008f7fc3-f9a9-4ba3-932d-e7563ef7b31a",
+      name: "g1",
+      root: "t1",
+      types: {
+        "t1": {
+          type: "oneOf",
+          oneOf: ["t2", "t3"]
+        },
+        "t2": {
+          type: "concrete",
+          attributes: []
+        },
+        "t3": {
+          type: "concrete",
+          attributes: []
+        }
+      }
+    };
+
+    const generator: BlockLanguageGeneratorDescription = {
+      targetId: "74e4fbc4-123e-485f-932a-4a92efece516",
+      editorComponents: [],
+      targetName: "b1",
+      targetProgrammingLanguage: "sql",
+    };
+
+    const r = convert(generator, grammar);
+
+    expect(r.name).toEqual(generator.targetName);
+    expect(r.editorBlocks.length).toEqual(2);
+  });
+});
