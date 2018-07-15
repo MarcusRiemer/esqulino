1bdbe430d1c3694da17ec46fa7b6e30e45dc4d7f
Marcus Riemer <Marcus@GurXite.de>
2018-07-15 16:38:51 +0200

    Server: Removed superflous `family` attribute of block languages

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180715111225) do
+ActiveRecord::Schema.define(version: 20180715141850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20180715111225) do
 
   create_table "block_languages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
-    t.string "family", null: false
     t.jsonb "model"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
