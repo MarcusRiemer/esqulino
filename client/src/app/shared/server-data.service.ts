850b8d88c85d8168ba0f461b07f80ba0620b120f
Marcus Riemer <Marcus@GurXite.de>
2018-07-16 17:23:28 +0200

    Client: UI to create and delete block languages

--- client/src/app/shared/server-data.service.ts
+++ client/src/app/shared/server-data.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Type } from '@angular/core'
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
+import { shareReplay, first } from 'rxjs/operators';
 
 import { ServerApiService } from '../shared/serverapi.service';
 
@@ -19,7 +19,7 @@ import {
 /**
  * Caches descriptions that may be requested from the server.
  */
-class DescriptionCache<T> {
+class IndividualDescriptionCache<T> {
   private cache: { [id: string]: Observable<T> } = {};
 
   public constructor(
@@ -41,6 +41,10 @@ class DescriptionCache<T> {
   }
 }
 
+class CachedRequest<T> {
+
+}
+
 /**
  * Convenient and cached access to server side descriptions.
  */
@@ -52,29 +56,26 @@ export class ServerDataService {
   ) {
   }
 
-  // Caching grammars
-  private readonly grammars = new DescriptionCache<GrammarDescription>(
+  // Caching individual grammars
+  private readonly grammars = new IndividualDescriptionCache<GrammarDescription>(
     this.http,
     id => this._serverApi.getGrammarUrl(id)
   );
 
-  // Caching block languages
-  private readonly blockLanguages = new DescriptionCache<BlockLanguageDescription>(
+  // Caching individual block languages
+  private readonly blockLanguages = new IndividualDescriptionCache<BlockLanguageDescription>(
     this.http,
-    id => this._serverApi.getBlockLanguageUrl(id)
+    id => this._serverApi.individualBlockLanguageUrl(id)
   );
 
-  /**
-   * @return All programming languages that are known on the server
-   */
-  readonly availableProgrammingLanguages = of(["css", "sql", "regex", "dxml-eruby"]);
+  // The HTTP-request that is backing `availableBlockLanguages`
+  private readonly requestBlockLanguages =
+    this.http.get<BlockLanguageListDescription[]>(this._serverApi.getBlockLanguageListUrl())
 
   /**
    * @return All block languages that are known on the server
    */
-  readonly availableBlockLanguages =
-    this.http.get<BlockLanguageListDescription[]>(this._serverApi.getBlockLanguageListUrl())
-      .pipe(shareReplay(1));
+  readonly availableBlockLanguages = this.requestBlockLanguages.pipe(shareReplay(1));
 
   /**
    * @return All block language generators that are known on the server.
@@ -89,6 +90,24 @@ export class ServerDataService {
   readonly availableGrammars =
     this.http.get<GrammarListDescription[]>(this._serverApi.getGrammarListUrl())
       .pipe(shareReplay(1));
+
+  /**
+   *
+   */
+  refreshBlockLanguages() {
+    let sub = this.requestBlockLanguages.subscribe(r => {
+      console.log("Refreshed Block languages");
+      sub.unsubscribe();
+    });
+  }
+
+  deleteBlockLanguage(id: string) {
+    let sub = this.http.delete(this._serverApi.individualBlockLanguageUrl(id))
+      .subscribe(r => {
+        console.log(`Deleted BlockLanguage "${id}"`);
+        sub.unsubscribe();
+      });
+  }
 
   /**
    * @return The details of the specified grammar.
