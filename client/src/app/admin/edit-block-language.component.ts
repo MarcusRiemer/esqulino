cb3bbdc07b35b1000bfd6c68e933b7136bc9eba2
Marcus Riemer <Marcus@GurXite.de>
2018-07-17 14:51:45 +0200

    Client: Block languages may now be saved

--- client/src/app/admin/edit-block-language.component.ts
+++ client/src/app/admin/edit-block-language.component.ts
@@ -1,34 +1,43 @@
 import { Component, OnInit } from '@angular/core'
 import { Router, ActivatedRoute, ParamMap } from '@angular/router'
 
-import { switchMap, map, tap } from 'rxjs/operators';
+import { switchMap, map, tap, first } from 'rxjs/operators';
 
 import { ServerDataService } from '../shared/server-data.service';
+
+import { BlockLanguageDescription } from '../shared/block/block-language.description';
 import { prettyPrintBlockLanguage } from '../shared/block/prettyprint';
 
 @Component({
   templateUrl: 'templates/edit-block-language.html'
 })
-export class EditBlockLanguageComponent {
+export class EditBlockLanguageComponent implements OnInit {
+  public editedSubject: BlockLanguageDescription;
+
   constructor(
     private _serverData: ServerDataService,
-    private _activatedRoute: ActivatedRoute
+    private _activatedRoute: ActivatedRoute,
   ) {
   }
 
-  /**
-   * Mapping the URL parameter to a actual block language
-   */
-  readonly blockLanguage = this._activatedRoute.paramMap.pipe(
-    map((params: ParamMap) => params.get('blockLanguageId')),
-    switchMap((id: string) => this._serverData.getBlockLanguage(id)),
-  );
+  ngOnInit() {
+    this._activatedRoute.paramMap
+      .pipe(
+        map((params: ParamMap) => params.get('blockLanguageId')),
+        switchMap((id: string) => this._serverData.getBlockLanguage(id).pipe(first())),
+    ).subscribe(blockLanguage => {
+      this.editedSubject = blockLanguage;
+    });
+  }
 
   /**
-   * The compiled version of the block language
+   * Compile the block language
    */
-  readonly prettyPrintedBlockLanguage = this.blockLanguage.pipe(
-    map(g => prettyPrintBlockLanguage(g))
-  );
+  prettyPrintBlockLanguage(blockLang: BlockLanguageDescription) {
+    return (prettyPrintBlockLanguage(blockLang));
+  }
 
+  save() {
+    this._serverData.updateBlockLanguage(this.editedSubject);
+  }
 }
