735db223b1e9556be2406f3e0c8679c668771ab9
Marcus Riemer <Marcus@GurXite.de>
2018-07-18 13:44:11 +0200

    Docker: Pointed `entrypoint-test.sh` to new target name

--- entrypoint-test.sh
+++ entrypoint-test.sh
@@ -9,4 +9,4 @@ useradd -m -u "$USER_UID" -g "$USER_GID" user
 su user -c "PKG_CONFIG_PATH=/usr/lib/imagemagick6/pkgconfig make --no-print-directory install-deps"
 su user -c "make --no-print-directory -C client dist-dev"
 su user -c "make --no-print-directory -C server test"
-su user -c "make --no-print-directory -C client test"
+su user -c "make --no-print-directory -C client client-test"
