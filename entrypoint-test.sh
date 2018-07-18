e0d5395f531c41c0c3cd4c7d07ee2063178fed9b
Marcus Riemer <Marcus@GurXite.de>
2018-07-18 14:39:33 +0200

    Server: Swapped in `mini_magick` for `rmagick`

--- entrypoint-test.sh
+++ entrypoint-test.sh
@@ -5,8 +5,7 @@ export TEST_SINGLE_RUN=true
 groupadd -g "$USER_GID" user
 useradd -m -u "$USER_UID" -g "$USER_GID" user
 
-# TODO: Remove PKG_CONFIG_PATH once rmagick works with imagemagick 7
-su user -c "PKG_CONFIG_PATH=/usr/lib/imagemagick6/pkgconfig make --no-print-directory install-deps"
+su user -c "make --no-print-directory install-deps"
 su user -c "make --no-print-directory -C client dist-dev"
 su user -c "make --no-print-directory -C server test"
 su user -c "make --no-print-directory -C client client-test"
