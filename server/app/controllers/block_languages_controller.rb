53f77d6ef06a5dc8bb5ac9e34f34c39290ffbc33
Marcus Riemer <Marcus@GurXite.de>
2018-07-21 14:41:54 +0200

    Server: Limited SQL retrieval scope of BlockLanguage listings

--- server/app/controllers/block_languages_controller.rb
+++ server/app/controllers/block_languages_controller.rb
@@ -2,7 +2,7 @@
 class BlockLanguagesController < ApplicationController
   # List all existing block languages
   def index
-    render :json => BlockLanguage.all.map{|b| b.to_list_api_response}
+    render :json => BlockLanguage.scope_list.map{|b| b.to_list_api_response}
   end
 
   # Find a single block language
