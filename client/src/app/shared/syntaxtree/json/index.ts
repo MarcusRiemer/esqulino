1310542d2cd5a0242fdf843ebe458aa79d355baf
Marcus Riemer <Marcus@GurXite.de>
2018-07-22 13:18:03 +0200

    Client: Code generation for JSON documents

--- client/src/app/shared/syntaxtree/json/index.ts
+++ client/src/app/shared/syntaxtree/json/index.ts
@@ -1,11 +1,11 @@
 import { LanguageDescription } from '../language.description'
 
-// import { NODE_CONVERTER, NODE_CONVERTER } from './dxml.codegenerator'
+import { NODE_CONVERTER } from './json.codegenerator'
 import { GRAMMAR_DESCRIPTION } from './json.grammar'
 
 export const LANGUAGE_DESCRIPTION: LanguageDescription = {
   id: "json",
   name: "JSON",
-  emitters: [],
+  emitters: NODE_CONVERTER,
   validators: [GRAMMAR_DESCRIPTION]
 }
