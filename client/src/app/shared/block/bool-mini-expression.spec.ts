7e8b1cd727ae8adf49665243a39121b32d983206
Marcus Riemer <Marcus@GurXite.de>
2018-08-02 15:55:25 +0200

    Client: Boolean mini expressions may now evaluate some variables lazily

--- client/src/app/shared/block/bool-mini-expression.spec.ts
+++ client/src/app/shared/block/bool-mini-expression.spec.ts
@@ -7,9 +7,11 @@ describe("Boolean Mini Expressions", () => {
   });
 
   it(`Variables`, () => {
-    expect(evalExpression({ $var: "foo" }, { "foo": true })).toBe(true);
-    expect(evalExpression({ $var: "foo" }, { "foo": false })).toBe(false);
-    expect(evalExpression<string>({ $var: "foo" }, { "bar": true })).toBe(false);
+    expect(evalExpression({ $var: "foo" }, { "foo": true })).toBe(true, "Constant true");
+    expect(evalExpression({ $var: "foo" }, { "foo": false })).toBe(false, "Constant false");
+    expect(evalExpression({ $var: "foo" }, { "foo": () => true })).toBe(true, "Lazy true");
+    expect(evalExpression({ $var: "foo" }, { "foo": () => false })).toBe(false, "Lazy false");
+    expect(evalExpression<string>({ $var: "foo" }, { "bar": true })).toBe(false, "Missing");
   });
 
   it(`Negation`, () => {
@@ -43,5 +45,56 @@ describe("Boolean Mini Expressions", () => {
     expect(evalExpression(expr, { "ifLegalDrag": true, "ifEmpty": false })).toBe(false);
     expect(evalExpression(expr, { "ifLegalDrag": false, "ifEmpty": true })).toBe(false);
     expect(evalExpression(expr, { "ifLegalDrag": false, "ifEmpty": false })).toBe(false);
+  });
+
+  it(`Lazy evaluation: Two values`, () => {
+    let v1Called = false;
+    let v2Called = false;
+
+    const res = evalExpression<"v1" | "v2">(
+      { $var: "v1" },
+      {
+        "v1": () => { v1Called = true; return (true) },
+        "v2": () => { v2Called = true; return (true) }
+      }
+    )
+
+    expect(res).toBe(true);
+    expect(v1Called).toBe(true);
+    expect(v2Called).toBe(false);
+  });
+
+  it(`Lazy evaluation: Some`, () => {
+    let v1Called = false;
+    let v2Called = false;
+
+    const res = evalExpression<"v1" | "v2">(
+      { $some: [{ $var: "v1" }, { $var: "v2" }] },
+      {
+        "v1": () => { v1Called = true; return (true) },
+        "v2": () => { v2Called = true; return (true) }
+      }
+    )
+
+    expect(res).toBe(true, "result");
+    expect(v1Called).toBe(true, "v1Called");
+    expect(v2Called).toBe(false, "v2Called");
+  });
+
+  it(`Lazy evaluation: Every`, () => {
+    let v1Called = false;
+    let v2Called = false;
+
+    const res = evalExpression<"v1" | "v2">(
+      { $every: [{ $var: "v1" }, { $var: "v2" }] },
+      {
+        "v1": () => { v1Called = true; return (false) },
+        "v2": () => { v2Called = true; return (false) }
+      }
+    )
+
+    expect(res).toBe(false, "result");
+    expect(v1Called).toBe(true, "v1Called");
+    expect(v2Called).toBe(false, "v2Called");
   });
 });
