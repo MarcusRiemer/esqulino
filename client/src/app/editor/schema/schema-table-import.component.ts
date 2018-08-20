dcfbdcb34e8bc13cae405dc3b99bf1faf7510670
kl <lessing@live.de>
2018-08-20 15:31:53 +0200

    Client: Added Import Component with Route

---
+++ client/src/app/editor/schema/schema-table-import.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+/**
+ * Displays the schema for a list of tables.
+ */
+@Component({
+  template: `<h1>Hello World</h1>`,
+  selector: "sql-table-import"
+})
+export class SchemaTableImportComponent {
+
+}
