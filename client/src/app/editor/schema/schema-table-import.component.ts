79f8febd2626ee418a3813e1ddba54d06bdb710e
kl <lessing@live.de>
2018-08-25 17:53:19 +0200

    Client: Use csv parser in component

--- client/src/app/editor/schema/schema-table-import.component.ts
+++ client/src/app/editor/schema/schema-table-import.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Parser from '../../shared/csv-parser';
 
 /**
  * Displays the schema for a list of tables.
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
   templateUrl: 'templates/schema-table-import.html',
   selector: "sql-table-import"
 })
-export class SchemaTableImportComponent {
+export class SchemaTableImportComponent implements OnInit {
+  row:string[];
 
+  constructor() {
+    console.log("constructor ran");
+
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit ran");
+
+    this.row = Parser.splitStringToRows('Stunde,Montag,Dienstag,Mittwoch,Donnerstag,Freitag');
+
+    console.log(this.row);
+  }
 }
+
+  /* ----- Frontend ----- */
+  
+  // Route in schema
+  // Add to Module
+  // Write Component
+
+  // Upload Button
+  // Settings
+  // Preview Below
+
+  // Bootstrap style error line for Error with information
+  // Show only the first 20 lines if file to large and no parsing error occured
