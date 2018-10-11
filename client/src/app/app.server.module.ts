ffdde3988e09b8df89ccf8a7fc3e5c39b0d7f8b0
Marcus Riemer <Marcus@GurXite.de>
2018-10-11 16:33:54 +0200

    Client: Attempted to fix universal rendering

--- client/src/app/app.server.module.ts
+++ client/src/app/app.server.module.ts
@@ -1,37 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { Routes, RouterModule } from '@angular/router'
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
+import { AppModule } from './app.module'
 import { SqlScratchComponent } from './app.component';
-
-import { SharedAppModule } from './shared/shared.module';
-
-import { FrontModule } from './front/front.module';
-import { frontRoutes } from './front/front.routes'
-
-const serverRoutes: Routes = [
-  {
-    path: '',
-    redirectTo: '/about',
-    pathMatch: 'full',
-  },
-  {
-    path: 'about',
-    children: frontRoutes
-  }
-]
-
-export const routing = RouterModule.forRoot(serverRoutes, {
-  paramsInheritanceStrategy: "always"
-});
-
 
 @NgModule({
   imports: [
-    SharedAppModule.forRoot(),
-    FrontModule,
+    AppModule,
     ServerModule,
-    routing
+    ModuleMapLoaderModule
   ],
   bootstrap: [SqlScratchComponent]
 })
