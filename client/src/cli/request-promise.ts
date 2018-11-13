9a6476187f9d8a5def8a4356e4477b6965f99b16
Marcus Riemer <Marcus@GurXite.de>
2018-11-13 11:20:31 +0100

    Server: Now uses `falcon` as Rails-server, should fix hickups on concurrent requests

--- client/src/cli/request-promise.ts
+++ client/src/cli/request-promise.ts
@@ -32,68 +32,61 @@ function getRandomInt(min: number, max: number): number {
  */
 export function httpRequest<T>(url: URL, method: "GET" | "POST" | "PUT", body: any = "") {
   const p = new Promise<T>((resolve, reject) => {
-    // Warning: Hack ahead!
-    // Because we do not want to hammer the server with all requests
-    // at the exact same time, we introduce some kind of delay. This
-    // function contains the code that is actually executed, but with delay.
-    const impl = () => {
-      // Build the complicated request options object
-      // This is basicly the same as the URL + required headers + the method
-      const options: http.RequestOptions = {
-        method: method,
-        hostname: url.hostname,
-        port: url.port,
-        protocol: url.protocol,
-        path: url.pathname,
-        headers: {
-          "Content-Type": "application/json"
-        },
-      }
 
-      const buffer = body instanceof Buffer ? body : Buffer.from(JSON.stringify(body), 'utf8');
-      options.headers["Content-Length"] = buffer.byteLength;
-
-      // Build the request object
-      const request = http.request(options, response => {
-        // temporary data holder
-        const responseBody = [];
-
-        // on every content chunk, push it to the data array
-        response.on('data', (chunk) => {
-          responseBody.push(chunk);
-        });
-
-        // We are done, resolve promise with those joined chunks
-        response.on('end', () => {
-          const result = tryParseJson(responseBody.join(''));
-
-          if (response.statusCode < 200 || response.statusCode > 299) {
-            reject({
-              statusCode: response.statusCode,
-              message: result
-            });
-          } else {
-            resolve(result as any);
-          }
-        });
-      });
-
-      // handle connection errors of the request
-      request.on('error', (err) => reject(`ERROR ${method}: ${url.toString()}\n${JSON.stringify(err)}`));
-
-      // Pump data into the request (if its a POST request)
-      if (buffer.length > 0) {
-        request.write(buffer);
-      }
-
-      console.error(`${method}: ${url.toString()} (${buffer.length} byte request body)`);
-
-      // And hit off the request for good
-      request.end();
+    // Build the complicated request options object
+    // This is basicly the same as the URL + required headers + the method
+    const options: http.RequestOptions = {
+      method: method,
+      hostname: url.hostname,
+      port: url.port,
+      protocol: url.protocol,
+      path: url.pathname,
+      headers: {
+        "Content-Type": "application/json"
+      },
     }
 
-    // Don't hammer the server with all requests simultaneously
-    setTimeout(impl, getRandomInt(200, 1000));
+    const buffer = body instanceof Buffer ? body : Buffer.from(JSON.stringify(body), 'utf8');
+    options.headers["Content-Length"] = buffer.byteLength;
+
+    // Build the request object
+    const request = http.request(options, response => {
+      // temporary data holder
+      const responseBody = [];
+
+      // on every content chunk, push it to the data array
+      response.on('data', (chunk) => {
+        responseBody.push(chunk);
+      });
+
+      // We are done, resolve promise with those joined chunks
+      response.on('end', () => {
+        const result = tryParseJson(responseBody.join(''));
+
+        if (response.statusCode < 200 || response.statusCode > 299) {
+          reject({
+            statusCode: response.statusCode,
+            message: result
+          });
+        } else {
+          resolve(result as any);
+        }
+      });
+    });
+
+    // handle connection errors of the request
+    request.on('error', (err) => reject(`ERROR ${method}: ${url.toString()}\n${JSON.stringify(err)}`));
+
+    // Pump data into the request (if its a POST request)
+    if (buffer.length > 0) {
+      request.write(buffer);
+    }
+
+    console.error(`${method}: ${url.toString()} (${buffer.length} byte request body)`);
+
+    // And hit off the request for good
+    request.end();
+
   });
 
   return (p);
