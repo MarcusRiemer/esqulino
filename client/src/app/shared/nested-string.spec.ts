1f62636ed1a6096673f7235e139053d86bac0c43
Marcus Riemer <Marcus@GurXite.de>
2018-12-20 09:11:08 +0100

    Client: Nested strings no skip over `undefined` values

--- client/src/app/shared/nested-string.spec.ts
+++ client/src/app/shared/nested-string.spec.ts
@@ -2,22 +2,28 @@ import * as n from './nested-string'
 
 describe('Util: Nested String', () => {
 
-  it('Helper "recursiveJoin": No nesting', () => {
+  it('Undefined', () => {
+    expect(n.recursiveJoin("s", "i", undefined)).toEqual("");
+    expect(n.recursiveJoin("s", "i", [undefined, "a"])).toEqual("a");
+    expect(n.recursiveJoin("s", "i", ["a", undefined])).toEqual("a");
+  });
+
+  it('No nesting', () => {
     const r = n.recursiveJoin("a", "x", ["1", "2", "3"]);
     expect(r).toEqual("1a2a3");
   });
 
-  it('Helper "recursiveJoin": First level', () => {
+  it('First level', () => {
     expect(n.recursiveJoin("a", "x", [["1", "2", "3"]])).toEqual("x1ax2ax3");
     expect(n.recursiveJoin("b", "x", [["1"], "2", ["3"]])).toEqual("x1b2bx3");
     expect(n.recursiveJoin("c", "x", [["1"], ["2"], ["3"]])).toEqual("x1cx2cx3");
   });
 
-  it('Helper "recursiveJoin": Second level', () => {
+  it('Second level', () => {
     expect(n.recursiveJoin("a", "x", [[["1"], "2", "3"]])).toEqual("xx1ax2ax3");
   });
 
-  it('Helper "recursiveJoin": Practical examples', () => {
+  it('Practical examples', () => {
     expect(n.recursiveJoin("\n", "  ", ["grammar g {", ["node"], "}"])).toEqual("grammar g {\n  node\n}");
     expect(n.recursiveJoin("\n", "  ", ['grammar "g1" {', ['node "n1" {', '}'], ['node "n2" {', '}'], '}'])).toEqual(`grammar "g1" {\n  node "n1" {\n  }\n  node "n2" {\n  }\n}`);
   });
