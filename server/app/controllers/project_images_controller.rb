c12346f54abb20fe1db76bb9dd86a839de21c8d5
Marcus Riemer <Marcus@GurXite.de>
2018-12-22 15:06:17 +0100

    Server: Complete set of tests for project image upload

--- server/app/controllers/project_images_controller.rb
+++ server/app/controllers/project_images_controller.rb
@@ -19,39 +19,60 @@ class ProjectImagesController < ApplicationController
 
   def file_update
     ensure_write_access do
-      if params['image-file'].nil? or params['image-file'].tempfile.nil? then
+      if params['image-file'].nil? or params['image-file'].tempfile.nil? or params['image_id'].nil? then
         render :status => 400
       else
-        Image.new(current_project, params['image_id']).file_update!(params['image-file'].tempfile)
-        render status => :success
+        image = Image.new(current_project, params['image_id'])
+        if image.exists?
+          image.file_update!(params['image-file'].tempfile)
+          render :status => 200
+        else
+          render :status => 404
+        end
       end
     end
   end
 
   def file_delete
     ensure_write_access do
-      Image.new(current_project, params['image_id']).file_destroy!
+      image = Image.new(current_project, params['image_id'])
+
+      if image.exists?
+        image.file_destroy!
+        render :status => 200
+      else
+        render :status => 404
+      end
     end
   end
 
   def metadata_show
-    image_id = params['image_id']
-    render status => :success, json: Image.new(current_project, image_id).metadata_show
+    image = Image.new(current_project, params['image_id'])
+
+    if image.exists?
+      render :status => 200, :json => image.metadata_show
+    else
+      render :status => 404
+    end
   end
 
   def metadata_update
     ensure_write_access do
       image_id = params['image_id']
-      metadata = Image.metadata_create(params['image-name'], params['author-name'], params['author-url'], params['licence-name'], params['licence-url'])
-      img = Image.new(current_project, image_id)
-      img.metadata_update(metadata)
-      img.metadata_save
-      render status => :success, json: img.metadata_show
+      image = Image.new(current_project, image_id)
+      if image.exists?
+        metadata = Image.metadata_create(params['image-name'], params['author-name'], params['author-url'], params['licence-name'], params['licence-url'])
+        image.metadata_update(metadata)
+        image.metadata_save
+        render :status => 200, :json => image.metadata_show
+      else
+        render :status => 404
+      end
     end
   end
 
   def list_show
-    render status => :success, json: Image.metadata_get_from_file(current_project)
+    render :status => 200, json: Image.metadata_get_from_file(current_project)
   end
 
   private
