be5ae13e7efe657604dfb59ebf3b19b6baaae0e3
Marcus Riemer <Marcus@GurXite.de>
2019-01-10 15:17:50 +0100

    Server: Mailer bugfixes, should now send on project creation

--- server/app/mailers/project_mailer.rb
+++ server/app/mailers/project_mailer.rb
@@ -1,11 +1,10 @@
 class ProjectMailer < ApplicationMailer
   # A new project was created, the admin should know
-  def created_admin(project_id, project_name)
-    @project_id = project_id
-    @project_name = project_name
-    @project_visitor_url = "http://#{project_id}.#{project_site_url}"
-    @project_editor_url = "http://#{project_editor_url}/editor/#{project_id}"
+  def created_admin
+    @project = @params[:project]
+    @project_visitor_url = "http://#{@project.id}.#{project_site_url}"
+    @project_editor_url = "http://#{project_editor_url}/editor/#{@project.id}"
 
-    mail(to: admin_mail, subject: "New Project: #{project_name} (#{project_id})")
+    mail(to: admin_mail, subject: "New Project: #{@project.name} (#{@project.id})")
   end
 end
