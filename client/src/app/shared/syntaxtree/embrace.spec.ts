89ce8519ce7ea0112cb7924418102b297482c819
Marcus Riemer <Marcus@GurXite.de>
2019-02-02 20:53:17 +0100

    Client: Added more embrace-testcases

--- client/src/app/shared/syntaxtree/embrace.spec.ts
+++ client/src/app/shared/syntaxtree/embrace.spec.ts
@@ -14,7 +14,7 @@ const EXPRESSION_GRAMMAR: GrammarDescription = {
   types: {
     "booleanExpression": {
       type: "oneOf",
-      oneOf: ["booleanBinary", "booleanConstant", "negate"]
+      oneOf: ["booleanBinary", "booleanConstant", "negate", "parentheses"]
     },
     "booleanConstant": {
       type: "concrete",
@@ -27,6 +27,16 @@ const EXPRESSION_GRAMMAR: GrammarDescription = {
       ]
     },
     "negate": {
+      type: "concrete",
+      attributes: [
+        {
+          type: "allowed",
+          name: "expr",
+          nodeTypes: ["booleanExpression"]
+        }
+      ]
+    },
+    "parentheses": {
       type: "concrete",
       attributes: [
         {
@@ -59,6 +69,9 @@ const EXPRESSION_GRAMMAR: GrammarDescription = {
 };
 
 describe('AST: Embracing', () => {
+
+  // ######################################################################
+
   describe('_findPossibleLocations', () => {
 
     it('false with constant => []', () => {
@@ -91,7 +104,23 @@ describe('AST: Embracing', () => {
       expect(res).toEqual([[["expr", 0]]]);
     });
 
-    it('binary(false, <hole>) with constant => [[rhs, 0]]', () => {
+    it('binary(<hole>, <hole>) with constant', () => {
+      const parentDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanBinary",
+        children: {
+          "lhs": [],
+          "rhs": []
+        }
+      }
+      const fillType: QualifiedTypeName = { languageName: "expr", typeName: "booleanConstant" };
+      const v = new Validator([EXPRESSION_GRAMMAR]);
+
+      const res = _findPossibleLocations(new Node(parentDesc, undefined), fillType, v);
+      expect(res).toEqual([[["lhs", 0]], [["rhs", 0]]]);
+    });
+
+    it('binary(false, <hole>) with constant', () => {
       const parentDesc: NodeDescription = {
         language: "expr",
         name: "booleanBinary",
@@ -115,7 +144,7 @@ describe('AST: Embracing', () => {
       expect(res).toEqual([[["rhs", 0]]]);
     });
 
-    it('binary(<hole>, false) with constant => [[lhs, 0]]', () => {
+    it('binary(<hole>, false) with constant', () => {
       const parentDesc: NodeDescription = {
         language: "expr",
         name: "booleanBinary",
@@ -138,7 +167,41 @@ describe('AST: Embracing', () => {
       const res = _findPossibleLocations(new Node(parentDesc, undefined), fillType, v);
       expect(res).toEqual([[["lhs", 0]]]);
     });
+
+    it('binary(true, false) with constant', () => {
+      const parentDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanBinary",
+        children: {
+          "lhs": [
+            {
+              language: "expr",
+              name: "booleanConstant",
+              properties: {
+                "value": "true"
+              }
+            }
+          ],
+          "rhs": [
+            {
+              language: "expr",
+              name: "booleanConstant",
+              properties: {
+                "value": "false"
+              }
+            }
+          ]
+        }
+      }
+      const fillType: QualifiedTypeName = { languageName: "expr", typeName: "booleanConstant" };
+      const v = new Validator([EXPRESSION_GRAMMAR]);
+
+      const res = _findPossibleLocations(new Node(parentDesc, undefined), fillType, v);
+      expect(res).toEqual([]);
+    });
   });
+
+  // ######################################################################
 
   describe('_localEmbrace', () => {
 
@@ -174,6 +237,8 @@ describe('AST: Embracing', () => {
       expect(res).toEqual(expDescription);
     });
   });
+
+  // ######################################################################
 
   describe('_findMatch', () => {
     it('false against [not(<hole>)]', () => {
@@ -241,87 +306,336 @@ describe('AST: Embracing', () => {
     });
   });
 
-  it('<emptyTree> => not(<hole>)', () => {
-    const embraceCandidates: NodeDescription[] = [
-      {
+  // ######################################################################
+
+  describe(`embraceNode()`, () => {
+
+    it('<emptyTree> => not(<hole>)', () => {
+      const embraceCandidates: NodeDescription[] = [
+        {
+          language: "expr",
+          name: "negate",
+          children: {
+            "expr": []
+          }
+        }
+      ];
+
+      const validator = new Validator([EXPRESSION_GRAMMAR]);
+
+      const prev = new Tree(undefined);
+      const curr = embraceNode(validator, prev, [], embraceCandidates);
+
+      const expTreeDesc: NodeDescription = {
         language: "expr",
         name: "negate",
         children: {
           "expr": []
         }
-      }
-    ];
+      };
+      expect(curr.toModel()).toEqual(expTreeDesc);
+    });
 
-    const validator = new Validator([EXPRESSION_GRAMMAR]);
+    it('false => not(<false>)', () => {
+      const inTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanConstant",
+        properties: {
+          "value": "false"
+        }
+      };
 
-    const prev = new Tree(undefined);
-    const curr = embraceNode(validator, prev, [], embraceCandidates);
+      const embraceCandidates: NodeDescription[] = [
+        {
+          language: "expr",
+          name: "negate",
+          children: {
+            "expr": []
+          }
+        }
+      ];
 
-    const expTreeDesc: NodeDescription = {
-      language: "expr",
-      name: "negate",
-      children: {
-        "expr": []
-      }
-    };
-    expect(curr.toModel()).toEqual(expTreeDesc);
-  });
+      const validator = new Validator([EXPRESSION_GRAMMAR]);
 
-  it('false => not(false)', () => {
-    const inTreeDesc: NodeDescription = {
-      language: "expr",
-      name: "booleanConstant",
-      properties: {
-        "value": "false"
-      }
-    };
+      const prev = new Tree(inTreeDesc);
+      const curr = embraceNode(validator, prev, [], embraceCandidates);
 
-    const embraceCandidates: NodeDescription[] = [
-      {
+      const expTreeDesc: NodeDescription = {
         language: "expr",
         name: "negate",
         children: {
-          "expr": []
+          "expr": [inTreeDesc]
         }
-      }
-    ];
+      };
+      expect(curr.toModel()).toEqual(expTreeDesc);
+    });
 
-    const validator = new Validator([EXPRESSION_GRAMMAR]);
-
-    const prev = new Tree(inTreeDesc);
-    const curr = embraceNode(validator, prev, [], embraceCandidates);
-
-    const expTreeDesc: NodeDescription = {
-      language: "expr",
-      name: "negate",
-      children: {
-        "expr": [inTreeDesc]
-      }
-    };
-    expect(curr.toModel()).toEqual(expTreeDesc);
-  });
-
-  it(`true => true (candidates don't match)`, () => {
-    const inTreeDesc: NodeDescription = {
-      language: "expr",
-      name: "booleanConstant",
-      properties: {
-        "value": "false"
-      }
-    };
-
-    const embraceCandidates: NodeDescription[] = [
-      {
+    it('not(false) => not(<not(false)>)', () => {
+      const inTreeDesc: NodeDescription = {
         language: "expr",
-        name: "noMatch"
-      }
-    ];
+        name: "negate",
+        children: {
+          "expr": [
+            {
+              language: "expr",
+              name: "booleanConstant",
+              properties: {
+                "value": "false"
+              }
+            }
+          ]
+        }
+      };
 
-    const validator = new Validator([EXPRESSION_GRAMMAR]);
+      const embraceCandidates: NodeDescription[] = [
+        {
+          language: "expr",
+          name: "negate",
+          children: {
+            "expr": []
+          }
+        }
+      ];
 
-    const prev = new Tree(inTreeDesc);
-    const curr = embraceNode(validator, prev, [], embraceCandidates);
+      const validator = new Validator([EXPRESSION_GRAMMAR]);
 
-    expect(curr.toModel()).toEqual(inTreeDesc);
+      const prev = new Tree(inTreeDesc);
+      const curr = embraceNode(validator, prev, [], embraceCandidates);
+
+      const expTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "negate",
+        children: {
+          "expr": [inTreeDesc]
+        }
+      };
+      expect(curr.toModel()).toEqual(expTreeDesc);
+    });
+
+    it('false => binary(<false>, <hole>)', () => {
+      const inTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanConstant",
+        properties: {
+          "value": "false"
+        }
+      };
+
+      const embraceCandidates: NodeDescription[] = [
+        {
+          language: "expr",
+          name: "booleanBinary",
+          children: {
+            "lhs": [],
+            "rhs": []
+          }
+        }
+      ];
+
+      const validator = new Validator([EXPRESSION_GRAMMAR]);
+
+      const prev = new Tree(inTreeDesc);
+      const curr = embraceNode(validator, prev, [], embraceCandidates);
+
+      const expTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanBinary",
+        children: {
+          "lhs": [inTreeDesc],
+          "rhs": []
+        }
+      };
+      expect(curr.toModel()).toEqual(expTreeDesc);
+    });
+
+    it('false => binary(true, <false>)', () => {
+      const inTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanConstant",
+        properties: {
+          "value": "false"
+        }
+      };
+
+      const embraceCandidates: NodeDescription[] = [
+        {
+          language: "expr",
+          name: "booleanBinary",
+          children: {
+            "lhs": [
+              {
+                language: "expr",
+                name: "booleanConstant",
+                properties: {
+                  "value": "true"
+                }
+              }
+            ],
+            "rhs": []
+          }
+        }
+      ];
+
+      const validator = new Validator([EXPRESSION_GRAMMAR]);
+
+      const prev = new Tree(inTreeDesc);
+      const curr = embraceNode(validator, prev, [], embraceCandidates);
+
+      const expTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanBinary",
+        children: {
+          "lhs": [embraceCandidates[0].children["lhs"][0]],
+          "rhs": [inTreeDesc]
+        }
+      };
+      expect(curr.toModel()).toEqual(expTreeDesc);
+    });
+
+    it('binary(true, <hole>) => not(<binary(true, <hole>)>)', () => {
+      const inTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanBinary",
+        children: {
+          "lhs": [
+            {
+              language: "expr",
+              name: "booleanConstant",
+              properties: {
+                "value": "true"
+              }
+            }
+          ],
+          "rhs": []
+        }
+      };
+
+      const embraceCandidates: NodeDescription[] = [
+        {
+          language: "expr",
+          name: "negate",
+          children: {
+            "expr": []
+          }
+        }
+      ];
+
+      const validator = new Validator([EXPRESSION_GRAMMAR]);
+
+      const prev = new Tree(inTreeDesc);
+      const curr = embraceNode(validator, prev, [], embraceCandidates);
+
+      const expTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "negate",
+        children: {
+          "expr": [inTreeDesc]
+        }
+      };
+      expect(curr.toModel()).toEqual(expTreeDesc);
+    });
+
+    it(`true => true (candidates don't match)`, () => {
+      const inTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanConstant",
+        properties: {
+          "value": "false"
+        }
+      };
+
+      const embraceCandidates: NodeDescription[] = [
+        {
+          language: "expr",
+          name: "noMatch"
+        }
+      ];
+
+      const validator = new Validator([EXPRESSION_GRAMMAR]);
+
+      const prev = new Tree(inTreeDesc);
+      const curr = embraceNode(validator, prev, [], embraceCandidates);
+
+      expect(curr.toModel()).toEqual(inTreeDesc);
+    });
+
+    it(`true => not(<true>) (second candidate matches)`, () => {
+      const inTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanConstant",
+        properties: {
+          "value": "false"
+        }
+      };
+
+      const embraceCandidates: NodeDescription[] = [
+        {
+          language: "expr",
+          name: "noMatch"
+        },
+        {
+          language: "expr",
+          name: "negate",
+          children: {
+            "expr": []
+          }
+        }
+      ];
+
+      const validator = new Validator([EXPRESSION_GRAMMAR]);
+
+      const prev = new Tree(inTreeDesc);
+      const curr = embraceNode(validator, prev, [], embraceCandidates);
+
+      const expTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "negate",
+        children: {
+          "expr": [inTreeDesc]
+        }
+      };
+      expect(curr.toModel()).toEqual(expTreeDesc);
+    });
+
+    it(`true => (<true>) (first candidate takes precedence)`, () => {
+      const inTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanConstant",
+        properties: {
+          "value": "false"
+        }
+      };
+
+      const embraceCandidates: NodeDescription[] = [
+        {
+          language: "expr",
+          name: "parentheses",
+          children: {
+            "expr": []
+          }
+        },
+        {
+          language: "expr",
+          name: "negate",
+          children: {
+            "expr": []
+          }
+        }
+      ];
+
+      const validator = new Validator([EXPRESSION_GRAMMAR]);
+
+      const prev = new Tree(inTreeDesc);
+      const curr = embraceNode(validator, prev, [], embraceCandidates);
+
+      const expTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "parentheses",
+        children: {
+          "expr": [inTreeDesc]
+        }
+      };
+      expect(curr.toModel()).toEqual(expTreeDesc);
+    });
   });
 });
