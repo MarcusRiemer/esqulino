d158558d3256a020df67649c788e5276d97e5500
Marcus Riemer <Marcus@GurXite.de>
2019-02-14 17:35:46 +0100

    Client: Dropping exact matches now takes cardinality into account

---
+++ client/src/app/shared/syntaxtree/drop-util.spec.ts
@@ -0,0 +1,137 @@
+import { NodeDescription } from './syntaxtree.description';
+import { Tree } from './syntaxtree';
+import { Validator } from './validator';
+import { _cardinalityAllowsInsertion } from './drop-util';
+import { BOOLEAN_GRAMMAR } from './boolean-expression.spec';
+import { ErrorCodes } from './validation-result';
+
+describe('Drop Utils', () => {
+  describe('_cardinalityAllowsInsertion', () => {
+    it('Full binary expression', () => {
+      const inTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanBinary",
+        children: {
+          "lhs": [
+            {
+              language: "expr",
+              name: "booleanConstant",
+              properties: {
+                "value": "true"
+              }
+            }
+          ],
+          "rhs": [
+            {
+              language: "expr",
+              name: "booleanConstant",
+              properties: {
+                "value": "true"
+              }
+            }
+          ]
+        },
+        properties: {
+          "operator": "+"
+        }
+      };
+
+      const candidateDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanConstant",
+        properties: {
+          "value": "false"
+        }
+      }
+
+      const validator = new Validator([BOOLEAN_GRAMMAR]);
+      const inNode = new Tree(inTreeDesc).rootNode;
+
+      expect(validator.validateFromRoot(inNode).errors).toEqual([]);
+
+      // Inserting to a filled right hand side
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "lhs", 0)).toBe(false);
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "lhs", 1)).toBe(false);
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "lhs", 2)).toBe(false);
+
+      // Inserting to a filled left hand side
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "rhs", 0)).toBe(false);
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "rhs", 1)).toBe(false);
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "rhs", 2)).toBe(false);
+    });
+
+
+    it('Partial binary expression', () => {
+      const inTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanBinary",
+        children: {
+          "lhs": [
+            {
+              language: "expr",
+              name: "booleanConstant",
+              properties: {
+                "value": "true"
+              }
+            }
+          ],
+          "rhs": []
+        },
+        properties: {
+          "operator": "+"
+        }
+      };
+
+      const candidateDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanConstant",
+        properties: {
+          "value": "false"
+        }
+      }
+
+      const validator = new Validator([BOOLEAN_GRAMMAR]);
+      const inNode = new Tree(inTreeDesc).rootNode;
+
+      expect(validator.validateFromRoot(inNode).errors.map(e => e.code))
+        .toEqual([ErrorCodes.InvalidMinOccurences]);
+
+      // Inserting to a filled right hand side
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "lhs", 0)).toBe(false);
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "lhs", 1)).toBe(false);
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "lhs", 2)).toBe(false);
+
+      // Inserting to an empty left hand side
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "rhs", 0)).toBe(true);
+    });
+
+    it('Empty binary expression', () => {
+      const inTreeDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanBinary",
+        children: {
+          "lhs": [],
+          "rhs": []
+        },
+        properties: {
+          "operator": "+"
+        }
+      };
+
+      const candidateDesc: NodeDescription = {
+        language: "expr",
+        name: "booleanConstant",
+        properties: {
+          "value": "false"
+        }
+      }
+
+      const validator = new Validator([BOOLEAN_GRAMMAR]);
+      const inNode = new Tree(inTreeDesc).rootNode;
+
+      // Insertion is valid at both sides
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "lhs", 0)).toBe(true);
+      expect(_cardinalityAllowsInsertion(validator, inNode, candidateDesc, "rhs", 0)).toBe(true);
+    });
+  });
+});
--- client/src/app/shared/syntaxtree/drop.description.ts
+++ client/src/app/shared/syntaxtree/drop.description.ts
@@ -1,17 +1,32 @@
 import { NodeLocation, NodeDescription } from "./syntaxtree.description";
 
 /**
+ * Possibilities to take into account when making a smart drop.
+ */
+export interface SmartDropOptions {
+  allowEmbrace?: boolean;
+  allowAnyParent?: boolean;
+  allowExact?: boolean;
+};
+
+/**
  * A drop operation that would not "worsen" the tree by violating
  * basic type or cardinality laws.
  */
 export type SmartDropLocation = InsertDropLocation | EmbraceDropLocation;
 
+/**
+ * An insertion at the given location.
+ */
 export interface InsertDropLocation {
   operation: "insert";
   location: NodeLocation;
   nodeDescription: NodeDescription;
 }
 
+/**
+ * An embrace at the given location.
+ */
 export interface EmbraceDropLocation {
   operation: "embrace";
   location: NodeLocation;
