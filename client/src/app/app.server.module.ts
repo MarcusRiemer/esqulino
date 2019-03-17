c799a97b13b73b46a8cf69bd45a6d5bd6c84c4da
Luca <mail@lucakiehn.de>
2019-03-17 21:24:55 +0100

    Client: simplify locale data registration

--- client/src/app/app.server.module.ts
+++ client/src/app/app.server.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
-import { registerLocaleData } from '@angular/common';
 import { ServerModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module'
 import { SqlScratchComponent } from './app.component';
 
-import localeDe from '@angular/common/locales/de';
+import registerLanguages from './locale-registration';
 
-// registering local data for custom locales
-registerLocaleData(localeDe, 'de');
+registerLanguages()
 
 @NgModule({
   imports: [
