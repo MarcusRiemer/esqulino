2ac896211319c9b67b9fabda801455e24f1a7131
Marcus Riemer <Marcus@GurXite.de>
2019-03-29 22:14:29 +0100

    Server: Use proper status code when rendering an exception
    This should also fix failing specs on BitBucket pipelines

--- server/app/helpers/locale_helper.rb
+++ server/app/helpers/locale_helper.rb
@@ -4,9 +4,17 @@ module LocaleHelper
   # it falls back to the default locale.
   def request_locale
     if not request.subdomain.blank?
-      request.subdomain
+      # For whatever reason RSpec (or Rails) runs all tests with a www-subdomain
+      # that does not seem to be configurable. In the deployed version the web-
+      # server should redirect all requests on the www subdomain, but it doesn't
+      # hurt too much to handle this here as well.
+      if request.subdomain == "www"
+        return "de"
+      else
+        return request.subdomain
+      end
     else
-      "de"
+      return "de"
     end
   end
 end
