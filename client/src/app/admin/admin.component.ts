d2064323644fc880159b80051f8d54ae7a2535e3
Tom <tom.hilge@gmail.com>
2019-04-08 17:31:05 +0200

    Client: updated javascript-required-wrapper and added to index, create_project, admin templates

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -3,6 +3,45 @@ import { Component } from '@angular/core'
 import { BrowserService } from '../shared/browser.service';
 import { NavItem } from '../shared/nav-interfaces';
 
+export const adminItems: NavItem[] = [
+  {
+    type: "link",
+    text: {
+      de: "Administration",
+      en: "Administration",
+    },
+    route: ["/admin"],
+    icon: "puzzle-piece",
+  },
+  {
+    type: "link",
+    text: {
+      de: "Grammatiken",
+      en: "Grammar",
+    },
+    route: ["/admin/grammar"],
+    icon: "puzzle-piece",
+  },
+  {
+    type: "link",
+    text: {
+      de: "Blocksprachen",
+      en: "Block languages",
+    },
+    route: ["/admin/block-language"],
+    icon: "puzzle-piece",
+  },
+  {
+    type: "external",
+    text: {
+      de: "Anleitung 🇬🇧",
+      en: "Manual 🇬🇧",
+    },
+    url: "http://manual.blattwerkzeug.de/",
+    icon: "book"
+  },
+];
+
 /**
  * Hosts general menus and layout.
  */
@@ -22,43 +61,6 @@ export class AdminComponent {
   /**
    * All items that need to be shown in the general navigation
    */
-  readonly adminItems: NavItem[] = [
-    {
-      type: "link",
-      text: {
-        de: "Administration",
-        en: "Administration",
-      },
-      route: ["/admin"],
-      icon: "puzzle-piece",
-    },
-    {
-      type: "link",
-      text: {
-        de: "Grammatiken",
-        en: "Grammar",
-      },
-      route: ["/admin/grammar"],
-      icon: "puzzle-piece",
-    },
-    {
-      type: "link",
-      text: {
-        de: "Blocksprachen",
-        en: "Block languages",
-      },
-      route: ["/admin/block-language"],
-      icon: "puzzle-piece",
-    },
-    {
-      type: "external",
-      text: {
-        de: "Anleitung 🇬🇧",
-        en: "Manual 🇬🇧",
-      },
-      url: "http://manual.blattwerkzeug.de/",
-      icon: "book"
-    },
-  ];
+  readonly adminItems = adminItems;
 
 }
--- client/src/app/front/front.component.ts
+++ client/src/app/front/front.component.ts
@@ -4,6 +4,85 @@ import { BrowserService } from '../shared/browser.service';
 import { first, tap } from 'rxjs/operators';
 import { NavItem } from '../shared/nav-interfaces';
 
+export const indexItems: NavItem[] = [
+  {
+    type: "link",
+    text: {
+      de: "Hauptseite",
+      en: "Home",
+    },
+    route: ["/about/"],
+    icon: "home",
+  },
+  {
+    type: "link",
+    text: {
+      de: "Beispielprojekte",
+      en: "Example Projects",
+    },
+    route: ["/about/projects"],
+    icon: "cubes"
+  },
+  {
+    type: "divider"
+  },
+  {
+    type: "link",
+    text: {
+      de: "Forschung",
+      en: "Academia",
+    },
+    route: ["/about/academia"],
+    icon: "flask"
+  },
+  {
+    type: "link",
+    text: {
+      de: "Impressum",
+      en: "Imprint",
+    },
+    route: ["/about/imprint"],
+    icon: "file-text-o"
+  },
+  {
+    type: "link",
+    text: {
+      de: "Datenschutz",
+      en: "Privacy",
+    },
+    route: ["/about/privacy"],
+    icon: "user-secret"
+  },
+  {
+    type: "fill"
+  },
+  {
+    type: "header",
+    text: {
+      de: "Administration",
+      en: "Administration",
+    },
+  },
+  {
+    type: "link",
+    text: {
+      de: "Sprachen",
+      en: "Languages",
+    },
+    route: ["/admin"],
+    icon: "puzzle-piece"
+  },
+  {
+    type: "external",
+    text: {
+      de: "Anleitung 🇬🇧",
+      en: "Manual 🇬🇧",
+    },
+    url: "http://manual.blattwerkzeug.de/",
+    icon: "book"
+  },
+];
+
 @Component({
   templateUrl: 'templates/index.html',
 })
@@ -20,82 +99,5 @@ export class FrontComponent {
   /**
    * All items that need to be shown in the general navigation
    */
-  readonly indexItems: NavItem[] = [
-    {
-      type: "link",
-      text: {
-        de: "Hauptseite",
-        en: "Home",
-      },
-      route: ["/about/"],
-      icon: "home",
-    },
-    {
-      type: "link",
-      text: {
-        de: "Beispielprojekte",
-        en: "Example Projects",
-      },
-      route: ["/about/projects"],
-      icon: "cubes"
-    },
-    {
-      type: "divider"
-    },
-    {
-      type: "link",
-      text: {
-        de: "Forschung",
-        en: "Academia",
-      },
-      route: ["/about/academia"],
-      icon: "flask"
-    },
-    {
-      type: "link",
-      text: {
-        de: "Impressum",
-        en: "Imprint",
-      },
-      route: ["/about/imprint"],
-      icon: "file-text-o"
-    },
-    {
-      type: "link",
-      text: {
-        de: "Datenschutz",
-        en: "Privacy",
-      },
-      route: ["/about/privacy"],
-      icon: "user-secret"
-    },
-    {
-      type: "fill"
-    },
-    {
-      type: "header",
-      text: {
-        de: "Administration",
-        en: "Administration",
-      },
-    },
-    {
-      type: "link",
-      text: {
-        de: "Sprachen",
-        en: "Languages",
-      },
-      route: ["/admin"],
-      icon: "puzzle-piece"
-    },
-    {
-      type: "external",
-      text: {
-        de: "Anleitung 🇬🇧",
-        en: "Manual 🇬🇧",
-      },
-      url: "http://manual.blattwerkzeug.de/",
-      icon: "book"
-    },
-  ];
+  readonly indexItems = indexItems;
 }
