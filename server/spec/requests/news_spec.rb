a3dd36947ec688e0791c0ab9edf7b57f187d9c24
Tom <tom.hilge@gmail.com>
2019-04-25 14:25:23 +0200

    Server: added routes, spec files and controller functions for the update part of the news-system

--- server/spec/requests/news_spec.rb
+++ server/spec/requests/news_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe NewsController, type: :request do
+RSpec.fdescribe NewsController, type: :request do
+  json_headers = { "CONTENT_TYPE" => "application/json" }
+  
   it 'nothing published' do
     news = create(:news, published_from: Date.new(2019, 11, 1) )
     get '/api/news'
@@ -29,5 +31,81 @@ RSpec.describe NewsController, type: :request do
     expect(json_data[1]['title']['en']).to eq("Headline")
     expect(json_data[2]['title']['de']).to eq("Schlagzeile")
     expect(json_data[2]['title']['en']).to eq("Headline")
+  end
+
+  it 'updating a news' do
+    a = create(:news, title: { 'de': "Schlagzeile 1", 'en': "Headline 1"}, published_from: Date.new(2019, 1, 1) )
+    news_params = {
+      "id" => a.id,
+      "title" => { 'de': "Test" }
+    }
+
+    put "/api/news/admin",
+        :headers => json_headers,
+        :params => news_params.to_json
+
+    if (not response.body.blank?) then
+      json_data = JSON.parse(response.body)
+      expect(json_data.fetch('errors', [])).to eq []
+    end
+    expect(json_data['title']['de']).to eq("Test")
+    expect(json_data['title']['en']).to be_nil
+  end
+
+  it 'updating a news without a valid language' do
+    a = create(:news, title: { 'de': "Schlagzeile 1", 'en': "Headline 1"}, published_from: Date.new(2019, 1, 1) )
+    news_params = {
+      "id" => a.id,
+      "title" => { 'ab': "Test" }
+    }
+
+    put "/api/news/admin",
+        :headers => json_headers,
+        :params => news_params.to_json
+
+    if (not response.body.blank?) then
+      json_data = JSON.parse(response.body)
+      expect(json_data.fetch('errors', [])).to eq []
+    end
+    expect(json_data['title']['ab']).to be_nil
+  end
+
+  it 'updating a news with a valid language and an invalid language' do
+    a = create(:news, title: { 'de': "Schlagzeile 1", 'en': "Headline 1"}, published_from: Date.new(2019, 1, 1) )
+    news_params = {
+      "id" => a.id,
+      "title" => { 'ab': "Test", 'de': "test2" }
+    }
+
+    put "/api/news/admin",
+        :headers => json_headers,
+        :params => news_params.to_json
+
+    if (not response.body.blank?) then
+      json_data = JSON.parse(response.body)
+      expect(json_data.fetch('errors', [])).to eq []
+    end
+    expect(json_data['title']['ab']).to be_nil
+    expect(json_data['title']['de']).to eq('test2')
+  end
+
+  it 'updating a news with an invalid date' do
+    a = create(:news, title: { 'de': "Schlagzeile 1", 'en': "Headline 1"}, published_from: Date.new(2019, 1, 1) )
+    news_params = {
+      "id" => a.id,
+      "title" => { 'de': "Test" },
+      "publishedFrom" => "test"
+    }
+
+    put "/api/news/admin",
+        :headers => json_headers,
+        :params => news_params.to_json
+
+    if (not response.body.blank?) then
+      json_data = JSON.parse(response.body)
+      expect(json_data.fetch('errors', [])).to eq []
+    end
+
+    expect(json_data['publishedFrom']).to eq("2019-01-01T00:00:00.000Z")
   end
 end
