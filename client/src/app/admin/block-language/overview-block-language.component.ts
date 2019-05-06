c61f05dd455a7ce7e5a20442dc087940f178a909
Marcus Riemer <Marcus@GurXite.de>
2019-05-06 13:50:40 +0200

    Client: Example for toolbar buttons

--- client/src/app/admin/block-language/overview-block-language.component.ts
+++ client/src/app/admin/block-language/overview-block-language.component.ts
@@ -1,16 +1,27 @@
-import { ServerDataService } from '../../shared';
-import { Component } from "@angular/core";
+import { Component, ViewChild, TemplateRef, OnInit } from "@angular/core";
 
+import { ServerDataService, ToolbarService } from '../../shared';
 
+/**
+ * Shows All block languages that are known to the server.
+ */
 @Component({
-  selector: 'block-language-overview-selector',
   templateUrl: './templates/overview-block-language.html'
 })
 
-export class OverviewBlockLanguageComponent {
+export class OverviewBlockLanguageComponent implements OnInit {
+  @ViewChild('toolbarItems')
+  toolbarItems: TemplateRef<any>;
+
   constructor(
-    private _serverData: ServerDataService
-  ) {}
+    private _serverData: ServerDataService,
+    private _toolbarService: ToolbarService,
+  ) { }
+
+  ngOnInit(): void {
+    this._toolbarService.clearItems();
+    this._toolbarService.addItem(this.toolbarItems);
+  }
 
   public get availableBlockLanguages() {
     return (this._serverData.listBlockLanguages);
