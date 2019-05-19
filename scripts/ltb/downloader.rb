9be10d4335bbaf0444e03de0ef798f439d018fa2
Sebastian <2424wrwr@web.de>
2019-05-19 23:59:08 +0200

    added databases and imports for ltb, pokemon

--- scripts/ltb/downloader.rb
+++ scripts/ltb/downloader.rb
@@ -1,9 +1,10 @@
 require "open-uri"
 require "csv"
 
+# Download Pictures from ltb-url.csv to /pics/
 CSV.foreach("ltb-url.csv") do |row|
   open(row[0]) {|f|
-  File.open('./pics/'+row[0][/([^\/]+)$/],"wb") do |file|
+  File.open('./pics/' + row[0][/([^\/]+)$/],"wb") do |file|
     file.puts f.read
   end
 
--- scripts/pokemon/playground.rb
+++
@@ -1,8 +0,0 @@
-require 'net/http'
-require 'nokogiri'
-require 'open-uri'
-require 'csv'
-require 'json'
-require_relative 'fetch_list'
-
-p fetch('https://pokeapi.co/api/v2/pokemon/')
