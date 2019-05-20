87490d823c7c0533f0654e34ea31e7af4bebaed6
Sebastian <2424wrwr@web.de>
2019-05-21 00:24:22 +0200

    Script: Fiexed ddf Database

--- scripts/ddf/import.sh
+++ scripts/ddf/import.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 #sqlite3 $1 <<EOF
-sqlite3 test.db <<EOF
+sqlite3 db.sqlite <<EOF
 .mode csv
 DELETE FROM Auftritt;
-DELETE FROM Charakter_Sprecher;
 DELETE FROM Charakter;
 DELETE FROM Sprecher;
 DELETE FROM Geschichte;
 .import Geschichte.csv Geschichte
 .import Sprecher.csv Sprecher
 .import Charakter.csv Charakter
-.import Charakter_Sprecher.csv Charakter_Sprecher
 .import Auftritt.csv Auftritt
 VACUUM
 EOF
