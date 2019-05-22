e84b0db71be211248099f01f7524c9aa5ecbb4c8
Tom <tom.hilge@gmail.com>
2019-05-22 13:25:20 +0200

    Client: modified login dialog

--- client/src/app/shared/auth/auth-dialog.component.ts
+++ client/src/app/shared/auth/auth-dialog.component.ts
@@ -1,16 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatDialogRef, MatDialog } from '@angular/material';
 import { Validators, FormControl, FormGroup } from '@angular/forms';
+import * as jwt_decode from 'jwt-decode';
 
 @Component({
   templateUrl: './templates/auth-dialog.html'
 })
 export class AuthDialogComponent {
   constructor(
-    private _dialogRef: MatDialogRef<AuthDialogComponent>
+    private _dialogRef: MatDialogRef<AuthDialogComponent>,
+    private _http: HttpClient
   ) {}
 
   public register: boolean = false;
+  public readonly provider = ['Google', 'Github']
 
   public readonly general = new FormGroup({
     email: new FormControl('', [
@@ -26,12 +30,19 @@ export class AuthDialogComponent {
   }
 
   public onChange(): void {
-    this.register = true;
+    this.register = !this.register;
+  }
+
+  public onSend(): void {
+    let headers = new HttpHeaders().set('Authorization',  `Bearer dasdasdasdasd`)
+    this._http.post<{["token"]: string}>('/api/auth/developer/callback', 
+    { "name": "email", "email": "tom.hilge"}, {headers: headers}).subscribe(log => console.log(jwt_decode(log.token)))
   }
 
   public static show(dialog: MatDialog) {
     dialog.open(AuthDialogComponent, {
-      width: '500px'
+      width: '800px',
+      height: '400px'
     });
   }
 }
--- client/src/app/shared/auth/login-button.component.ts
+++ client/src/app/shared/auth/login-button.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
 import { AuthDialogComponent } from './auth-dialog.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'authentication-button',
   templateUrl: './templates/login-button.html'
 })
 export class LoginButtonComponent{
-  constructor() {}
-  public openDialog(): void {}
+  constructor(
+    private _dialog: MatDialog
+  ) {}
+
+  public openDialog(): void {
+    AuthDialogComponent.show(this._dialog)
+  }
 }
