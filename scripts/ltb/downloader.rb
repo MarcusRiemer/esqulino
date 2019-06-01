53b9eee681912d4b1784145d4ff22a70d81fef88
Sebastian <2424wrwr@web.de>
2019-06-01 21:25:16 +0200

    Script: Final version

--- scripts/ltb/downloader.rb
+++ scripts/ltb/downloader.rb
@@ -3,7 +3,7 @@ require "csv"
 
 # Download Pictures from ltb-url.csv to /pics/
 CSV.foreach("ltb-url.csv") do |row|
-  open(row[0]) {|f|
+  open(row[1]) {|f|
   #The URL is at the 2nd Column -> row[1]
   File.open('./pics/' + row[1][/([^\/]+)$/],"wb") do |file|
     file.puts f.read
