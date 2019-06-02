b3c3085fd0c6b9ed7571f048dc0f1b1b01d91983
Tom <tom.hilge@gmail.com>
2019-06-02 12:47:52 +0200

    Server: e-mail confirmation, markdown e-mails

--- server/app/controllers/auth_controller.rb
+++ server/app/controllers/auth_controller.rb
@@ -19,12 +19,28 @@ class AuthController < ApplicationController
       data: {
         email: params[:email],
         password: params[:password],
-        verify_token: gen_random_uuid()
+        verify_token: SecureRandom.uuid,
+        confirmed: false
       }
     }
-    
+
     create_identity(auth, true)
     render json: { loggged_in: false }
+  end
+
+  def email_confirmation
+    identity = Identity.where("data ->> 'verify_token' = ?", params[:verify_token]).first!
+    # identity found and not confirmed
+    if identity && !identity[:data][:confirmed]
+      identity[:data][:confirmed] = true;
+      identity.save
+
+      @identity = identity
+      sign_in
+      redirect_to "/"
+    else
+      # TODO ERROR MSG: Does not found an e-mail with this token
+    end
   end
 
   def destroy
---
+++ server/config/initializers/maildown.rb
@@ -0,0 +1,4 @@
+Maildown::MarkdownEngine.set_html do |text|
+  carpet = Redcarpet::Markdown.new(Redcarpet::Render::HTML, {})
+  carpet.render(text).html_safe
+end
