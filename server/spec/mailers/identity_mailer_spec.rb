4ccc970015a5a0ed17e161619587d2b5f63b9cae
Tom <tom.hilge@gmail.com>
2019-06-22 11:30:24 +0200

    Server: fixed mailer_spec, identities_spec

--- server/spec/mailers/identity_mailer_spec.rb
+++ server/spec/mailers/identity_mailer_spec.rb
@@ -1,37 +1,38 @@
 require "rails_helper"
 
 RSpec.describe IdentityMailer, :type => :mailer do
-  let(:user) { create(:user) }
   
   describe "Identity Mailer" do
     locale = "de"
     display_name = "Blattwerkzeug"
     base_url = Rails.configuration.sqlino["editor_domain"]
-    let(:identity) { create(:identity, :identity_provider, user_id: user[:id]) }
 
     it "reset password" do
+      identity = create(:identity_provider, :new)
       mail = IdentityMailer.reset_password(identity, locale)
 
       expect(mail.subject).to eq("Blattwerkzeug.de password reset")
-      expect(mail.to).to eq(identity["email"])
+      expect(mail.to.first).to eq(identity[:uid])
 
-      expect(mail.body.to_s).to include("http://#{locale}.#{base_url}/user/reset_password?token=#{identity["password_reset_token"]}&email=#{@identity["email"]}")
+      expect(mail.text_part.body.to_s).to include("http://#{locale}.#{base_url}/user/reset_password/#{identity["password_reset_token"]}")
     end
 
     it "changed password" do
+      identity = create(:identity_provider, :new)
       mail = IdentityMailer.changed_password(identity)
 
       expect(mail.subject).to eq("Blattwerkzeug.de password changed")
-      expect(mail.to).to eq(identity["email"])
+      expect(mail.to.first).to eq(identity[:uid])
     end
 
     it "confirm email" do
-      mail = IdentityMailer.changed_password(identity, locale)
+      identity = create(:identity_provider, :new)
+      mail = IdentityMailer.confirm_email(identity, locale)
 
       expect(mail.subject).to eq("Blattwerkzeug.de confirmation")
-      expect(mail.to).to eq(identity["email"])
+      expect(mail.to.first).to eq(identity[:uid])
 
-      expect(mail.body.to_s).to include("http://#{locale}.#{base_url}/api/identities/confirmation/#{identity[:data][:verify_token]}")
+      expect(mail.text_part.body.to_s).to include("http://#{locale}.#{base_url}/api/identities/confirmation/#{identity[:data]["verify_token"]}")
     end
   end
 end
