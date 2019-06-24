fd7f220d181ad873f76aab988d0022990f916e96
Marcus Riemer <Marcus@GurXite.de>
2019-06-24 12:59:40 +0200

    Server: OOified identities

--- server/app/models/identities/developer.rb
+++ server/app/models/identities/developer.rb
@@ -1,7 +1,14 @@
 class Developer < Identity
   def self.create_with_auth(auth, user)
-    auth[:info]["confirmed"] = true
-
     Developer.create(:user => user, :uid => auth[:uid], :provider => auth[:provider], :data => auth[:info])
+  end
+
+  # Developers are never confirmed manually, we always believe them
+  def confirmed?
+    true
+  end
+
+  def email
+    self.uid
   end
 end
--- server/app/models/identities/google.rb
+++ server/app/models/identities/google.rb
@@ -1,7 +1,15 @@
 class Google < Identity
   def self.create_with_auth(auth, user)
-    auth[:info]["confirmed"] = true
-
     Google.create(:user => user, :uid => auth[:uid], :provider => auth[:provider], :data => auth[:info])
+  end
+
+  # Google tells us whether the mail is valid
+  def confirmed?
+    self.data["email_verified"]
+  end
+
+  # Github provides the mail in the JSON blob
+  def email
+    self.data["email"]
   end
 end
