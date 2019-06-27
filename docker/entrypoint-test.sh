d17ed7471ec9ae533490d87decf0a9d2c79f825c
Marcus Riemer <Marcus@GurXite.de>
2019-06-27 08:59:04 +0200

    Docker: Re-organisation of files and services
    * Moved most files into subfolder
    * Cleaned up test image
    * ToDo: Client and Server development images

--- docker/entrypoint-test.sh
+++ docker/entrypoint-test.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 export TEST_SINGLE_RUN=true
+export NG_CLI_ANALYTICS=ci
 
 # Run all operations using the uid and gid of the host user
 groupadd -g "$USER_GID" user
 useradd -m -u "$USER_UID" -g "$USER_GID" user
 
 su user -c "make --no-print-directory install-deps"
-su user -c "make --no-print-directory -C client dist-dev"
+su user -c "make --no-print-directory -C client client-compile-dev cli-compile"
 su user -c "make --no-print-directory -C server setup-database-schema test"
 su user -c "make --no-print-directory -C client client-test"
