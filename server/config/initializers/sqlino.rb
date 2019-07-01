7a8e914849fecdbe70be297820bd4100973e5650
Marcus Riemer <Marcus@GurXite.de>
2019-07-01 16:35:00 +0200

    Server: Startup changes
    * `make setup-database-schema` is now only run if no db is present
    * Missing cli versions of the client are now tolerated during startup

--- server/config/initializers/sqlino.rb
+++ server/config/initializers/sqlino.rb
@@ -2,17 +2,19 @@
 Rails.application.config.to_prepare do
   config = Rails.application.config
   config.sqlino = Rails.application.config_for :sqlino
-  
+
   # Turn client_dir and data_dir into absolute paths
   config.sqlino[:client_dir] = Rails.root.join(Rails.configuration.sqlino['client_dir'])
   config.sqlino[:data_dir] = Rails.root.join(Rails.configuration.sqlino['data_dir'])
-  
+
   config.sqlino[:projects_dir] = config.sqlino[:data_dir].join 'projects'
 end
 
 # Ensuring that all relevant folders exist
 Rails.application.config.after_initialize do
   projects_dir = Rails.application.config.sqlino[:projects_dir]
+
+  # Ensure that there is a data directory available
   if not File.directory? projects_dir
     abort("Startup Error: Projects directory at \"#{projects_dir}\" must exist and be writeable")
   end
@@ -27,10 +29,8 @@ Rails.application.config.after_initialize do
 
   Rails.logger.info "IDE service configured, testing availability ..."
   begin
-    if not IdeService.instance.cli_program_exists?
-      abort("Startup Error: No cli program at \"#{IdeService.instance.cli_program_path}\"")
-    end
-    
+    IdeService.instance.wait_cli_program_exists!
+
     if not IdeService.instance.ping!
       abort("Startup Error: IDE service did not respond")
     end
