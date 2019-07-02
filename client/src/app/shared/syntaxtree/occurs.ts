e5db1268725b892f86cd5b34bd208d3b75a97dca
Marcus Riemer <Marcus@GurXite.de>
2019-07-02 14:00:50 +0200

    Client: Added validation for SQL function parameter count

--- client/src/app/shared/syntaxtree/occurs.ts
+++ client/src/app/shared/syntaxtree/occurs.ts
@@ -1,7 +1,11 @@
-import { OccursDescription, OccursSpecificDescription } from './occurs.description';
+import { OccursDescription, OccursSpecificDescription, isOccursSpecificDescription } from './occurs.description';
 
 export * from './occurs.description'
 
+/**
+ * Resolves the short-hand regex-like occurs definition to a verbose
+ * exact description.
+ */
 export function resolveOccurs(desc: OccursDescription): OccursSpecificDescription {
   switch (desc) {
     case "*": return ({ minOccurs: 0, maxOccurs: +Infinity });
@@ -10,4 +14,9 @@ export function resolveOccurs(desc: OccursDescription): OccursSpecificDescriptio
     case "1": return ({ minOccurs: 1, maxOccurs: 1 });
     default: throw new Error(`Unknown occurences: "${JSON.stringify(desc)}"`);
   }
+}
+
+export function isInOccurs(val: number, desc: OccursDescription): boolean {
+  let specific = isOccursSpecificDescription(desc) ? desc : resolveOccurs(desc);
+  return (val <= specific.maxOccurs && val >= specific.minOccurs);
 }
