b95e5436becae57b8daab7a7f29f6126aba0332e
Marcus Riemer <Marcus@GurXite.de>
2019-07-03 22:49:25 +0200

    Docker: Test image now also uses seperate volume for project data

--- docker/entrypoint-test.sh
+++ docker/entrypoint-test.sh
@@ -6,6 +6,10 @@ export NG_CLI_ANALYTICS=ci # Stop Angular CLI from asking for usage statistics
 groupadd -g "$USER_GID" user
 useradd -m -u "$USER_UID" -g "$USER_GID" user
 
+# Ensure that the projects folder has the correct rights and exists
+chown user:user -R /srv/project-data
+su user -c "mkdir -p /srv/project-data/projects"
+
 su user -c "make --no-print-directory install-deps"
 su user -c "make --no-print-directory -C client client-compile-de universal-de cli-compile"
 su user -c "make --no-print-directory -C client client-test"
