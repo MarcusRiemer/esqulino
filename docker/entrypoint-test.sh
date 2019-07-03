f6c200688146d07217992fce09f0ac058b98ee44
Marcus Riemer <Marcus@GurXite.de>
2019-07-03 12:35:08 +0200

    Client: Provide additional bundled version of CLI (including dependencies)
    This solves server startup errors where 'rxjs' couldn't be resolved.

--- docker/entrypoint-test.sh
+++ docker/entrypoint-test.sh
@@ -6,7 +6,8 @@ export NG_CLI_ANALYTICS=ci # Stop Angular CLI from asking for usage statistics
 groupadd -g "$USER_GID" user
 useradd -m -u "$USER_UID" -g "$USER_GID" user
 
-su user -c "make --no-print-directory install-deps"
-su user -c "make --no-print-directory -C client client-compile-dev cli-compile"
-su user -c "make --no-print-directory -C server setup-database-schema test"
-su user -c "make --no-print-directory -C client client-test"
+bash
+# su user -c "make --no-print-directory install-deps"
+# su user -c "make --no-print-directory -C client client-compile-dev cli-compile"
+# su user -c "make --no-print-directory -C server setup-database-schema test"
+# su user -c "make --no-print-directory -C client client-test"
