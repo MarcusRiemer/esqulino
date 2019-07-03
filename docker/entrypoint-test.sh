b92233ab96efb656a577b8dfe17201f7316c6a68
Marcus Riemer <Marcus@GurXite.de>
2019-07-03 22:40:00 +0200

    Docker: Fix incorrect comments on test entrypoint, was meant for testing

--- docker/entrypoint-test.sh
+++ docker/entrypoint-test.sh
@@ -6,8 +6,8 @@ export NG_CLI_ANALYTICS=ci # Stop Angular CLI from asking for usage statistics
 groupadd -g "$USER_GID" user
 useradd -m -u "$USER_UID" -g "$USER_GID" user
 
-bash
-# su user -c "make --no-print-directory install-deps"
-# su user -c "make --no-print-directory -C client client-compile-dev cli-compile"
-# su user -c "make --no-print-directory -C server setup-database-schema test"
-# su user -c "make --no-print-directory -C client client-test"
+su user -c "make --no-print-directory install-deps"
+su user -c "make --no-print-directory -C client client-compile-de universal-de cli-compile"
+su user -c "make --no-print-directory -C client client-test"
+su user -c "make --no-print-directory -C server setup-database-schema test"
+su user -c "make --no-print-directory -C server load-live-data"
