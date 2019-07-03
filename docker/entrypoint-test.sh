c65098befed681ef0dc9b20da584b5a8104d923d
Marcus Riemer <Marcus@GurXite.de>
2019-07-03 23:03:00 +0200

    Docker: Run test image as root

--- docker/entrypoint-test.sh
+++ docker/entrypoint-test.sh
@@ -2,16 +2,10 @@
 export TEST_SINGLE_RUN=true
 export NG_CLI_ANALYTICS=ci # Stop Angular CLI from asking for usage statistics
 
-# Run all operations using the uid and gid of the host user
-groupadd -g "$USER_GID" user
-useradd -m -u "$USER_UID" -g "$USER_GID" user
+mkdir -p /srv/project-data/projects
 
-# Ensure that the projects folder has the correct rights and exists
-chown user:user -R /srv/project-data
-su user -c "mkdir -p /srv/project-data/projects"
-
-su user -c "make --no-print-directory install-deps"
-su user -c "make --no-print-directory -C client client-compile-de universal-de cli-compile"
-su user -c "make --no-print-directory -C client client-test"
-su user -c "make --no-print-directory -C server setup-database-schema test"
-su user -c "make --no-print-directory -C server load-live-data"
+make --no-print-directory install-deps
+make --no-print-directory -C client client-compile-de universal-de cli-compile
+make --no-print-directory -C client client-test
+make --no-print-directory -C server setup-database-schema test
+make --no-print-directory -C server load-live-data
