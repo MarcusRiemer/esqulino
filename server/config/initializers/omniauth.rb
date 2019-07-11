d3443f275fd7ef247e2c00e12f8001ae3b5044f6
Tom <tom.hilge@gmail.com>
2019-07-11 15:20:11 +0200

    Server: without own specs

--- server/config/initializers/omniauth.rb
+++ server/config/initializers/omniauth.rb
@@ -8,7 +8,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     config.full_host = Rails.env.production? ? "https://#{domain}" : "http://#{domain}"
     config.path_prefix = '/api/auth'
   end
-  provider :identity, :fields => [], :on_registration => AuthController.action(:register), :on_login => AuthController.action(:login_with_password), :model => PasswordIdentity
+  # provider :identity, :fields => [], :on_registration => AuthController.action(:register), :on_login => AuthController.action(:login_with_password), :model => PasswordIdentity
   provider :developer, :fields => [:name, :email], :uid_field => :email unless Rails.env.production?
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
