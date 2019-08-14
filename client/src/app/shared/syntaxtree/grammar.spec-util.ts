32ab6563c1feb8bb579fa30910a7fc23735e6cb1
Marcus Riemer <Marcus@GurXite.de>
2019-08-14 20:30:42 +0200

    Client: Spec for typedef at root of polyglot grammar

--- client/src/app/shared/syntaxtree/grammar.spec-util.ts
+++ client/src/app/shared/syntaxtree/grammar.spec-util.ts
@@ -1,5 +1,6 @@
 import { GrammarDocument, NamedTypes, NamedLanguages } from './grammar.description';
-import { QualifiedTypeName } from './syntaxtree.description';
+import { QualifiedTypeName, NodeLocation } from './syntaxtree.description';
+import { ValidationError, ValidationResult } from './validation-result';
 
 export function singleLanguageGrammar(
   langName: string,
@@ -29,4 +30,21 @@ export function multiLanguageGrammar(
     types: languages,
     root: rootType,
   });
+}
+
+export interface ComparableValidationError {
+  code: string,
+  location: NodeLocation,
+}
+
+/**
+ * Actual errors may contain all sorts of circular references that are handy when
+ */
+export function comparableErrors(result: ValidationResult) {
+  return (result.errors.map((e): ComparableValidationError => {
+    return ({
+      code: e.code,
+      location: e.node.location
+    });
+  }));
 }
