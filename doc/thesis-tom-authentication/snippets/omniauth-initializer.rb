bd71f4aa5bc0e65eb65e36892e0e83ff60ba79fd
Tom <tom.hilge@gmail.com>
2019-08-28 16:30:13 +0200

    Thesis: todos done

--- doc/thesis-tom-authentication/snippets/omniauth-initializer.rb
+++ doc/thesis-tom-authentication/snippets/omniauth-initializer.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :identity, :on_registration => AuthController.action(:register),
-						:on_login => AuthController.action(:login_with_password), :model => PasswordIdentity
+					:on_login => AuthController.action(:login_with_password), :model => PasswordIdentity
 	provider :developer, :fields => [:name, :email], :uid_field => :email unless Rails.env.production?
 	provider :google_oauth2, GOOGLE_ID, GOOGLE_SECRET
 	provider :github, GITHUB_ID, GITHUB_ID
