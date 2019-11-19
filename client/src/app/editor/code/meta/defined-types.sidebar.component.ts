3961f3340403b77765229d973c5a26f6226dca90
Marcus Riemer <Marcus@GurXite.de>
2019-11-19 13:14:37 +0100

    Client: Meta grammars now show available types

--- client/src/app/editor/code/meta/defined-types.sidebar.component.ts
+++ client/src/app/editor/code/meta/defined-types.sidebar.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
 
+import { map } from 'rxjs/operators';
+
+import { QualifiedTypeName, NodeDescription } from '../../../shared/syntaxtree';
 import { DragService } from '../../drag.service';
 import { CurrentCodeResourceService } from '../../current-coderesource.service';
+
+/**
+ * Create appropriate descriptions from the available procedure
+ */
+function makeCallBlock(name: QualifiedTypeName): NodeDescription[] {
+  return ([
+    {
+      language: "MetaGrammar",
+      name: "nodeRef",
+      properties: {
+        "languageName": name.languageName,
+        "typeName": name.typeName
+      }
+    }
+  ]);
+}
 
 @Component({
   templateUrl: 'templates/defined-types-sidebar.html',
@@ -11,5 +30,38 @@ export class DefinedTypesSidebarComponent {
     private _dragService: DragService,
     private _current: CurrentCodeResourceService
   ) {
+  }
+
+  readonly availableNodes = this._current.currentTree.pipe(
+    map(
+      t => {
+        const concrete = t.getNodesOfType({ languageName: "MetaGrammar", typeName: "concreteNode" });
+        const typedefs = t.getNodesOfType({ languageName: "MetaGrammar", typeName: "typedef" });
+
+        return (
+          [...concrete, ...typedefs])
+          .map((n): QualifiedTypeName => {
+            return ({
+              languageName: n.properties["languageName"],
+              typeName: n.properties["typeName"]
+            });
+          }
+          );
+      }),
+  );
+
+  printableName(n: QualifiedTypeName) {
+    return (n.languageName + "." + n.typeName);
+  }
+
+  /**
+   * The user has decided to start dragging something from the sidebar.
+   */
+  startDrag(evt: DragEvent, name: QualifiedTypeName) {
+    try {
+      this._dragService.dragStart(evt, makeCallBlock(name));
+    } catch (e) {
+      alert(e);
+    }
   }
 }
