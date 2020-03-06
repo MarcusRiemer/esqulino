896447751d4fcaa10640d973e536cdda6184e4a4
Yannick Schroeder <yas@fh-wedel.de>
2020-03-06 17:37:19 +0100

    Client & Server: Grammar pagination completed (without tests)

--- client/src/app/admin/grammar/overview-grammar.component.ts
+++ client/src/app/admin/grammar/overview-grammar.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { GrammarDataService } from '../../shared/serverdata';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { GrammarDescription, GrammarListDescription } from '../../shared/syntaxtree';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'grammar-overview-selector',
@@ -7,15 +11,42 @@ import { GrammarDataService } from '../../shared/serverdata';
 })
 
 export class OverviewGrammarComponent {
+  // Angular Material UI to paginate
+  @ViewChild(MatPaginator)
+  _paginator: MatPaginator;
+
+  // Angular Material UI to sort by different columns
+  @ViewChild(MatSort)
+  _sort: MatSort;
+
   constructor(
     private _serverData: GrammarDataService
   ) { }
 
-  public get availableGrammars() {
-    return (this._serverData.listCache);
-  }
+availableGrammars: Observable<GrammarListDescription[]> = this._serverData.list;
+
+resultsLength = this._serverData.listTotalCount;
+
+
 
   public deleteGrammar(id: string) {
     this._serverData.deleteSingle(id);
   }
+
+  /**
+   * User has requested a different chunk of data
+   */
+  onChangePagination() {
+    this._serverData.setListPagination(this._paginator.pageSize, this._paginator.pageIndex);
+  }
+
+  /**
+   * User has requested different sorting options
+   */
+  onChangeSort() {
+    this._serverData.setListOrdering(this._sort.active, this._sort.direction);
+  }
+
+  displayedColumns: (keyof GrammarListDescription)[] = ["name", "slug", "id"];
+
 }
