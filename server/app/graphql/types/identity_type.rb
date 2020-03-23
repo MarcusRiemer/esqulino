14e9da54d5df81a865bae398342970a6d368bc84
Yannick Schröder <yannickpschroeder@gmail.com>
2020-03-23 17:26:15 +0100

    Server: Fixed Typo in Graphql schema

--- server/app/graphql/types/identity_type.rb
+++ server/app/graphql/types/identity_type.rb
@@ -4,7 +4,7 @@ module Types
     field :provider, String, null:true
     field :type, String, null:true
     field :providerData, Types::Json, null:true
-    field :own_data, Types::Json, null:true
+    field :ownData, Types::Json, null:true
     field :user, Types::UserType,null:true
     field :createdAt, Types::Datetime, null:false
     field :updatedAt, Types::Datetime, null:false
