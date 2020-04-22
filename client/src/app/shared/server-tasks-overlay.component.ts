a797d6a17d59eb1c17190960f89ef940ca8b1828
Yannick Schröder <yannickpschroeder@gmail.com>
2020-04-22 14:47:58 +0200

    Client: Reversed Servertask list so the latest task is on top, made list scrollable

--- client/src/app/shared/server-tasks-overlay.component.ts
+++ client/src/app/shared/server-tasks-overlay.component.ts
@@ -7,6 +7,7 @@ import {
 } from "./serverdata/server-tasks.service";
 import { Component } from "@angular/core";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "server-tasks-list",
@@ -16,7 +17,9 @@ import { Observable } from "rxjs";
 export class ServerTasksOverlayComponent {
   constructor(private _serverTasks: ServerTasksService) {}
 
-  readonly allTasks$ = this._serverTasks.publicTasks$;
+  readonly allTasks$ = this._serverTasks.publicTasks$.pipe(
+    map((list) => list.reverse())
+  );
 
   hasNoTasks(): Observable<boolean> {
     return this._serverTasks.hasNoTasks$;
