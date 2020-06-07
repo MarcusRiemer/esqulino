c39582eb6d7ee6c78f1f3d206d4f2b9cf7159b5e
Marcus Riemer <Marcus@GurXite.de>
2020-06-07 22:28:08 +0200

    Server: Sketches recursive references for grammars and code resources

---
+++ server/db/migrate/20200607185733_create_code_resource_references.rb
@@ -0,0 +1,62 @@
+class CreateCodeResourceReferences < ActiveRecord::Migration[6.0]
+  def change
+    # Using a custom PostgreSQL enum sadly conflicts with the schema.rb
+    #
+    # reversible do |change|
+    #   change.up do
+    #     execute <<-SQL
+    #       CREATE TYPE code_resource_reference_type AS ENUM ('generic', 'include_types', 'visualizes');
+    #     SQL
+    #   end
+    #   change.down do
+    #     execute <<-SQL
+    #       DROP TYPE code_resource_reference_type;
+    #     SQL
+    #   end
+    # end
+
+    create_table :code_resource_references, id: :uuid, default: 'gen_random_uuid()' do |t|
+      t.references :origin,
+                   type: :uuid,
+                   index: true,
+                   null: false,
+                   foreign_key: { to_table: :code_resources }
+      t.references :target,
+                   type: :uuid,
+                   index: true,
+                   null: false,
+                   foreign_key: { to_table: :code_resources }
+      t.integer :reference_type,
+                null: false
+      t.timestamps
+    end
+
+    # Adding a reference twice is not allowed
+    add_index :code_resource_references,
+              [:origin_id, :target_id],
+              :unique => true,
+              :name => 'code_resource_references_unique'
+
+    create_table :grammar_references, id: :uuid, default: 'gen_random_uuid()' do |t|
+      t.references :origin,
+                   type: :uuid,
+                   index: true,
+                   null: false,
+                   foreign_key: { to_table: :grammars }
+      t.references :target,
+                   type: :uuid,
+                   index: true,
+                   null: false,
+                   foreign_key: { to_table: :grammars }
+      t.integer :reference_type,
+                null: false
+      t.timestamps
+    end
+
+    # Adding a reference twice is not allowed
+    add_index :grammar_references,
+              [:origin_id, :target_id],
+              :unique => true,
+              :name => 'grammar_references_unique'
+  end
+end
