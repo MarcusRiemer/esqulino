abb2c142dee661084363c67e6a787c00f3280ca0
dario <dario_zuban@hotmail.de>
2020-07-20 14:17:42 +0200

    Seed: current RegEx state

--- client/src/app/shared/syntaxtree/regex/index.ts
+++ client/src/app/shared/syntaxtree/regex/index.ts
@@ -1,9 +1,9 @@
-import { LanguageDefinition } from '../language'
-import { NODE_CONVERTER } from './regex.codegenerator'
+import { LanguageDefinition } from "../language";
+import { NODE_CONVERTER } from "./regex.codegenerator";
 
 export const LANGUAGE_DESCRIPTION: LanguageDefinition = {
   id: "regex",
   name: "RegEx",
   emitters: NODE_CONVERTER,
-  validators: []
-}
+  validators: [],
+};
--- client/src/app/shared/syntaxtree/regex/regex.codegenerator.spec.ts
+++ client/src/app/shared/syntaxtree/regex/regex.codegenerator.spec.ts
@@ -1,7 +1,7 @@
-import { CodeGenerator } from '../codegenerator'
-import { NodeDescription, Tree } from '../syntaxtree'
+import { CodeGenerator } from "../codegenerator";
+import { NodeDescription, Tree } from "../syntaxtree";
 
-import { NODE_CONVERTER } from './regex.codegenerator'
+import { NODE_CONVERTER } from "./regex.codegenerator";
 
 /**
  * Ensures that the given in and output files do match correctly.
@@ -10,9 +10,13 @@ import { NODE_CONVERTER } from './regex.codegenerator'
  * to `require` are relative to the file the function is defined in.
  * So for the moment this function is copy and pasted into some spec files :(
  */
-export function verifyFiles<T>(fileName: string, transform: (obj: T) => string) {
+export function verifyFiles<T>(
+  fileName: string,
+  transform: (obj: T) => string
+) {
   const input = require(`./regex.spec/${fileName}.json`);
-  let expected = require(`raw-loader!./regex.spec/${fileName}.txt`).default as string;
+  let expected = require(`raw-loader!./regex.spec/${fileName}.txt`)
+    .default as string;
 
   if (expected.endsWith("\n")) {
     expected = expected.substr(0, expected.length - 1);
@@ -28,7 +32,7 @@ function emitTree(astDesc: NodeDescription, emitProgressCallback = false) {
   const ast = new Tree(astDesc).rootNode;
   const codeGen = new CodeGenerator(NODE_CONVERTER, []);
 
-  return (codeGen.emit(ast));
+  return codeGen.emit(ast);
 }
 
 /**
@@ -39,11 +43,8 @@ function emitTreeWithProgressCallbacks(astDesc: NodeDescription) {
   return emitTree(astDesc, true);
 }
 
-
-describe('Language: RegEx Program (Codegen)', () => {
-  it('constant', () => {
+describe("Language: RegEx Program (Codegen)", () => {
+  it("constant", () => {
     verifyFiles("ast-01-constant", emitTree);
   });
-
-
 });
