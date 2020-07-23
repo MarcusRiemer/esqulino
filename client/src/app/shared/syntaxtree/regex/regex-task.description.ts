e3e837720d53cd08130564d2bdaa4355c3d2a86c
Marcus Riemer <Marcus@GurXite.de>
2020-07-23 13:20:20 +0200

    Client: Example descriptions for testcases

---
+++ client/src/app/shared/syntaxtree/regex/regex-task.description.ts
@@ -0,0 +1,24 @@
+/**
+ * A list of testcases for regular expressions.
+ */
+interface RegexTestDescription {
+  cases: RegexTestCaseDescription[];
+}
+
+/**
+ * A single input for a regular expression and the expected result:
+ *
+ * - `wholeMatch`: The regex must match the whole string
+ * - `noMatch`: The regex must not match anything in the string
+ * - `exactMatch`: The regex must match exactly according to the expected hits.
+ */
+interface RegexTestCaseDescription {
+  input: string;
+
+  expected:
+    | { type: "wholeMatch" | "noMatch" }
+    | {
+        type: "exactMatch";
+        hits: string[];
+      };
+}
