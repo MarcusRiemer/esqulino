9319c55e6e37c32d7f30fbba31433950de753027
Marcus Riemer <Marcus@GurXite.de>
2020-07-23 15:12:38 +0200

    Client: Sketched simplification process for regex test matching

--- client/src/app/shared/syntaxtree/regex/regex-test.ts
+++ client/src/app/shared/syntaxtree/regex/regex-test.ts
@@ -1,5 +1,18 @@
 import { RegexTestCaseDescription } from "./regex-task.description";
 
+function simplifyMatch(
+  testCase: RegexTestCaseDescription
+): RegexTestCaseDescription["expected"] {
+  switch (testCase.expected.type) {
+    case "wholeMatch": {
+      return {
+        type: "exactMatch",
+        hits: [testCase.input],
+      };
+    }
+  }
+}
+
 // TODO: Maybe also return list of results
 export function runTestCase(
   regex: RegExp,
