b91b17a1db258b3f82bd1d512b4d089fd8dc0cf9
Marcus Riemer <Marcus@GurXite.de>
2020-08-18 10:24:01 +0200

    Client & GraphQL: Attempt to properly regenerate `graphql.ts`
    Is generated, but does not compile.

--- server/app/graphql/types/grammar_type.rb
+++ server/app/graphql/types/grammar_type.rb
@@ -1,22 +1,22 @@
 module Types
   class Types::GrammarType < Types::Base::BaseObject
-    field :id, ID, null:false
-    field :name, String, null:false
-    field :slug, String, null:true
-    field :types, GraphQL::Types::JSON, null:false, default_value: {}
-    field :foreign_types, GraphQL::Types::JSON, null:false, default_value: {}
-    field :root, Types::Scalar::QualifiedTypeName, null:false, default_value: {"typeName"=> "","languageName"=> ""}
-    field :programmingLanguageId,  ID, null:false
-    field :programmingLanguage, Types::ProgrammingLanguageType, null:false
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :slug, String, null: true
+    field :types, GraphQL::Types::JSON, null: false, default_value: {}
+    field :foreign_types, GraphQL::Types::JSON, null: false, default_value: {}
+    field :root, Types::Scalar::QualifiedTypeName, null: true
+    field :programmingLanguageId,  ID, null: false
+    field :programmingLanguage, Types::ProgrammingLanguageType, null: false
     field :generatedFromId, ID, null:true
-    field :generatedFrom, Types::CodeResourceType, null:true
-    field :blockLanguages, Types::BlockLanguageType.connection_type, null:true
-    field :codeResources, Types::CodeResourceType.connection_type, null:true
-    field :includes, [ID], null:false
-    field :visualizes, [ID], null:false
+    field :generatedFrom, Types::CodeResourceType, null: true
+    field :blockLanguages, Types::BlockLanguageType.connection_type, null: true
+    field :codeResources, Types::CodeResourceType.connection_type, null: true
+    field :includes, [ID], null: true
+    field :visualizes, [ID], null: true
 
-    field :createdAt, GraphQL::Types::ISO8601DateTime, null:false
-    field :updatedAt, GraphQL::Types::ISO8601DateTime, null:false
+    field :createdAt, GraphQL::Types::ISO8601DateTime, null: false
+    field :updatedAt, GraphQL::Types::ISO8601DateTime, null: false
 
     class OrderFieldEnum < Types::Base::BaseEnum
       graphql_name 'GrammarOrderFieldEnum'
