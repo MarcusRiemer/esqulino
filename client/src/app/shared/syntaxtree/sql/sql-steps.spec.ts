776bd16750bc871bed663e621c497dba09a90b5f
D.Achziger <inf102736@fh-wedel.de>
2020-10-30 14:32:48 +0100

    Client: Added first basic functions and tests for sql-steps

--- client/src/app/shared/syntaxtree/sql/sql-steps.spec.ts
+++ client/src/app/shared/syntaxtree/sql/sql-steps.spec.ts
@@ -1,8 +1,333 @@
-import { stepwiseSqlQuery } from "./sql-steps";
+import { stepwiseSqlQuery, SqlStepDescription } from "./sql-steps";
+import { Tree, Node, NodeDescription } from "../syntaxtree";
+
+
+function starSelectTest(stepDesc: SqlStepDescription) {
+  const node = new Node(stepDesc.ast, undefined);
+  expect(node.typeName).toEqual("querySelect");
+  expect(node.getChildrenInCategory("select").length).toEqual(1);
+  const selectNode = node.getChildInCategory("select");
+  expect(selectNode.getChildrenInCategory("columns").length).toEqual(1);
+  expect(selectNode.getChildInCategory("columns").typeName).toEqual(
+    "starOperator"
+  );
+}
+
+function crossJoinTest(stepDesc: SqlStepDescription, desc: NodeDescription, childIndex: number){
+  const node = new Node(stepDesc.ast, undefined);
+  //console.log("crossJoinTest: \n" + JSON.stringify(node.toModel(), undefined, 2));
+
+  expect(node.childrenCategoryNames).toEqual(
+    ["select", "from"]
+  );
+
+  starSelectTest(stepDesc);
+
+  const fromNode = node.getChildInCategory("from");
+  expect(fromNode).toBeDefined();
+  expect(fromNode.childrenCategoryNames).toEqual(["tables", "joins"]);
+
+  //first table should equal to first table from the initial-query
+  // TODO first table could be result table of a previous join
+  expect(fromNode.getChildInCategory("tables").toModel()).toEqual(desc.children.from[0].children.tables[0]);
+
+  //second (join) table
+  expect(fromNode.getChildrenInCategory("joins").length).toEqual(childIndex+1);
+  const joinNode = fromNode.getChildrenInCategory("joins")[childIndex];
+  // joinNode without on
+  expect(joinNode.childrenCategoryNames).toEqual(["table"]);
+  // innerJoinOn -> innerJoin
+  expect(joinNode.typeName).toEqual("innerJoin");
+  expect(joinNode.getChildrenInCategory("table").length).toEqual(1);
+  expect(joinNode.getChildInCategory("table").toModel()).toEqual(desc.children.from[0].children.joins[childIndex].children.table[0]);
+
+  expect(stepDesc.description).toEqual(
+    {
+      type: "inner",
+      tables: [fromNode.children.tables[0].properties.name, joinNode.children.table[0].properties.name]
+    }
+  );
+}
+
+function onNodeTest(stepDesc: SqlStepDescription, desc: NodeDescription, index: number) {
+  const sourceTree = new Tree(desc);
+  const node = new Node(stepDesc.ast, undefined);
+  //console.log("checkOnNode: \n" + JSON.stringify(node.toModel(), undefined, 2));
+  starSelectTest(stepDesc);
+
+  expect(node.childrenCategoryNames).toEqual(
+    ["select", "from"]
+  );
+
+  //maybe pass onNode as parameter
+  const onNode = node.getChildInCategory("from").getChildrenInCategory("joins")[index].getChildInCategory("on");
+  expect(onNode.toModel()).toEqual(desc.children.from[0].children.joins[index].children.on[0]);
+
+  // TODO probably not generally applicable in function
+  let lhs = sourceTree.locate([["from", 0], ["joins", index], ["on", 0], ["lhs",0]]);
+  let rhs = sourceTree.locate([["from", 0], ["joins", index], ["on", 0], ["rhs",0]]);
+  let operator = sourceTree.locate([["from", 0], ["joins", index], ["on", 0], ["operator",0]]);
+  expect(stepDesc.description).toEqual(
+    {
+      type: "on",
+      expressions: [lhs.properties.refTableName + '.' + lhs.properties.columnName, 
+        operator.properties.operator, rhs.properties.refTableName + '.' + rhs.properties.columnName]
+    }
+  );
+  
+}
 
 describe(`SQL Steps`, () => {
   it(`Empty Tree`, () => {
     expect(stepwiseSqlQuery(undefined)).toEqual([]);
-    expect(() => stepwiseSqlQuery(undefined)).toThrowError(/Empty Tree/);
+    //expect(() => stepwiseSqlQuery(undefined)).toThrowError(/Empty Tree/);
+  });
+
+  it('Basic select-from, not breaking down', () => {
+    const desc: NodeDescription = require('./spec/ast-40-select-from.json');
+    const steps = stepwiseSqlQuery(desc);
+    const node = new Node(steps[0].ast, undefined);
+
+    expect(steps.length).toEqual(1);
+
+    starSelectTest(steps[0]);
+    expect(node.toModel()).toEqual(desc);
+
+    expect(steps[0].description).toEqual(
+      {
+        type: "select",
+        expressions: ["*"]
+      }
+    );
   });
 });
+
+describe(`simple where filter`, () => {
+  const desc: NodeDescription = require('./spec/ast-41-select-from-where.json');
+  const steps = stepwiseSqlQuery(desc);
+
+  // first step containing a starSelect with the where-clause
+  it('first step', () => {
+    expect(steps.length).toEqual(2);
+
+    const node = new Node(steps[0].ast, undefined);
+    expect(node.childrenCategoryNames).toEqual(
+      ["select", "from", "where"]
+    );
+
+    starSelectTest(steps[0]);
+
+    expect(node.getChildInCategory("where").toModel()).toEqual(desc.children.where[0]);
+
+    expect(steps[0].description).toEqual(
+      {
+        type: "where",
+        expressions: ["adresse.LKZ", "<>", "D"]
+      }
+    );
+  });
+
+  //second step containing the fields from the select-clause and the where-clause
+  it('second step', () => {
+    const node = new Node(stepwiseSqlQuery(desc)[1].ast, undefined);
+    expect(node.childrenCategoryNames).toEqual(
+      ["select", "from", "where"]
+    );
+    // fields from select should correspond to initial-query
+    expect(node.getChildInCategory("select").toModel()).toEqual(desc.children.select[0]);
+
+    //where should correspond to initial-query----- maybe redundant?
+    expect(node.getChildInCategory("where").toModel()).toEqual(desc.children.where[0]);
+
+    expect(steps[1].description).toEqual(
+      {
+        type: "select",
+        expressions: ["termin.TAG"]
+      }
+    );
+  });
+});
+
+
+describe("simple join", () => {
+  const desc: NodeDescription = require('./spec/ast-42-simple-join.json');
+  const steps = stepwiseSqlQuery(desc);
+
+  //first step containing a starSelect with the join-clause
+  it('first step: cross join', () => {
+    expect(steps.length).toEqual(3);
+    crossJoinTest(steps[0], desc, 0);
+  });
+
+  //second step containing the join with the on-clouse
+  it('second step: on-clouse', () => {
+    onNodeTest(steps[1], desc, 0);
+  });
+
+  it('third step: fields from the select-clause', () => {
+    expect(steps[2].ast).toEqual(desc);
+
+    expect(steps[2].description).toEqual(
+      {
+        type: "select",
+        expressions: ["Charakter.Charakter_Name"]
+      }
+    );
+  });
+});
+
+describe("query with join,group,order", () => {
+  const desc: NodeDescription = require('./spec/ast-43-join-group-order.json');
+  const steps = stepwiseSqlQuery(desc);
+
+  //first step containing a starSelect with the join-clause
+  it('first step: cross join', () => {
+    expect(steps.length).toEqual(5);
+
+    crossJoinTest(steps[0], desc, 0);
+  });
+
+  //second step containing the join with the on-clouse
+  it('second step: on-clouse', () => {
+    onNodeTest(steps[1], desc, 0);
+  });
+
+  //third step containing the groupBy-clouse
+  it('third step: groupBy-clouse', () => {
+    expect(steps[2]).toBeDefined();
+    const node = new Node(steps[2].ast, undefined);
+    starSelectTest(steps[2]);
+    // TODO test from ?
+    expect(node.childrenCategoryNames).toEqual(["select", "from", "groupBy"]);
+    expect(node.getChildInCategory("groupBy").toModel()).toEqual(desc.children.groupBy[0]);
+
+    expect(steps[2].description).toEqual(
+      {
+        type: "groupBy",
+        expressions: ["Charakter.Charakter_ID"]
+      }
+    );
+  });
+
+  it('fourth step: select-clause', () => {
+    //TODO test the other nodes ?
+    const node = new Node(steps[3].ast, undefined);
+    expect(node.childrenCategoryNames.sort()).toEqual(["from", "groupBy", "select"]);
+
+    // contain all select fields
+    expect(node.getChildInCategory("select").toModel()).toEqual(desc.children.select[0]);
+
+    expect(steps[3].description).toEqual(
+      {
+        type: "select",
+        expressions: ["COUNT()", "Charakter.Charakter_Name"]
+      }
+    );
+  });
+
+  it('fifth step: orderBy-clause', () => {
+    expect(steps[4]).toBeDefined();
+    const node = new Node(steps[4].ast, undefined);
+ 
+    // TODO test from ?
+    expect(node.childrenCategoryNames.sort()).toEqual(["from", "groupBy", "orderBy", "select"]);
+    expect(node.getChildInCategory("orderBy").toModel()).toEqual(desc.children.orderBy[0]);
+
+    // only this enough?
+    expect(node.toModel()).toEqual(desc);
+
+    expect(steps[4].description).toEqual(
+      {
+        type: "orderBy",
+        expressions: ["COUNT()", "DESC", "Charakter.Charakter_Name"]
+      }
+    );
+  });
+
+});
+
+describe("two inner-joins", () => {
+  /*
+  SELECT Charakter.Charakter_Name, Geschichte.Geschichte_Name
+    FROM Charakter
+	INNER JOIN Auftritt ON Auftritt.Charakter_ID = Charakter.Charakter_ID
+	INNER JOIN Geschichte ON Auftritt.Geschichte_ID = Geschichte.Geschichte_ID*/
+  const desc: NodeDescription = require('./spec/ast-44-two-inner-joins.json');
+  const steps = stepwiseSqlQuery(desc);
+  
+    /*
+    SELECT *
+      FROM Charakter
+    INNER JOIN Auftritt 
+  */
+  it('first step: cross-join', () => { 
+    expect(steps.length).toEqual(5);
+    crossJoinTest(steps[0], desc, 0);
+  });
+
+   //second step containing the join with the on-clouse
+   /* 
+    SELECT *
+      FROM Charakter
+	  INNER JOIN Auftritt ON Auftritt.Charakter_ID = Charakter.Charakter_ID
+   */
+   it('second step: on-filter', () => {
+
+    //const node = new Node(steps[1].ast, undefined);
+    onNodeTest(steps[1], desc, 0);
+  });
+
+    /*
+    SELECT *
+      FROM Charakter
+	  INNER JOIN Auftritt ON Auftritt.Charakter_ID = Charakter.Charakter_ID
+    INNER JOIN Geschichte 
+    */
+  it('third step: second inner join', () => {
+    //const node = new Node(steps[2].ast, undefined);
+    //console.log("checkOnNode: \n" + JSON.stringify(node.toModel(), undefined, 2));
+    crossJoinTest(steps[2], desc, 1);
+
+  });
+
+      /*
+    SELECT *
+      FROM Charakter
+	  INNER JOIN Auftritt ON Auftritt.Charakter_ID = Charakter.Charakter_ID
+    INNER JOIN Geschichte ON Auftritt.Geschichte_ID = Geschichte.Geschichte_ID
+    */
+   it('fourth step: second inner join with on-filter', () => {
+    //const node = new Node(steps[2].ast, undefined);
+    //console.log("checkOnNode: \n" + JSON.stringify(node.toModel(), undefined, 2));
+    onNodeTest(steps[3], desc, 1);
+
+  });
+
+      /*
+    SELECT Charakter.Charakter_Name, Geschichte.Geschichte_Name
+      FROM Charakter
+	  INNER JOIN Auftritt ON Auftritt.Charakter_ID = Charakter.Charakter_ID
+    INNER JOIN Geschichte ON Auftritt.Geschichte_ID = Geschichte.Geschichte_ID*/
+    it('fifth step: fields from the select-clause', () => {
+      //TODO test the other nodes ?
+      const node = new Node(steps[4].ast, undefined);
+      expect(node.childrenCategoryNames.sort()).toEqual(["from", "select"]);
+  
+      // contain all select fields
+      expect(node.getChildInCategory("select").toModel()).toEqual(desc.children.select[0]);
+  
+      // only this enough?
+      expect(node.toModel()).toEqual(desc);
+
+      expect(steps[4].description).toEqual(
+        {
+          type: "select",
+          expressions: ["Charakter.Charakter_Name", "Geschichte.Geschichte_Name"]
+        }
+      );
+    });
+});
+
+// next: test outer-join
+
+
--- client/src/app/shared/syntaxtree/sql/sql-steps.ts
+++ client/src/app/shared/syntaxtree/sql/sql-steps.ts
@@ -1,19 +1,42 @@
-import { NodeDescription, Tree } from "../syntaxtree";
+import { NodeDescription, Tree, Node } from "../syntaxtree";
 
 interface SqlStepInnerJoinDescription {
   type: "inner";
   tables: string[];
 }
 
-interface SqlStepInnerWhereDescription {
-  type: "inner";
+interface SqlStepSelectDescription {
+  type: "select";
   expressions: string[];
 }
 
-interface SqlStepDescription {
-  ast: NodeDescription;
-  description: SqlStepInnerJoinDescription | SqlStepInnerWhereDescription;
+interface SqlStepWhereDescription {
+  type: "where";
+  expressions: string[];
 }
+
+interface SqlStepOnDescription {
+  type: "on";
+  expressions: string[];
+}
+
+interface SqlStepGroupByDescription {
+  type: "groupBy";
+  expressions: string[];
+}
+
+interface SqlStepOrderByDescription {
+  type: "orderBy";
+  expressions: string[];
+}
+
+export interface SqlStepDescription {
+  ast: NodeDescription;
+  description: SqlStepSelectDescription | SqlStepWhereDescription
+  | SqlStepInnerJoinDescription | SqlStepOnDescription | SqlStepGroupByDescription
+  | SqlStepOrderByDescription;
+}
+
 
 export function stepwiseSqlQuery(
   q: Tree | NodeDescription // Maybe decide which representation is most helpful
@@ -21,5 +44,213 @@ export function stepwiseSqlQuery(
   q = q instanceof Tree ? q : new Tree(q);
   // TODO: Transform
 
-  return [];
+  if(q.isEmpty){
+    // throwError?
+    return [];
+  }
+
+  let arr: SqlStepDescription[] = [];
+  //let starSelect: NodeDescription = createSelectNodeDescription();
+  let t = new Tree({
+    name: "querySelect",
+    language: "sql",
+    children: {
+      select: [createSelectNodeDescription()],
+      from: [
+        {
+          name: "from",
+          language: "sql"
+        }
+      ]
+    }
+  });
+
+  //first from table
+  let table = q.locate([["from", 0], ["tables", 0]]);
+  t = t.insertNode([["from", 0], ["tables", 0]], table.toModel());
+
+  //cartesian product (cross join) if more then one table
+
+  let joinNode = q.locateOrUndefined([["from", 0], ["joins", 0]]);
+  let index = 0;
+  while (joinNode != undefined) {
+    // TODO distinguish between join types
+
+    let joinTable = joinNode.getChildInCategory("table");
+
+    // innerJoinOn -> innerJoin
+    let typeName = joinNode.typeName == "innerJoinOn" ? "innerJoin" : joinNode.typeName;
+
+    // maybe customizing existing Node more efficient then insert a new one ?
+    t = t.insertNode([["from", 0], ["joins", index]],
+      {
+        name: typeName,
+        language: joinNode.languageName,
+        children: {
+          table: [joinTable.toModel()]
+        }
+      }
+    );
+
+    arr.push({
+      ast: t.toModel(),
+      description: {
+        type: "inner",
+        tables: [table.properties.name, joinTable.properties.name]
+      }
+    });
+    //console.log('join added');
+
+    // 2. case innerJoinOn -  add the on-filter 
+    let onNode = q.locateOrUndefined([["from", 0], ["joins", index], ["on", 0]]);
+    if (onNode != undefined) {
+
+      //change join name and add onNode
+      t = t.replaceNode([["from", 0], ["joins", index]], joinNode.toModel());
+      
+      arr.push({
+        ast: t.toModel(),
+        description: {
+          type: "on",
+          expressions: collectExpressions(joinNode.getChildrenInCategory("on"))
+        }
+      });
+      //console.log("after adding onNode: \n" + JSON.stringify(t.toModel(), undefined, 2));
+    }
+    joinNode = q.locateOrUndefined([["from", 0], ["joins", ++index]]);
+  }
+
+  //WHERE clause
+  let whereNode = q.locateOrUndefined([["where", 0]]);
+  if (whereNode != undefined) {
+    t = t.insertNode([["where", 0]], whereNode.toModel());
+
+    arr.push({
+      ast: t.rootNode.toModel(),
+      description: {
+        type: "where",
+        expressions: collectExpressions(whereNode.getChildrenInCategory("expressions"))
+      }
+    });
+    //console.log("after where-step: \n" + JSON.stringify(t.toModel(), undefined, 2));
+  }
+
+  //GROUP BY clause
+  let groupNode = q.locateOrUndefined([["groupBy", 0]]);
+  if (groupNode != undefined) {
+    t = t.insertNode([["groupBy", 0]], groupNode.toModel());
+
+    arr.push({
+      ast: t.rootNode.toModel(),
+      description: {
+        type: "groupBy",
+        expressions: collectExpressions(groupNode.getChildrenInCategory("expressions"))
+      }
+    });
+    //console.log('groupBy added');
+  }
+
+  // HAVING clause -> needed???
+
+  //SELECT clause
+  if (!(q.locate([["select", 0], ["columns", 0]]).typeName == "starOperator")) {
+    t = t.replaceNode([["select", 0]], q.locate([["select", 0]]).toModel());
+  } 
+
+  arr.push({
+    ast: t.rootNode.toModel(),
+    description: {
+      type: "select",
+      expressions: collectExpressions(q.locate([["select", 0]]).children.columns)
+    }
+  });
+  //console.log("after select-step: \n" + JSON.stringify(t.toModel(), undefined, 2));
+
+  // TODO DISCTINCT ??
+
+  //ORDER BY clause
+  let orderByNode = q.locateOrUndefined([["orderBy", 0]]);
+  if (orderByNode != undefined) {
+    t = t.insertNode([["orderBy", 0]], orderByNode.toModel());
+
+    arr.push({
+      ast: t.rootNode.toModel(),
+      description: {
+        type: "orderBy",
+        expressions: collectExpressions(orderByNode.getChildrenInCategory("expressions"))
+      }
+    });
+    //console.log('orderBy added');
+  }
+
+  return arr;
+}
+
+
+function createSelectNodeDescription(): NodeDescription {
+  return {
+    name: "select",
+    language: "sql",
+    children: {
+      columns: [
+        {
+          name: "starOperator",
+          language: "sql"
+        }
+      ]
+    }
+  };
+}
+
+function collectExpressions(nodes: Node[]): string[] {
+  // typedef "sql"."expression" ::= columnName | binaryExpression | constant | parameter | functionCall | parentheses
+  let exp: string[] = [];
+  
+  for(let node of nodes){
+  
+    switch(node.typeName){
+      case "binaryExpression": {
+        exp = exp.concat(collectExpressions(node.getChildrenInCategory("lhs")));
+        exp.push(node.getChildInCategory("operator").properties.operator);
+        exp = exp.concat(collectExpressions(node.getChildrenInCategory("rhs")));
+        break;
+      }
+      case "columnName": {
+        exp.push(node.properties.refTableName + '.' + node.properties.columnName);
+        break;
+      }
+      case "constant": {
+        exp.push(node.properties.value);
+        break;
+      }
+      case "parameter": {
+        break;
+      }
+      case "functionCall": {
+        exp.push(node.properties.name + "()");
+        exp = exp.concat(collectExpressions(node.getChildrenInCategory("arguments")));
+        break;
+      }
+      case "parentheses": {
+        break;
+      }
+      // actually not an expression-type
+      case "sortOrder": {
+        // typename expression instead of expressions
+        exp = exp.concat(collectExpressions(node.getChildrenInCategory("expression")));
+        exp.push(node.properties.order);
+        break;
+      }
+      case "starOperator": {
+        exp.push("*");
+        break;
+      }
+      default: {
+        console.log("default case should not appear: " + node.typeName);
+        //return [];
+      }
+    }
+  }
+  //console.log("return exp:\n" + exp);
+  return exp;
 }
