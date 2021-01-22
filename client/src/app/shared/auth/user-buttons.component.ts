c768a044d4e9f3daa623ca87d4c47153c633e329
Marcus Riemer <Marcus@GurXite.de>
2021-01-22 14:59:40 +0100

    Client: Don't show auth dialogue for single provider

--- client/src/app/shared/auth/user-buttons.component.ts
+++ client/src/app/shared/auth/user-buttons.component.ts
@@ -1,10 +1,14 @@
 import { Router } from "@angular/router";
 import { MatDialog } from "@angular/material/dialog";
 import { Component } from "@angular/core";
+
 import { first } from "rxjs/operators";
+
+import { ServerApiService } from "../serverdata";
 
 import { AuthDialogComponent } from "./auth-dialog.component";
 import { UserService } from "./user.service";
+
 @Component({
   selector: "user-buttons",
   templateUrl: "./templates/user-buttons.html",
@@ -13,7 +17,8 @@ export class UserButtonsComponent {
   constructor(
     private _dialog: MatDialog,
     private _userService: UserService,
-    private _router: Router
+    private _router: Router,
+    private _serverApi: ServerApiService
   ) {}
 
   /**
@@ -21,18 +26,34 @@ export class UserButtonsComponent {
    */
   public userDisplayName$ = this._userService.userDisplayName$;
 
+  get providerList() {
+    return this._userService.availableProviders.value.pipe(first()).toPromise();
+  }
+
   /**
    * Opens a dialog for sign in or sign up
    * Dialog: https://material.angular.io/components/dialog/overview
    */
-  public openDialog(type: "signIn" | "signUp"): void {
-    AuthDialogComponent.showDialog(this._dialog, { type: type });
-    // If youre on the base url and your loggin in, the dialog will be closed#
-    this._userService.isLoggedIn$.pipe(first()).subscribe((loggedIn) => {
-      if (loggedIn) {
-        this._dialog.closeAll();
-      }
-    });
+  async openDialog(type: "signIn" | "signUp") {
+    console.log("Attempting to show login dialog ...");
+
+    const providerList = await this.providerList;
+
+    // Show a Dialog if the user can choose between multiple providers
+    if (providerList.length > 1) {
+      AuthDialogComponent.showDialog(this._dialog, { type: type });
+      // If youre on the base url and your loggin in, the dialog will be closed#
+      this._userService.isLoggedIn$.pipe(first()).subscribe((loggedIn) => {
+        if (loggedIn) {
+          this._dialog.closeAll();
+        }
+      });
+    }
+    // Redirect to the only provider that is available
+    else if (providerList.length === 1) {
+      const soleProvider = providerList[0];
+      window.location.href = this._serverApi.getSignInUrl(soleProvider.urlName);
+    }
   }
 
   /**
