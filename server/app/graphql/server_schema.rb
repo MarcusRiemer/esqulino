eb06bafc7d9b4b8e5ca04e6dd641ec71d2305bee
Marcus Riemer <Marcus@GurXite.de>
2021-02-07 10:40:02 +0100

    Server: Disable GraphQL introspection in live environment

--- server/app/graphql/server_schema.rb
+++ server/app/graphql/server_schema.rb
@@ -1,6 +1,10 @@
 class ServerSchema < GraphQL::Schema
   default_max_page_size 100
+
   # Required basic schema parts
   mutation(Types::MutationType)
   query(Types::QueryType)
+
+  # No need to allow introspection for live environments
+  disable_introspection_entry_points if Rails.env.production?
 end
