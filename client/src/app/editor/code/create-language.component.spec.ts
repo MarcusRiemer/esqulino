7c86c4d1583594f2a8a05a195bc8deef1e13bee8
Marcus Riemer <Marcus@GurXite.de>
2021-03-11 14:12:04 +0100

    Client: Minimal set of dependencies for `CreateLanguageComponent` spec

--- client/src/app/editor/code/create-language.component.spec.ts
+++ client/src/app/editor/code/create-language.component.spec.ts
@@ -1,5 +1,18 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterTestingModule } from "@angular/router/testing";
+import { ApolloTestingModule } from "apollo-angular/testing";
+import { CreateProgrammingLanguageGQL } from "../../../generated/graphql";
+import { LanguageService, ServerApiService } from "../../shared";
+import { EmptyComponent } from "../../shared/empty.component";
+import { ResourceReferencesOnlineService } from "../../shared/resource-references-online.service";
+import { ResourceReferencesService } from "../../shared/resource-references.service";
+import {
+  IndividualBlockLanguageDataService,
+  IndividualGrammarDataService,
+} from "../../shared/serverdata";
+import { ProjectService } from "../project.service";
 import { CreateLanguageComponent } from "./create-language.component";
 
 describe("CreateLanguageComponent", () => {
@@ -8,6 +21,26 @@ describe("CreateLanguageComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        ApolloTestingModule,
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([
+          { path: ":id", component: EmptyComponent },
+        ]),
+        HttpClientTestingModule,
+      ],
+      providers: [
+        ProjectService,
+        LanguageService,
+        ServerApiService,
+        CreateProgrammingLanguageGQL,
+        {
+          provide: ResourceReferencesService,
+          useClass: ResourceReferencesOnlineService,
+        },
+        IndividualBlockLanguageDataService,
+        IndividualGrammarDataService,
+      ],
       declarations: [CreateLanguageComponent],
     }).compileComponents();
   });
