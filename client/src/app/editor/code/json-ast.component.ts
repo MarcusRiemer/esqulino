26950b9bf6e52e057604996ae2ccfa2635b009a0
Marcus Riemer <Marcus@GurXite.de>
2021-05-20 15:17:24 +0200

    Client: Added possiblity to copy JSON AST

--- client/src/app/editor/code/json-ast.component.ts
+++ client/src/app/editor/code/json-ast.component.ts
@@ -1,13 +1,14 @@
 import { Component } from "@angular/core";
 
 import { CurrentCodeResourceService } from "../current-coderesource.service";
-import { switchMap, map } from "rxjs/operators";
+import { switchMap, map, first } from "rxjs/operators";
 
 /**
  * Shows the JSON representation of a certain resource
  */
 @Component({
   templateUrl: "templates/json-ast.html",
+  styleUrls: ["json-ast.component.scss"],
 })
 export class JsonAstComponent {
   constructor(private _currentCodeResource: CurrentCodeResourceService) {}
@@ -16,4 +17,19 @@ export class JsonAstComponent {
     switchMap((c) => c.syntaxTree$),
     map((t) => t.toModel())
   );
+
+  readonly hasClipboard = !!navigator.clipboard;
+
+  async copyToClipboard() {
+    if (this.hasClipboard) {
+      const content = await this.jsonTree$.pipe(first()).toPromise();
+      await navigator.clipboard.writeText(
+        JSON.stringify(content, undefined, 2)
+      );
+
+      console.log("Copied the following AST to the clipboard", content);
+    } else {
+      alert("Sorry, no clipboard availabe");
+    }
+  }
 }
