676ff7f5afde70bd45dacd1a7077eb70e4890b2f
Marcus Riemer <Marcus@GurXite.de>
2021-05-21 13:06:34 +0200

    Server: Project creation defaults guest user to system user

---
+++ server/spec/graphql/mutations/projects/create_project_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.describe Mutations::Projects::CreateProject do
+
+  # These specs relies on
+  # * an existing guest user
+  # * an existing meta grammar language
+  before(:each) {
+    create(:user, :guest)
+    create(:user, :system)
+  }
+
+  def init_args(user: User.guest)
+    {
+      context: {
+        user: user
+      },
+      object: nil,
+      field: nil,
+    }
+  end
+
+  it "Creation as guest denied" do
+    mut = described_class.new(**init_args())
+
+    expect { mut.resolve(name: { "en" => "Test" }) }.to raise_error(Pundit::NotAuthorizedError)
+    expect(Project.count).to eq 0
+  end
+
+  it "Creation as admin works" do
+    admin = create(:user, :admin)
+    mut = described_class.new(**init_args(user: admin))
+
+    result = mut.resolve(name: { "en" => "Test" })
+
+    p = Project.find(result[:id])
+
+    expect(p.user_id).to eq admin.id
+    expect(p.name).to eq({ "en" => "Test" })
+  end
+
+  it "Creation as admin guest ends up as system creation" do
+    User.make_guest_admin!
+
+    mut = described_class.new(**init_args())
+
+    result = mut.resolve(name: { "en" => "Test" })
+    expect(Project.first.user_id).to eq User.system_id
+  end
+
+end
