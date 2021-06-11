c09a7ac48d4259d9fc8455b2aa3531e1f84dec70
LeonardoAG <leonardo.adriano.goncalves@gmail.com>
2021-06-11 17:07:20 +0200

    Server: Create migrations for the assigments

---
+++ server/db/migrate/20210608075017_create_assigments.rb
@@ -0,0 +1,12 @@
+class CreateAssigments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :assigments, id: :uuid, default: 'gen_random_uuid()' do |t|
+      t.string :name
+      t.timestamp :start_date
+      t.timestamp :end_date
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
