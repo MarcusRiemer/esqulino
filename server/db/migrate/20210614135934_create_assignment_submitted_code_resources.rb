937680fcd916055ada2b69b41d2f908cb03456b1
Leonardo <leonardo.adriano.goncalves@gmail.com>
2021-06-15 11:32:35 +0200

    Server & Client: Generate Schema and added index names to the migrations

--- server/db/migrate/20210614135934_create_assignment_submitted_code_resources.rb
+++ server/db/migrate/20210614135934_create_assignment_submitted_code_resources.rb
@@ -1,9 +1,9 @@
 class CreateAssignmentSubmittedCodeResources < ActiveRecord::Migration[6.1]
   def change
     create_table :assignment_submitted_code_resources, id: :uuid, default: 'gen_random_uuid()' do |t|
-      t.references :assignment_required_code_resource, null: true, type: :uuid, foreign_key: true
+      t.references :assignment_required_code_resource, null: true, type: :uuid, foreign_key: true, index: {:name => "index_required_code_ressource"}
       t.references :code_resource, null: false, type: :uuid, foreign_key: true
-      t.references :assigment_submission, null: false, type: :uuid, foreign_key: true
+      t.references :assignment_submission, null: false, type: :uuid, foreign_key: true, index: {:name => "index_submitted_code_ressource"}
 
       t.timestamps
     end
