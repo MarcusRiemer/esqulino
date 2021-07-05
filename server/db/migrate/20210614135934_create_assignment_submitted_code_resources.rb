9690f5b44af2a252760685efb8906349efa2c0bb
Leonardo <leonardo.adriano.goncalves@gmail.com>
2021-07-05 15:57:27 +0200

    Server: Added unique references for submitted- and template_code_resources

--- server/db/migrate/20210614135934_create_assignment_submitted_code_resources.rb
+++ server/db/migrate/20210614135934_create_assignment_submitted_code_resources.rb
@@ -2,7 +2,7 @@ class CreateAssignmentSubmittedCodeResources < ActiveRecord::Migration[6.1]
   def change
     create_table :assignment_submitted_code_resources, id: :uuid, default: 'gen_random_uuid()' do |t|
       t.references :assignment_required_code_resource, null: false, type: :uuid, foreign_key: true, index: {:name => "index_assignment_submission_c_r_on_assignment_required_c_r"}
-      t.references :code_resource, null: false, type: :uuid, foreign_key: true
+      t.references :code_resource, null: false, type: :uuid, foreign_key: true, index: {unique: true}
       t.references :assignment_submission, null: false, type: :uuid, foreign_key: true, index: {:name => "index_assignment_submitted_c_r_on_assignment_submission"} 
 
       t.timestamps
--- server/db/migrate/20210630121249_create_assignment_template_code_resources.rb
+++ server/db/migrate/20210630121249_create_assignment_template_code_resources.rb
@@ -3,7 +3,7 @@ class CreateAssignmentTemplateCodeResources < ActiveRecord::Migration[6.1]
     create_table :assignment_template_code_resources, id: :uuid, default: 'gen_random_uuid()'  do |t|
 
       t.references :code_resource, null: false, type: :uuid, foreign_key: true, index: {:name => "index_assignment_template_on_code_resource"}
-      t.references :assignment_required_code_resource, null: false, type: :uuid, foreign_key: true, index: {:name => "index_assignment_template_on_assignment_required"}
+      t.references :assignment_required_code_resource, null: false, type: :uuid, foreign_key: true, index: {:name => "index_assignment_template_on_assignment_required", unique: true}
       t.integer :reference_type, null: false
 
 
