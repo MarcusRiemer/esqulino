5481060c8ba8598ff80785656a74d923fe626703
Marcus Riemer <Marcus@GurXite.de>
2021-07-26 09:47:51 +0200

    Client: WIP to demonstrate how assignment navigation data can be mapped

--- client/src/app/editor/course/navigation/course-nav.component.ts
+++ client/src/app/editor/course/navigation/course-nav.component.ts
@@ -1,17 +1,58 @@
 import { Component } from "@angular/core";
-import { concat, forkJoin, from } from "rxjs";
-import {
-  filter,
-  first,
-  map,
-  distinctUntilChanged,
-  concatMap,
-  switchMap,
-  toArray,
-} from "rxjs/operators";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
-import { FullProjectGQL } from "../../../../generated/graphql";
+import {
+  AssignmentTemplateCodeResource,
+  FullProjectGQL,
+} from "../../../../generated/graphql";
 import { ProjectService } from "../../project.service";
+
+interface CourseNavEntry {
+  id: string;
+  name: string;
+  templates: {
+    id: string;
+    reference_type: AssignmentTemplateCodeResource["referenceType"];
+  }[];
+  solutions: {
+    name: string;
+  }[];
+}
+
+interface CourseNavEntryAlternative {
+  id: string;
+  name: string;
+  references: (
+    | {
+        type: "template";
+        id: string;
+        name: string;
+        reference_type: AssignmentTemplateCodeResource["referenceType"];
+      }
+    | {
+        type: "solution";
+        id: string;
+        name: string;
+      }
+  )[];
+}
+
+interface CourseNavEntryAlternativeWithPick {
+  id: string;
+  name: string;
+  references: (
+    | ({ type: "template" } & Pick<
+        AssignmentTemplateCodeResource,
+        "id" | "referenceType"
+      >)
+    | {
+        type: "solution";
+        id: string;
+        name: string;
+      }
+  )[];
+}
 
 @Component({
   selector: "course-nav",
@@ -26,92 +67,37 @@ export class CourseNavComponent {
     private readonly _fullProject: FullProjectGQL
   ) {}
 
-  readonly courseData$ = this._fullProject
+  readonly fullCourseData$ = this._fullProject
     .watch({ id: this._projectService.cachedProject.id })
     .valueChanges.pipe(map((project) => project.data.project));
 
-  toSideNavStructure(id, name, assignmentRequiredCodeResources) {
-    return {
-      id,
-      name,
-      solutions: assignmentRequiredCodeResources.map(
-        ({ solution }) => solution?.id
-      ),
-      templates: assignmentRequiredCodeResources.map(({ template }) => {
-        return {
-          id: template?.codeResource?.id,
-          reference_type: template?.referenceType,
-        };
-      }),
-    };
-  }
-
-  assignments$ = this.courseData$.pipe(
-    distinctUntilChanged(),
-    switchMap(({ assignments }) =>
-      from(assignments).pipe(
-        map((e) =>
-          this.toSideNavStructure(
-            e.id,
-            e.name,
-            e.assignmentRequiredCodeResources
-          )
-        ),
-        concatMap((assignment) =>
-          this.resolveSolutions(assignment.solutions).pipe(
-            map((solutionCodeResource) => ({
-              ...assignment,
-              solutions: solutionCodeResource,
-            }))
-          )
-        ),
-        // for templates
-        concatMap((assignment) =>
-          this.resolveTemplates(assignment.templates).pipe(
-            map((templates) => ({ ...assignment, templates }))
-          )
-        ),
-        toArray()
-      )
-    )
+  readonly fullAssignments$ = this.fullCourseData$.pipe(
+    map((data) => data.assignments)
   );
 
-  readonly codeResource$ = this.courseData$.pipe(map((p) => p.codeResources));
+  assignments$: Observable<CourseNavEntry[]> = this.fullCourseData$.pipe(
+    map((fullData) => {
+      const toReturn: CourseNavEntry[] = fullData.assignments.map((a) => {
+        const navData = {
+          id: a.id,
+          name: a.name,
+          solutions: a.assignmentRequiredCodeResources
+            .filter((req) => req.solution)
+            .map((req) =>
+              fullData.codeResources.find((res) => res.id === req.solution.id)
+            ),
+          templates: a.assignmentRequiredCodeResources
+            .filter((req) => req.template)
+            .map((req) => ({
+              id: req.template.codeResource.id,
+              reference_type: req.template.referenceType,
+            })),
+        };
 
-  resolveSolutions(ids: string[]) {
-    console.log(ids);
-    const solutions = from(ids).pipe(
-      concatMap((id) =>
-        this.getCodeResourcesOfProject(id).pipe(filter((e) => !!e))
-      ),
-      toArray()
-    );
-    return solutions; //array von Observable<string>[] -> ein Observable<string[]> -> muss fertig sein!
-  }
+        return navData;
+      });
 
-  resolveTemplates(templates: { id: string; reference_type: string }[]) {
-    const solutions = from(templates).pipe(
-      concatMap((template) =>
-        this.getCodeResourcesOfProject(template.id).pipe(
-          map((code_resource) => ({
-            reference_type: template.reference_type,
-            code_resource,
-          }))
-        )
-      ),
-      filter((v) => !!v?.reference_type),
-      toArray() // macht ein Array => Observable<string> => Observable<string []>
-    );
-
-    return solutions;
-  }
-
-  getCodeResourcesOfProject(id: string) {
-    // -> Das await macht den Promis weg
-    return this.codeResource$ //Observabl<CodeResource[]]
-      .pipe(
-        first(),
-        map((e) => e.find((c) => c.id == id))
-      ); //Observabl<CodeResource>;
-  }
+      return toReturn;
+    })
+  );
 }
