ed3896900a51699e7c95adaad93489535455ea12
Leonardo <leonardo.adriano.goncalves@gmail.com>
2021-09-04 12:26:37 +0200

    Server: Added index to grades and members

--- server/db/migrate/20210514094710_create_members.rb
+++ server/db/migrate/20210514094710_create_members.rb
@@ -1,7 +1,6 @@
 class CreateMembers < ActiveRecord::Migration[6.1]
   def change
     create_table :project_members, id: :uuid, default: 'gen_random_uuid()' do |t|
-      
       t.timestamp :joined_at
       t.references :user, null: false, type: :uuid, foreign_key: true
       t.references :project, null: false, type: :uuid, foreign_key: true
@@ -9,5 +8,10 @@ class CreateMembers < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+
+    add_index :project_members,
+              %i[user_id project_id],
+              unique: true,
+              name: 'project_member_references_unique'
   end
 end
