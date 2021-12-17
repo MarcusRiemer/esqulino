9106f44511a73415210cf25831542e1d61693d02
Timo Nagel <tinf103898@fh-wedel.de>
2021-12-17 18:31:08 +0100

    Server is waiting until database is ready

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,20 +2,28 @@
 
 #If the variable is not set the container exit with an error 
 if [ -z "$SECRET_KEY_BASE" ]; then
-	echo "Pleas set SECRET_KEY_BASE in the docker-compose-File" 1>&2
-	exit 1
+  echo "Pleas set SECRET_KEY_BASE in the docker-compose-File" 1>&2
+  exit 1
+fi
+
+if ! pg_isready --host "$DATABASE_HOST" --quiet; then
+  echo "Waiting for database..."
+  while ! pg_isready --host "$DATABASE_HOST" --quiet; do 
+    :
+  done
+  echo "Database is ready"
 fi
 
 #Only at the first start the server and the database will be resetet
 FILE_NAME=init
-if [ ! -e $FILE_NAME ]; then
-	TPUT_BIN=/bin/false make --directory /esqulino/server reset-live-data
-	echo "If you remove this File, the Server and Database will be reset after a newstart." >$FILE_NAME
+if [ ! -e "$FILE_NAME" ]; then
+  TPUT_BIN=/bin/false make --directory /esqulino/server reset-live-data
+  echo "If you remove this File, the Server and Database will be reset after a newstart." >$FILE_NAME
 fi
 
 #If no command is set the server will be startet
 if [ $# -eq 0 ]; then
-	TPUT_BIN=/bin/false make --directory /esqulino/server run
+  TPUT_BIN=/bin/false make --directory /esqulino/server run
 else
-	exec "$@"
+  exec "$@"
 fi
