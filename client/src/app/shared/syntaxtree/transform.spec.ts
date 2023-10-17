791bc1ef3e477495ba469fdf6945741729d28ba1
enme <enme@akra.de>
2023-10-17 15:36:27 +0200

    Expanded the transform.spec.ts file with some new test cases

--- client/src/app/shared/syntaxtree/transform.spec.ts
+++ client/src/app/shared/syntaxtree/transform.spec.ts
@@ -1,13 +1,17 @@
+import { startTransaction } from "@sentry/browser";
 import * as AST from "./syntaxtree";
 import { apply } from "./transform";
 
-/** 
- * Syntax tree for testing the splitting of a single character node into multiple ones if it contains more then one character in its property value. 
- */
+const patterns : string[] = [] ;
 
-const multiValuedCharacterNodeInput : AST.SyntaxTree = new AST.SyntaxTree( 
-    {
-        language : "regex-test",
+it("Multivalued character nodes are seperated into multiple single valued character nodes", () => {
+    /** 
+     * Syntax tree for testing the splitting of a single character node into multiple ones if it 
+     * contains more then one character in its property value. 
+     */
+
+    const inputDesc : AST.NodeDescription = {
+        language : "regex-test", 
         name: "root", 
         children: {
             nodes: [
@@ -21,11 +25,9 @@ const multiValuedCharacterNodeInput : AST.SyntaxTree = new AST.SyntaxTree(
                 }
             ]
         }
-    }
-);
+    };
 
-const multiValuedCharacterNodeResult: AST.SyntaxTree = new AST.SyntaxTree (
-    {
+    const resultDesc : AST.NodeDescription = {
         language : "regex-test",
         name: "root", 
         children: {
@@ -72,9 +74,334 @@ const multiValuedCharacterNodeResult: AST.SyntaxTree = new AST.SyntaxTree (
                 }
             ]
         }
-    }
-);
+    };
 
-it("Multivalued character nodes are seperated into multiple single valued character nodes", () => {
-    expect(multiValuedCharacterNodeResult).toEqual(apply(multiValuedCharacterNodeInput, []));
+    const inp = new AST.SyntaxTree(inputDesc); 
+    const res = new AST.SyntaxTree(resultDesc); 
+    expect(res).toEqual(apply(inp, patterns)); 
 });
+
+it ("Autowrapping of single root quantifier nodes with an invis-container node", () => {
+    // Cases for Quantifiers
+        // * | + | ?
+
+    const starQuantifierDesc : AST.NodeDescription = 
+    {
+        name: "builtInQuantifier",
+        language: "regex",
+        properties: {
+            symbol: "*"
+        },
+        children: {
+            element: [
+            {
+                name: "string",
+                language: "regex",
+                properties: {
+                    value: "abc"
+                }
+            }
+            ],
+        }
+    }
+
+    const inputDesc: AST.NodeDescription = {
+        language : "regex", 
+        name: "root", 
+        children: {
+            nodes: [
+                starQuantifierDesc,
+            ]
+        }
+    };
+
+    const resultDesc: AST.NodeDescription = {
+        language: "regex", 
+        name: "root", 
+        children: {
+            nodes: [
+                {
+                    name: "invis-container",
+                    language: "regex",
+                    children: {
+                      elements: [
+                        starQuantifierDesc,
+                      ]
+                    }
+                }
+            ]
+        }
+    };
+
+    const inp = new AST.SyntaxTree(inputDesc); 
+    const res = new AST.SyntaxTree(resultDesc); 
+    expect(res).toEqual(apply(inp, patterns)); 
+
+}); 
+
+it ("Autowrapping of single root element pattern nodes with an invis-container node", () => {
+    // Cases for ElemPatterns
+    // String
+    // Char
+    // Set 
+    // Group
+    // Shorthand
+    // Meta-chars
+
+    const stringNodeDesc : AST.NodeDescription = {
+        "name": "string",
+        "language": "regex",
+        "properties": {
+          "value": "12345"
+        }
+    }
+
+    const inputDesc: AST.NodeDescription = {
+        language: "regex", 
+        name: "root", 
+        children: {
+            nodes: [
+                stringNodeDesc,
+            ]
+        }
+    }
+
+    const resultDesc: AST.NodeDescription = {
+        language: "regex", 
+        name: "root", 
+        children: {
+            nodes: [
+                {
+                    "name": "invis-container",
+                    "language": "regex",
+                    "children": {
+                      "elements": [
+                        {
+                          "name": "string",
+                          "language": "regex",
+                          "properties": {
+                            "value": "12345"
+                          }
+                        }
+                      ]
+                    }
+                }
+            ]
+        }
+    }
+
+
+    const inp = new AST.SyntaxTree(inputDesc); 
+    const res = new AST.SyntaxTree(resultDesc); 
+    expect(res).toEqual(apply(inp, patterns)); 
+
+}); 
+
+/* it ("Autowrapping of single root alternation pattern nodes with an invis-container node", () => {
+    // Cases for alternations 
+        // Binary Alternation 
+        // N-ary Alternation
+
+    const binaryAltDesc : AST.NodeDescription = {
+
+    }
+
+    const inputDesc: AST.NodeDescription = {
+        language: "regex", 
+        name: "root", 
+        children: {
+            nodes: [
+                
+            ]
+        }
+    }
+
+    const resultDesc: AST.NodeDescription = {
+        language: "regex", 
+        name: "root", 
+        children: {
+            nodes: [
+                
+            ]
+        }
+    }
+
+
+    const inp = new AST.SyntaxTree(inputDesc); 
+    const res = new AST.SyntaxTree(resultDesc); 
+    expect(res).toEqual(apply(inp, patterns)); 
+
+});  */
+
+it ("Removal of nested invis-container nodes", () => {
+    const inputDesc: AST.NodeDescription = {
+        language: "regex", 
+        name: "root", 
+        children: {
+            nodes: [
+                {
+                    "name": "invis-container",
+                    "language": "regex",
+                    "children": {
+                      "elements": [
+                        {
+                          "name": "invis-container",
+                          "language": "regex",
+                          "children": {
+                            "elements": [
+                              {
+                                "name": "group",
+                                "language": "regex",
+                                "children": {
+                                  "elements": [
+                                    {
+                                      "name": "string",
+                                      "language": "regex",
+                                      "properties": {
+                                        "value": "abcd"
+                                      }
+                                    }
+                                  ]
+                                }
+                              }
+                            ]
+                          }
+                        }
+                      ]
+                    }
+                }
+            ]
+        }
+    }
+
+    const resultDesc: AST.NodeDescription = {
+        language: "regex", 
+        name: "root", 
+        children: {
+            nodes: [
+                {
+                    "name": "invis-container",
+                    "language": "regex",
+                    "children": {
+                      "elements": [
+                        {
+                          "name": "group",
+                          "language": "regex",
+                          "children": {
+                            "elements": [
+                              {
+                                "name": "string",
+                                "language": "regex",
+                                "properties": {
+                                  "value": "abcd"
+                                }
+                              }
+                            ]
+                          }
+                        }
+                      ]
+                    }
+                }
+            ]
+        }
+    }
+
+
+    const inp = new AST.SyntaxTree(inputDesc); 
+    const res = new AST.SyntaxTree(resultDesc); 
+    expect(res).toEqual(apply(inp, patterns)); 
+
+}); 
+
+it ("Merging of two sibling invis-container nodes into one invis-container node that contains all their children", () => {
+
+    const inputDesc: AST.NodeDescription = {
+        language: "regex", 
+        name: "root", 
+        children: {
+            nodes: [
+                {
+                    "name": "group",
+                    "language": "regex",
+                    "children": {
+                      "elements": [
+                        {
+                          "name": "invis-container",
+                          "language": "regex",
+                          "children": {
+                            "elements": [
+                                {
+                                    "name": "string",
+                                    "language": "regex",
+                                    "properties": {
+                                      "value": "hello "
+                                    }
+                                  }
+                            ]
+                          }
+                        },
+                        {
+                          "name": "invis-container",
+                          "language": "regex",
+                          "children": {
+                            "elements": [
+                                {
+                                    "name": "string",
+                                    "language": "regex",
+                                    "properties": {
+                                      "value": "world"
+                                    }
+                                }
+                            ]
+                          }
+                        }
+                      ]
+                    }
+                }
+            ]
+        }
+    }
+
+    const resultDesc: AST.NodeDescription = {
+        language: "regex", 
+        name: "root", 
+        children: {
+            nodes: [
+                {
+                    "name": "group",
+                    "language": "regex",
+                    "children": {
+                      "elements": [
+                        {
+                          "name": "invis-container",
+                          "language": "regex",
+                          "children": {
+                            "elements": [
+                                {
+                                    "name": "string",
+                                    "language": "regex",
+                                    "properties": {
+                                      "value": "hello "
+                                    }
+                                }, 
+                                {
+                                    "name": "string",
+                                    "language": "regex",
+                                    "properties": {
+                                      "value": "world"
+                                    }
+                                }
+                            ]
+                          }
+                        }, 
+                      ]
+                    }
+                }
+            ]
+        }
+    }
+
+    const inp = new AST.SyntaxTree(inputDesc); 
+    const res = new AST.SyntaxTree(resultDesc); 
+    expect(res).toEqual(apply(inp, patterns)); 
+}); 
