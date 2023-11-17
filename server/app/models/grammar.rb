21287ecc2bd5eaca0719f732331e14689e86db9e
Birte Goldt <bigo@akra.de>
2023-11-17 11:46:02 +0100

    rubocop: remove unused options method argument

--- server/app/models/grammar.rb
+++ server/app/models/grammar.rb
@@ -213,7 +213,7 @@ class Grammar < ApplicationRecord
   # Computes a hash that may be sent back to the client if only superficial
   # information is required. This usually happens when the client attempts
   # to list available grammars.
-  def to_list_api_response(options: {})
+  def to_list_api_response
     to_json_api_response
       .slice('id', 'slug', 'name', 'technicalName', 'programmingLanguageId', 'generatedFromId')
   end
