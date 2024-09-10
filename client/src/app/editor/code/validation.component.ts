a0e2bce1614ba4cf7697a3f79d8220a409d912e7
Julia Schmaglowski <julia.schmaglowski@akra.de>
2024-09-10 09:58:06 +0200

    client: run prettier

--- client/src/app/editor/code/validation.component.ts
+++ client/src/app/editor/code/validation.component.ts
@@ -5,7 +5,7 @@ import { ErrorCodes, ValidationError } from "src/app/shared";
 import { Observable } from "rxjs";
 import { map, reduce } from "rxjs/operators";
 interface HumanValidationError extends ValidationError {
-  humanError : string
+  humanError: string;
 }
 
 /**
@@ -13,7 +13,7 @@ interface HumanValidationError extends ValidationError {
  */
 @Component({
   templateUrl: "templates/validation.html",
-  styleUrls: ["templates/validation.css"]
+  styleUrls: ["templates/validation.css"],
 })
 export class ValidationComponent {
   constructor(private _currentCodeResource: CurrentCodeResourceService) {}
@@ -21,18 +21,21 @@ export class ValidationComponent {
   readonly codeResource$ = this._currentCodeResource.currentResource;
 
   readonly result$ = this._currentCodeResource.validationResult;
-  readonly errors$ : Observable< Readonly<HumanValidationError[]> > = this.result$.pipe(
-    map( result => result.errors.map(e => {
-      return({...e, humanError:this.humanError(e.code)})
-    }))
-  )
+  readonly errors$: Observable<Readonly<HumanValidationError[]>> =
+    this.result$.pipe(
+      map((result) =>
+        result.errors.map((e) => {
+          return { ...e, humanError: this.humanError(e.code) };
+        })
+      )
+    );
 
-  humanError(code : string) : string {
-    switch(code) {
-      case ErrorCodes.Empty: 
+  humanError(code: string): string {
+    switch (code) {
+      case ErrorCodes.Empty:
         return "Der AST existiert nicht";
       case ErrorCodes.UnspecifiedRoot:
-        return  "Der AST kann nicht evaluiert werden, da nicht klar ist welche Wurzel verwendet wird";
+        return "Der AST kann nicht evaluiert werden, da nicht klar ist welche Wurzel verwendet wird";
       case ErrorCodes.UnknownRoot:
         return "Der AST hat einen Wurzelknoten, der zu keinem der erlaubten Wurzelknoten passt";
       case ErrorCodes.UnknownRootLanguage:
@@ -40,45 +43,45 @@ export class ValidationComponent {
       case ErrorCodes.UnexpectedType:
         return "Es wurde explizit ein anderer Typ erwartet";
       case ErrorCodes.TransientNode:
-        return " Es wurde ein Knoten mit dem Typ \"transient\" gefunden"
+        return 'Es wurde ein Knoten mit dem Typ "transient" gefunden';
       case ErrorCodes.MissingChild:
-        return "Es wurde ein bestimmtes Kind erwartet, dass allerdings nicht existiert."
+        return "Es wurde ein bestimmtes Kind erwartet, dass allerdings nicht existiert.";
       case ErrorCodes.SuperflousChild:
-        return "A specific child was entirely unexpected"
+        return "A specific child was entirely unexpected";
       case ErrorCodes.InvalidMaxOccurences:
-        return "Eins oder mehrere Kinder kommen zu oft vor (angesichts der erlaubten Beschränkungnen)"
+        return "Eins oder mehrere Kinder kommen zu oft vor (angesichts der erlaubten Beschränkungnen)";
       case ErrorCodes.InvalidMinOccurences:
-        return  "Eins oder mehrer Kinder kommen nicht oft genug vor (angesichts der erlaubten Beschränkungnen)"
+        return "Eins oder mehrer Kinder kommen nicht oft genug vor (angesichts der erlaubten Beschränkungnen)";
       case ErrorCodes.MissingProperty:
-        return  "Eine Eigenschaft wurde erwartet. Diese existiert jedoch nicht."
+        return "Eine Eigenschaft wurde erwartet. Diese existiert jedoch nicht.";
       case ErrorCodes.IllegalPropertyType:
-        return  "Eine Eigenschaft verletzt eine Beschränkung"
+        return "Eine Eigenschaft verletzt eine Beschränkung";
       case ErrorCodes.IllegalChildType:
-        return  "Ein Kind ist anwesend, aber der Typ wurde nicht erfragt"
+        return "Ein Kind ist anwesend, aber der Typ wurde nicht erfragt";
       case ErrorCodes.SuperflousChildCategory:
-        return  "Ein Typ erwähnt eine Kind Kategorie, die nicht im Wurzelknoten vorhanden ist"
+        return "Ein Typ erwähnt eine Kind Kategorie, die nicht im Wurzelknoten vorhanden ist";
       case ErrorCodes.NoChoiceMatching:
-        return  "Es wurde keine Übereinstimmung zwischen Wurzelknoten und Auswahlmöglichkeit gefunden."
+        return "Es wurde keine Übereinstimmung zwischen Wurzelknoten und Auswahlmöglichkeit gefunden.";
       case ErrorCodes.NoChoiceNodeAvailable:
-        return  "Es sollte ein Wurzelknoten vorhanden sein."
+        return "Es sollte ein Wurzelknoten vorhanden sein.";
       case ErrorCodes.SuperflousChoiceNodeAvailable:
-        return  "Es wurden zu viele Wurzelknoten gefunden Es sollt nur genau einen Wurzelknoten geben."
+        return "Es wurden zu viele Wurzelknoten gefunden Es sollt nur genau einen Wurzelknoten geben.";
       case ErrorCodes.ParenthesesEmptyTypes:
-        return  "Eine Gruppe von Klammern haben keinen Typ und sind deshalb unentschlossen."
+        return "Eine Gruppe von Klammern haben keinen Typ und sind deshalb unentschlossen.";
       case ErrorCodes.InvalidResourceId:
-        return "Der String sollte eine ID sein."
+        return "Der String sollte eine ID sein.";
       case "UNKNOWN_TABLE":
-        return "Die Tabelle ist nicht bekannt."
+        return "Die Tabelle ist nicht bekannt.";
       case "UNKNOWN_COLUMN":
-        return "Die Tabellenspalte ist nicht bekannt."
+        return "Die Tabellenspalte ist nicht bekannt.";
       case "TABLE_NOT_IN_FROM":
-        return "Die Tabelle ist nicht im FROM"
+        return "Die Tabelle ist nicht im FROM";
       case "DUPLICATE_TABLE_NAME":
-        return "Es liegt eine Dopplung vom Tabellennamen vor"
+        return "Es liegt eine Dopplung vom Tabellennamen vor";
       case "AGGREGATION_WITHOUT_GROUP_BY":
-        return "Ansammlung ohne ein \"GROUP BY\""
+        return 'Ansammlung ohne ein "GROUP BY"';
       default:
-        return "Kein Fehlercode bekannt"
-      }
+        return "Kein Fehlercode bekannt";
     }
   }
+}
